a58fd2f6c154e39942b97126d74543b850541d81
VAGWAYA <yasmin.agwan@pearson.com>
2019-08-20 17:13:52 +0530

    merge with develop

--- src/App.js
+++ src/App.js
@@ -33,35 +33,6 @@ class App extends Component {
         return (
             <Router>
                 <div>
-                    <h1>Expack</h1>
-                    <p className="description">Express and Webpack Boilerplate App</p>
-                    <div className="awful-selfie"></div>
-                    <hr />
-                    <React.Fragment>
-                        <h2>Your React App Is Running</h2>
-                        <hr />
-                        <Chunked />
-                        <br />
-                        <img src={img} />
-                    </React.Fragment>
-                    
-                    {/* <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/about/">About</Link>
-                            </li>
-                            <li>
-                                <Link to="/users/">Users</Link>
-                            </li>
-                        </ul>
-                    </nav>
-
-                    <Route path="/" exact component={Index} />
-                    <Route path="/about/" component={About} />
-                    <Route path="/users/" component={Users} /> */}
                 </div>
             </Router>
         );
