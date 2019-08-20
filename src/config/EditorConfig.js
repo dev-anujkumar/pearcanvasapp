186a2ec16755b7a51b2747dc9bc55c3a24c63eee
VAGWAYA <yasmin.agwan@pearson.com>
2019-08-20 21:05:21 +0530

    added test cases for element authering and tiny mce

--- src/config/EditorConfig.js
+++ src/config/EditorConfig.js
@@ -1,16 +1,16 @@
 import { CONTENT_STYLE } from './TinymceDefaultCss'
 export const EditorConfig = {
-    formats : {
+    formats: {
         // Changes the default format for h1 to have a class of heading
         "element-authoredtext": { block: 'p', classes: 'paragraphNumeroUno' },
         h1: { block: 'h1', classes: 'heading1NummerEins' },
-        h2:{ block: 'h2', classes: 'heading2NummerEins' },
-        h3:{ block: 'h3', classes: 'heading3NummerEins' },
-        h4:{ block: 'h4', classes: 'heading4NummerEins' },
-        h5:{ block: 'h5', classes: 'heading5NummerEins' },
-        h6:{ block: 'h6', classes: 'heading6NummerEins' },
+        h2: { block: 'h2', classes: 'heading2NummerEins' },
+        h3: { block: 'h3', classes: 'heading3NummerEins' },
+        h4: { block: 'h4', classes: 'heading4NummerEins' },
+        h5: { block: 'h5', classes: 'heading5NummerEins' },
+        h6: { block: 'h6', classes: 'heading6NummerEins' },
     },
-    toolbar : 'undo redo| bold italic underline strikethrough removeformat| alignleft aligncenter alignright alignjustify outdent indent numlist bullist | superscript subscript jsplus_special_symbols ',
-    contentStyle : CONTENT_STYLE,
-    plugins : "lists advlist"   
+    toolbar: 'undo redo| bold italic underline strikethrough removeformat| alignleft aligncenter alignright alignjustify outdent indent numlist bullist | superscript subscript jsplus_special_symbols ',
+    contentStyle: CONTENT_STYLE,
+    plugins: "lists advlist"
 }
