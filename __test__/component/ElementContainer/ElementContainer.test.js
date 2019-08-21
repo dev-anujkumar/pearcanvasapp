79aa2b0e8f976c5a6e92c0df296ee70df2d07fec
Abhishek Pal <abhishek.pal@magicsw.com>
2019-08-21 20:28:25 +0530

    Add test cases for element container

---
+++ __test__/component/ElementContainer/ElementContainer.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import ElementContainer from './../../../src/component/ElementContainer/ElementContainer';
+
+describe('Test for element container component', () => {
+    const element = {
+        id: "urn:pearson:work:8a49e877-144a-4750-92d2-81d5188d8e0a",
+        type: "element-authoredtext",
+        subtype: "",
+        schema: "http://schemas.pearson.com/wip-authoring/element/1",
+        elementdata: {
+            schema: "http://schemas.pearson.com/wip-authoring/authoredtext/1#/definitions/authoredtext",
+            text: ""
+        },
+        html: {
+            text: "<p class=\"paragraphNumeroUno\"><br></p>"
+        },
+        comments: true,
+        tcm: true,
+        versionUrn: "urn:pearson:work:8a49e877-144a-4750-92d2-81d5188d8e0a",
+        contentUrn: "urn:pearson:entity:b70a5dbe-cc3b-456d-87fc-e369ac59c527"
+    };
+
+    let elementContainer = mount(<ElementContainer element={element} />);
+
+    it('Render element container ', () => {
+        expect(elementContainer).toMatchSnapshot();
+
+        element.type = "opener";
+        elementContainer = mount(<ElementContainer element={element} />);
+        expect(elementContainer).toMatchSnapshot();
+
+        element.type = "figure";
+        elementContainer = mount(<ElementContainer element={element} />);
+        expect(elementContainer).toMatchSnapshot();
+    })
+});
--- src/component/ElementContainer/ElementContainer.js
+++ src/component/ElementContainer/ElementContainer.js
@@ -5,21 +5,23 @@ import Button from './../ElementButtons/ElementButton';
 import './../../styles/ElementContainer/ElementContainer.css';
 
 class ElementContainer extends Component {
-    renderElement = (element = '') => {
+    renderElement = (element = {}) => {
         let editor = '';
         let labelText = '';
         switch(element.type) {
             case 'opener':
                 editor = "Opener Element";
+                labelText = 'OE';
                 break;
 
             case "element-authoredtext":
-                editor = <ElementAuthoring type={element.type}/>;
+                editor = <ElementAuthoring type={element.type} />;
                 labelText = 'P';
                 break;
 
             case "figure":
-                editor = <ElementAuthoring type={element.type}/>;
+                editor = <ElementAuthoring type={element.type} />;
+                labelText = 'FG';
                 break;
         }
 
