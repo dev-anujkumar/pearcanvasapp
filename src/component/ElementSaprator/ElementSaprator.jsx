145c8c7dcb13c4fbdf31dc21f4252638253589b6
Devendra Dhakad <devendra.dhakad@magicedtech.com>
2019-08-21 19:32:26 +0530

    Element saprator Animation effect

--- src/component/ElementSaprator/ElementSaprator.jsx
+++ src/component/ElementSaprator/ElementSaprator.jsx
@@ -22,53 +22,61 @@ export default function ElementSaprator() {
           }
     });
 
-    function toggleElementList() {
+    function toggleElementList() {       
         setShowClass(!showClass)
     }
 
     return (
         <div className="elementSapratorContainer">
-            <div>
-                <Button type='split' onClick={splitSlateClickHandler} />              
-                <hr className='horizontalLine' />
-                <div className="dropdown">
+            <div className='elemDiv1'>
+                <Button type='split' onClick={splitSlateClickHandler} /> 
+            </div>
+            <div className='elemDiv2'>
+            <hr className='horizontalLine' />
+            </div>
+            <div className='elemDiv3'>
+            <div className="dropdown">
                     <Button onClick={ toggleElementList} className="dropbtn" type="expand" />
                     {/* <button onClick={() => setShowClass(!showClass)} className="dropbtn">Dropdown</button> */}
                     <div id="myDropdown" className={showClass ? 'dropdown-content show' : 'dropdown-content' }>
                         <ul>
                             <li>
-                            <Button type='split' onClick={splitSlateClickHandler} />              
+                            <Button type='split' onClick={addMediaClickHandler} />              
                             </li>
                             <li>
-                            <Button type='split' onClick={splitSlateClickHandler} />              
+                            <Button type='split' onClick={addMediaClickHandler} />              
                             </li>
                             <li>
-                            <Button type='split' onClick={splitSlateClickHandler} />              
+                            <Button type='split' onClick={addMediaClickHandler} />              
                             </li>
                             <li>
-                            <Button type='split' onClick={splitSlateClickHandler} />              
+                            <Button type='split' onClick={addMediaClickHandler} />              
                             </li>
                             <li>
-                            <Button type='split' onClick={splitSlateClickHandler} />              
+                            <Button type='split' onClick={addMediaClickHandler} />              
                             </li>
                             <li>
-                            <Button type='split' onClick={splitSlateClickHandler} />              
+                            <Button type='split' onClick={addMediaClickHandler} />              
                             </li>
                             <li>
-                            <Button type='split' onClick={splitSlateClickHandler} />              
+                            <Button type='split' onClick={addMediaClickHandler} />              
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
+                            
+                
         </div>
     )
 }
 
 function splitSlateClickHandler() {
     alert('split slate button clicked')
+    console.log('split slate button clicked')
 }
 
 function addMediaClickHandler() {
     alert('add media button clicked')
+    console.log('add media button clicked')
 }
