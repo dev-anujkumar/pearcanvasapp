fa8589b2883a182f9c3c6742aaed52c0cd8f2195
Chavi Srivastava <chavi.srivastava@magicsw.com>
2019-09-12 15:24:42 +0530

    add comment api integration

--- src/component/ElementContainer/ElementContainer.jsx
+++ src/component/ElementContainer/ElementContainer.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
 import ElementSingleAssessment from './../ElementSingleAssessment';
 import ElementAuthoring from './../ElementAuthoring';
 import ElementAudioVideo from './../ElementAudioVideo';
