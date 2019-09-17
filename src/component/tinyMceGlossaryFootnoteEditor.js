8b6eda87aa51fed49b498c2b82432c0c7e71d368
Akanksha Shakya <akanksha.shakya@pearson.com>
2019-09-17 15:41:25 +0530

    added Placeholder with tinymce

--- src/component/tinyMceGlossaryFootnoteEditor.js
+++ src/component/tinyMceGlossaryFootnoteEditor.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Editor } from '@tinymce/tinymce-react';
-import { GlossaryFootnoteEditorConfig } from '../config/EditorConfig';
+import { GlossaryFootnoteEditorConfig, EditorConfig } from '../config/EditorConfig';
 
 export class ReactEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.editorConfig = {
+      toolbar: GlossaryFootnoteEditorConfig.toolbar,
+      plugins: "placeholder",
+      menubar: false,
+      selector: '#glossary-0',
+      inline: true,
+      menubar: false,
+      statusbar: false,
+      object_resizing: false,
+      fixed_toolbar_container: '#toolbarGlossaryFootnote',
+      setup: (editor) => {
+        this.onEditorBlur(editor);
+      },
+      init_instance_callback: function (editor) {
+        editor.fire('focus');
+      },
+    }
   }
 
   onEditorBlur = (editor) => {
@@ -14,30 +30,40 @@ export class ReactEditor extends React.Component {
       e.preventDefault();
     });
   };
-  handleEditorChange = (e) => {
-    tinymce.activeEditor.formatter.apply('p');
+  // handleEditorChange = (e) => {
+  //   tinymce.activeEditor.formatter.apply('p');
+  // }
+  handleFocus = (e) => {
+    if (tinymce.activeEditor && tinymce.activeEditor.id === e.target.id) {
+      return false;
+    }
+
+    if (tinymce.activeEditor) {
+      let activeEditorId = tinymce.activeEditor.id;
+      tinymce.remove('#' + tinymce.activeEditor.id)
+      document.getElementById(activeEditorId).contentEditable = true;
+    }
+
+    this.editorConfig.selector = '#' + e.target.id;
+    tinymce.init(this.editorConfig);
   }
-
+  
   render() {
-
+    if (tinymce.activeEditor !== null && tinymce.activeEditor && tinymce.activeEditor.id) {
+      let activeEditorId = tinymce.activeEditor.id;
+      let element = document.getElementById(activeEditorId);
+      tinymce.remove('#' + tinymce.activeEditor.id)
+      element.contentEditable = true;
+      this.editorConfig.selector = '#' + activeEditorId;
+      tinymce.init(this.editorConfig);
+    }
     return (
-      <div className={this.props.className}>
+      <div >
         <Editor
-          init={{
-            toolbar: GlossaryFootnoteEditorConfig.toolbar,
-            menubar: false,
-            selector: '.definition-editor',
-            inline: true,
-            fixed_toolbar_container: '#toolbarGlossaryFootnote',
-            setup: (editor) => {
-              this.onEditorBlur(editor);
-            },
-            init_instance_callback: function (editor) {
-              editor.fire('focus');
-            },
-          }}
-          onChange={this.handleEditorChange}
+          init={this.editorConfig}
+        // onChange={this.handleEditorChange}
         />
+        <p className={this.props.className} placeholder={this.props.placeholder} onFocus={this.handleFocus} contentEditable="true" id={this.props.id} ></p>
       </div>
     )
   }
--- src/config/EditorConfig.js
+++ src/config/EditorConfig.js
@@ -23,6 +23,5 @@ export const GlossaryFootnoteEditorConfig = {
     formats: {
         'paragraph': { block: 'p', classes: 'paragraphNumeroUno' },
     },
-    toolbar: 'bold italic underline strikethrough removeformat superscript subscript',
-    plugins: "placeholder"
+    toolbar: 'bold italic underline strikethrough removeformat superscript subscript'
 }
