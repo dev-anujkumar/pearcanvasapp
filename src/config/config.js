8a400eb94de0275c385e032b48bd08ec15d73301
Shivam Gupta <shivam.gupta@magicsw.com>
2019-10-03 16:08:19 +0530

    resolved conflicts

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ let config = {
     JAVA_API_URL: "https://10.11.7.24:8443/app/toc-javaapp/",
     NARRATIVE_API_URL: "https://10.11.7.24:8443/app/toc-javaapp/v1/",
     STRUCTURE_APIKEY: "Gf7G8OZPaVGtIquQPbqpZc6D2Ri6A5Ld",
-    ssoToken: "Woi9AlmHZjGj1znAjrcRH8dnj2g.*AAJTSQACMDIAAlNLABw3NFRvcGkvN0tCUFJ5RENXcDlMRVluU2pRUjA9AAJTMQACMDE.*",
+    ssoToken: "EiFGuwLt6yvAlBpmYGJybnasrZE.*AAJTSQACMDIAAlNLABw0SDZ6dUwxTzRzcmlMMkhMZWM3QUlvaEFia1E9AAJTMQACMDM.*",
     userId: 'c5test01',
     assignee:'c5test01',
     slateEntityURN : "urn:pearson:entity:2b03e70f-8730-451b-9f9a-b496b6d91c9e",
