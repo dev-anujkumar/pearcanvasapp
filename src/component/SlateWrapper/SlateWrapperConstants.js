b153965ad1907e9248602c27c0fb2f86050fb012
Kanika Arora <kanika.arora@magicsw.com>
2019-10-14 20:12:11 +0530

    fixes of conflict

--- src/component/SlateWrapper/SlateWrapperConstants.js
+++ src/component/SlateWrapper/SlateWrapperConstants.js
@@ -6,9 +6,6 @@ export const INTERACTIVE="INTERACTIVE";
 export const CONTAINER="CONTAINER";
 export const WORKED_EXAMPLE = "WORKED_EXAMPLE";
 export const SECTION_BREAK = "SECTION_BREAK";
-<<<<<<< HEAD
 export const METADATA_ANCHOR = "LO";
 export const LO_LIST = "LO_LIST";
-=======
 export const ASSESSMENT_SLATE = "element-assessment";
->>>>>>> b0282d8a9ea6a31197e02f270e7767ddc9ec2622
