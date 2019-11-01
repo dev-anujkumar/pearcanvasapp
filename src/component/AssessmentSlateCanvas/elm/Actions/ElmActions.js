93e40d1947203331695bc4ed4d856028c79b9d12
Chavi Srivastava <chavi.srivastava@magicsw.com>
2019-11-01 15:56:36 +0530

    learnosity api integration - case handled

--- src/component/AssessmentSlateCanvas/elm/Actions/ElmActions.js
+++ src/component/AssessmentSlateCanvas/elm/Actions/ElmActions.js
@@ -1,4 +1,5 @@
 import config from '../../../../config/config';
+import {FULL_ASSESSMENT_PUF} from '../../AssessmentSlateConstants';
 import axios from 'axios';
 let headers = {
     ApiKey: config.STRUCTURE_APIKEY,
@@ -6,29 +7,53 @@ let headers = {
 }
 
 /**
- * This action creator is used to fetch ELM resources added to the project
+ * This action creator is used to fetch ELM or Learnosity resources added to the project
  */
-export const insertElmResourceAction = () => (dispatch) => {
+export const insertElmResourceAction = (assessmentType) => (dispatch) => {
+    if(assessmentType === FULL_ASSESSMENT_PUF){
         return axios.get(`${config.ELM_ENDPOINT}v2/${config.projectUrn}/alignments/resources`, {
-          headers: headers
-    }).then((res) => {
-        dispatch({
-            type: 'GET_ELM_RESOURCES',
-            payload: {
-                data: res.data,
-                errFlag: false,
-                apiStatus: "200"
-            }
-        })
-    }).catch((error) => {
-        dispatch({
-            type: 'GET_ELM_RESOURCES',
-            payload: {
-                data: [],
-                errFlag: true,
-                apiStatus: error.response.status
-            }
-        })
-    })
+            headers: headers
+      }).then((res) => {
+          dispatch({
+              type: 'GET_ELM_RESOURCES',
+              payload: {
+                  data: res.data,
+                  errFlag: false,
+                  apiStatus: "200"
+              }
+          })
+      }).catch((error) => {
+          dispatch({
+              type: 'GET_ELM_RESOURCES',
+              payload: {
+                  data: [],
+                  errFlag: true,
+                  apiStatus: error.response.status
+              }
+          })
+      })
+    }else{
+        return axios.get(`${config.ELM_ENDPOINT}v2/${config.projectUrn}/alignments/resources`, {
+            headers: headers
+      }).then((res) => {
+          dispatch({
+              type: 'GET_ELM_RESOURCES',
+              payload: {
+                  data: res.data,
+                  errFlag: false,
+                  apiStatus: "200"
+              }
+          })
+      }).catch((error) => {
+          dispatch({
+              type: 'GET_ELM_RESOURCES',
+              payload: {
+                  data: [],
+                  errFlag: true,
+                  apiStatus: error.response.status
+              }
+          })
+      })
+    }
 }
 
