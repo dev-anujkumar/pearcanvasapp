eaa2fd6cf2493ebc262f858eb0824ac9c39fe7c2
Chavi Srivastava <chavi.srivastava@magicsw.com>
2019-11-05 11:16:35 +0530

    test cases updated

--- __test__/appstore/toolbarReducer.test.js
+++ __test__/appstore/toolbarReducer.test.js
@@ -4,20 +4,21 @@ import { TOGGLE_BORDERS } from '../../src/constants/Action_Constants';
 const INIT_STATE = {
     elemBorderToggle: true
 }
-
+const expectedState={
+    elemBorderToggle: false
+}
 describe('testing Toolbar Reducer cases --', () => {
 
     it('should return the initial state', () => {
         expect(toolbarReducer(undefined, {})).toEqual(INIT_STATE);
     });
     it('Change elemBorderToggle value ', () => {
-        toolbarReducer(INIT_STATE, {
+     expect(toolbarReducer(INIT_STATE, {
             type: TOGGLE_BORDERS,
             payload: {
                 elemBorderToggle: false
             }
-        })
+        })).toEqual(expectedState);
     })
-
 });
 
