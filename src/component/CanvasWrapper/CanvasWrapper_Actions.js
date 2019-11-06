b927881cb966ab3d5892910e4b1bc1a02bdefb58
Abhishek Pal <abhishek.pal@magicsw.com>
2019-11-06 15:52:51 +0530

    handle incorrect data common

--- src/component/CanvasWrapper/CanvasWrapper_Actions.js
+++ src/component/CanvasWrapper/CanvasWrapper_Actions.js
@@ -15,34 +15,23 @@ import elementDataBank from './elementDataBank'
 const findElementType = (element, index) => {
 	let elementType = {};
 	elementType['tag'] = '';
-	if(element.type) {
+
+	try {
 		switch (element.type) {
 			case 'element-authoredtext':
-				if('html' in element) {
-					elementType['elementType'] = elementDataBank[element.type]["elementType"];
-					if ('elementdata' in element && 'headers' in element.elementdata && element.elementdata.headers) {
-						elementType['primaryOption'] = elementDataBank["element-authoredtext-heading"]["primaryOption"];
-						elementType['secondaryOption'] = 'secondary-heading-' + element.elementdata.headers[0].level;
-					} else {
-						elementType['primaryOption'] = elementDataBank[element.type]["primaryOption"];
-						elementType['secondaryOption'] = elementDataBank[element.type]["secondaryOption"];
-					}
+				elementType['elementType'] = elementDataBank[element.type]["elementType"];
+				if ('elementdata' in element && 'headers' in element.elementdata && element.elementdata.headers) {
+					elementType['primaryOption'] = elementDataBank["element-authoredtext-heading"]["primaryOption"];
+					elementType['secondaryOption'] = 'secondary-heading-' + element.elementdata.headers[0].level;
 				} else {
-					elementType = {
-						elementType: ''
-					}
+					elementType['primaryOption'] = elementDataBank[element.type]["primaryOption"];
+					elementType['secondaryOption'] = elementDataBank[element.type]["secondaryOption"];
 				}
 				break;
 
 			case 'element-blockfeature':
-				if(element.elementdata && element.elementdata.type && 'html' in element) {
-					elementType = {
-						...elementDataBank[element.type][element.elementdata.type]
-					}
-				} else {
-					elementType = {
-						elementType: ''
-					}
+				elementType = {
+					...elementDataBank[element.type][element.elementdata.type]
 				}
 				break;
 
@@ -53,103 +42,61 @@ const findElementType = (element, index) => {
 					case "mathImage":
 					case "authoredtext":
 					case "tableasmarkup":
-						if(element.figuretype && element.subtype && 'html' in element) {
-							elementType = {
-								elementType : elementDataBank[element.type][element.figuretype]["elementType"],
-								primaryOption : elementDataBank[element.type][element.figuretype]["primaryOption"],
-								...elementDataBank[element.type][element.figuretype][element.subtype]
-							}
-						} else {
-							elementType = {
-								elementType: ''
-							}
+						elementType = {
+							elementType : elementDataBank[element.type][element.figuretype]["elementType"],
+							primaryOption : elementDataBank[element.type][element.figuretype]["primaryOption"],
+							...elementDataBank[element.type][element.figuretype][element.subtype]
 						}
 						break;
 					case "codelisting":
-						if(element.figuretype && element.figuredata && 'html' in element) {
-							elementType = {
-								elementType : elementDataBank[element.type][element.figuretype]["elementType"],
-								primaryOption : elementDataBank[element.type][element.figuretype]["primaryOption"],
-							}
-							switch (element.figuredata.programlanguage){
-								case "Select":
-									elementType.secondaryOption = `secondary-blockcode-language-Default`
-									break;
+						elementType = {
+							elementType : elementDataBank[element.type][element.figuretype]["elementType"],
+							primaryOption : elementDataBank[element.type][element.figuretype]["primaryOption"],
+						}
+						switch (element.figuredata.programlanguage){
+							case "Select":
+								elementType.secondaryOption = `secondary-blockcode-language-Default`
+								break;
 
-								default:
-									elementType.secondaryOption = `secondary-blockcode-language-${element.figuredata.programlanguage}`
-							}
-						} else {
-							elementType = {
-								elementType: ''
-							}
+							default:
+								elementType.secondaryOption = `secondary-blockcode-language-${element.figuredata.programlanguage}`
 						}
 						break;
 					case "video":
 					case "audio" :
-						if(element.figuretype && element.figuredata && element.figuredata.srctype && 'html' in element) {
-							elementType = {
-								elementType : elementDataBank[element.type][element.figuretype]["elementType"],
-								primaryOption : elementDataBank[element.type][element.figuretype]["primaryOption"],
-								...elementDataBank[element.type][element.figuretype][element.figuredata.srctype]
-							}
-						} else {
-							elementType = {
-								elementType: ''
-							}
+						elementType = {
+							elementType : elementDataBank[element.type][element.figuretype]["elementType"],
+							primaryOption : elementDataBank[element.type][element.figuretype]["primaryOption"],
+							...elementDataBank[element.type][element.figuretype][element.figuredata.srctype]
 						}
 						break;
 					case "interactive":
-						if(element.figuretype && element.figuredata && element.figuredata.interactivetype && 'html' in element) {
-							elementType = {
-								elementType : elementDataBank[element.type][element.figuretype]["elementType"],
-								primaryOption : elementDataBank[element.type][element.figuretype][element.figuredata.interactivetype]["primaryOption"],
-								secondaryOption : elementDataBank[element.type][element.figuretype][element.figuredata.interactivetype]["secondaryOption"]
-							}
-						} else {
-							elementType = {
-								elementType: ''
-							}
+						elementType = {
+							elementType : elementDataBank[element.type][element.figuretype]["elementType"],
+							primaryOption : elementDataBank[element.type][element.figuretype][element.figuredata.interactivetype]["primaryOption"],
+							secondaryOption : elementDataBank[element.type][element.figuretype][element.figuredata.interactivetype]["secondaryOption"]
 						}
 						break;
 					case "assessment":
-						if(element.figuretype&& element.figuredata && element.figuredata.elementdata.assessmentformat) {
-							elementType = {
-								elementType : elementDataBank[element.type][element.figuretype]["elementType"],
-								primaryOption : elementDataBank[element.type][element.figuretype]["primaryOption"],
-								...elementDataBank[element.type][element.figuretype][element.figuredata.elementdata.assessmentformat]
-							}
-						} else {
-							elementType = {
-								elementType: ''
-							}
+						elementType = {
+							elementType : elementDataBank[element.type][element.figuretype]["elementType"],
+							primaryOption : elementDataBank[element.type][element.figuretype]["primaryOption"],
+							...elementDataBank[element.type][element.figuretype][element.figuredata.elementdata.assessmentformat]
 						}
 						break;
 				}
 				break;
 
 			case 'element-aside':
-				if(element.subtype && element.designtype) {
-					elementType = {
-						elementType : elementDataBank[element.type][element.subtype]["elementType"],
-						...elementDataBank[element.type][element.subtype][element.designtype]
-					}
-				} else {
-					elementType = {
-						elementType: ''
-					}
+				elementType = {
+					elementType : elementDataBank[element.type][element.subtype]["elementType"],
+					...elementDataBank[element.type][element.subtype][element.designtype]
 				}
 				break;
 
 			case 'element-list':
-				if(element.subtype && 'html' in element) {
-					elementType = {
-						...elementDataBank[element.type][element.subtype]
-					}
-				} else {
-					elementType = {
-						elementType: ''
-					}
+				elementType = {
+					...elementDataBank[element.type][element.subtype]
 				}
 				break;
 
@@ -163,7 +110,7 @@ const findElementType = (element, index) => {
 			default:
 				elementType = {...elementDataBank["element-authoredtext"]}
 		}
-	} else {
+	} catch(err) {
 		elementType = {
 			elementType: ''
 		}
