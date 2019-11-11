b8427ddbf1144c08f6310d7de6932d950bc6c085
Shivam Gupta <shivam.gupta1@pearson.com>
2019-11-11 22:10:58 +0530

    BCE bug fix initial commit

--- src/component/tinyMceEditor.js
+++ src/component/tinyMceEditor.js
@@ -252,14 +252,14 @@ export class TinyMceEditor extends Component {
                         activeElement.innerHTML = div.children[0].outerHTML;
                     }
                 }
-                else if (activeElement.children.length <= 1 && activeElement.children[0].tagName === 'BR') {
+                else if (activeElement.children.length <= 1 && activeElement.children[0].tagName === 'BR' && activeElement.nodeName !== "CODE") {
                     //code to avoid deletion of editor first child(like p,h1,blockquote etc)
                     let div = document.createElement('div');
                     div.innerHTML = this.lastContent;
-                    if(div.children && div.children[0]){
+                    // if(div.children && div.children[0]){
                         div.children[0].innerHTML = '<br/>';
                         activeElement.innerHTML = div.children[0].outerHTML;
-                    }
+                    // }
                 }
                 if (activeElement.innerText.trim().length) {
                     activeElement.classList.remove('place-holder')
@@ -295,7 +295,7 @@ export class TinyMceEditor extends Component {
                         activeElement.innerHTML = div.children[0].outerHTML;
                     }
                 }
-                else if (activeElement.children.length <= 1 && activeElement.children[0].tagName === 'BR') {
+                else if (activeElement.children.length <= 1 && activeElement.children[0].tagName === 'BR' && activeElement.nodeName !== "CODE") {
                     let div = document.createElement('div');
                     div.innerHTML = this.lastContent;
                     if(div.children && div.children[0]){
