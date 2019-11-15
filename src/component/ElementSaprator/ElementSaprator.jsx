45f47061aed74e0c98952603c4a3c27d30a5aae9
Kanika Arora <kanika.arora@magicsw.com>
2019-11-15 14:26:47 +0530

    fixes

--- src/component/ElementSaprator/ElementSaprator.jsx
+++ src/component/ElementSaprator/ElementSaprator.jsx
@@ -142,7 +142,6 @@ function asideButton(esProps,sectionBreak){
  */
 export function renderDropdownButtons(esProps, elementType, sectionBreak, closeDropDown) {
     let updatedEsProps, buttonType;
-
     if (config.parentEntityUrn == FRONT_MATTER || config.parentEntityUrn == BACK_MATTER) {
         if (elementType == ELEMENT_ASIDE) {
             esProps = asideButton(esProps, sectionBreak);
@@ -199,59 +198,9 @@ export function renderDropdownButtons(esProps, elementType, sectionBreak, closeD
                 return buttonType !== SECTION_BREAK && buttonType !== OPENER;
             })
         }
-        console.log(updatedEsProps)
     }
 
-    // if (config.slateType == CONTAINER_INTRO && (!config.isCO || config.isLOL)) {
-    //     // hide the metadata anchor on IS when its once created
-    //     if (config.isLOL) {
-    //         let elements = document.getElementsByClassName(METADATA_ANCHOR);
-    //         for (let key in elements) {
-    //             if (elements[key].className) { elements[key].className += " disabled"; }
-    //         }
-    //     }
-
-    //     if (!config.isCO) {
-    //         updatedEsProps = esProps.filter((btnObj) => {
-    //             return btnObj.buttonType !== SECTION_BREAK;
-    //         })
-    //     } else {
-    //         updatedEsProps = esProps.filter((btnObj) => {
-    //             buttonType = btnObj.buttonType;
-    //             return buttonType !== SECTION_BREAK && buttonType !== OPENER;
-    //         })
-    //     }
-
-    // } else {
-    //     updatedEsProps = esProps.filter((btnObj) => {
-    //         buttonType = btnObj.buttonType;
-    //         return buttonType !== SECTION_BREAK && buttonType !== OPENER;
-    //     })
-    // }
-
-    // //hide the metadata anchor from frontmatter and backmatter
-    // if (config.parentEntityUrn == FRONT_MATTER || config.parentEntityUrn == BACK_MATTER) {
-    //     updatedEsProps = esProps.filter((btnObj) => {
-    //         buttonType = btnObj.buttonType;
-    //         return buttonType !== METADATA_ANCHOR && buttonType !== SECTION_BREAK && buttonType !== OPENER;
-    //     })
-    // }
-
-    // if (elementType == ELEMENT_ASIDE ) {
-    //     updatedEsProps = esProps.filter((btnObj) => {
-    //         buttonType = btnObj.buttonType;
-    //         if (sectionBreak && (config.parentEntityUrn !== FRONT_MATTER && config.parentEntityUrn !== BACK_MATTER)) {
-    //             return  buttonType !== WORKED_EXP && buttonType !== CONTAINER && buttonType !== OPENER;
-    //         } else if(!sectionBreak && (config.parentEntityUrn == FRONT_MATTER || config.parentEntityUrn == BACK_MATTER)) {
-    //             return buttonType !== SECTION_BREAK && buttonType !== METADATA_ANCHOR && buttonType !== OPENER && buttonType !== WORKED_EXP && buttonType !== CONTAINER;
-    //         }else if (sectionBreak && (config.parentEntityUrn == FRONT_MATTER || config.parentEntityUrn == BACK_MATTER)){
-    //             return buttonType !== METADATA_ANCHOR && buttonType !== OPENER && buttonType !== WORKED_EXP && buttonType !== CONTAINER;
-    //         }else{
-    //             return buttonType !== OPENER && buttonType !== SECTION_BREAK && buttonType !== WORKED_EXP && buttonType !== CONTAINER;
-    //         }
-    //     })
-        
-    // }
+    
 
     return updatedEsProps.map((elem, key) => {
         function buttonHandlerFunc() {
