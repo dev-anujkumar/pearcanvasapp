8d47c1556aeec04734d0e880b62c998068a2c6c3
Abhishek Pal <abhishek.pal@magicsw.com>
2019-11-25 15:13:17 +0530

    Tinymce glossary footnote config coverage

--- __test__/component/ElementAuthoring/tinyMceGlossaryFootnoteEditor.test.js
+++ __test__/component/ElementAuthoring/tinyMceGlossaryFootnoteEditor.test.js
@@ -1,29 +1,48 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import tinymce from 'tinymce/tinymce';
+import { mount, shallow } from 'enzyme';
+// import tinymce from 'tinymce/tinymce';
+// import "tinymce/plugins/paste";
 import ReactEditor from '../../../src/component/tinyMceGlossaryFootnoteEditor'
 import { getGlossaryFootnoteId } from "../../../src/js/glossaryFootnote"
 import moxios from 'moxios';
+import { JestEnvironment } from '@jest/environment';
+import { tinymceFormulaChemistryIcon } from '../../../src/images/TinyMce/TinyMce';
 
 beforeEach(() => moxios.install() )
     
 afterEach(() => moxios.uninstall());
 
 const callback = (res) => {}
+
+// jest.mock('tinymce/tinymce', () => {
+//     tinymce: {
+//         init: jest.fn()
+//     }
+// });
+// jest.mock('tinymce/plugins/paste', () => {
+//     return {}
+// });
+
 describe('Testing tinyMce component for glossary footnote with  props', () => {
     let props = {
         placeholder: "Type Something...",
-        id: 0,
+        id: "glossary-0",
         glossaryFootNoteCurrentValue: "",
         className: "place-holder"
     }
     const tinyMceEditor = mount( <ReactEditor {...props}  />, {attachTo: window.domNode} )
     // let tinyMceEditorInstance = tinyMceEditor.find('TinyMceEditor').instance();
     
-    it('Test id id would be null -', () => {
-        // tinyMceEditor.setProps({
-        //     id: null
-        // });
+    it('Test for glossary -', () => {
+        tinyMceEditor.setProps({
+            id: "glossary-0"
+        });
+    });
+
+    it('Editor Click Event -', () => {
+        tinyMceEditor.find('p').simulate('click', (e) => {
+            console.log('event', e);
+        });
     });
 
     it('Handle Place-Holder event -', () => {
@@ -31,9 +50,6 @@ describe('Testing tinyMce component for glossary footnote with  props', () => {
             glossaryFootNoteCurrentValue: "<p>Sample Data</p>",
             className: ""
         });
-    });
-
-    it('Editor Click Event -', () => {
-        tinyMceEditor.find('p').simulate('click')
+        new ReactEditor();
     });
 })
