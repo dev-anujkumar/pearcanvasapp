383816d747b8fe4821d2b2a50cfbdbbda80eaa04
Pratap Singh, Raghvendra <raghvendra.pratapsingh@pearson.com>
2019-11-29 08:01:34 -0600

    updated with tag canvas-0.1.13

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 // IMPORT - Assets //
 import './styles/style.css';
 
-console.log("!!!!! ---- canvas-rc-0.0.1 ---- !!!!!!")
+console.log("!!!!! ---- canvas-0.1.13 ---- !!!!!!")
 
 class App extends Component {
     constructor(props) {
