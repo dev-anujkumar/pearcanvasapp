9cc4462e39a944267e1a18586f2abb66201a90b2
Chavi Srivastava <chavi.srivastava@magicsw.com>
2019-12-03 17:32:55 +0530

    console removed

--- src/component/Sidebar/Sidebar_Action.js
+++ src/component/Sidebar/Sidebar_Action.js
@@ -24,7 +24,6 @@ const convertElement = (oldElementData, newElementData, oldElementInfo, store, i
 
     let inputSubTypeEnum = inputSubType['enum'],
     inputPrimaryOptionEnum = inputPrimaryOptionType['enum']
-console.log("oldElementData",oldElementData)
     if(oldElementData.figuretype==="assessment"){
         inputPrimaryOptionEnum=inputSubType['enum'];
         inputSubTypeEnum=document.querySelector(`div[data-id='${oldElementData.id}'] span.singleAssessment_Dropdown_currentLabel`).innerText.toUpperCase().replace(" ", "_").replace("-", "_");
@@ -40,15 +39,12 @@ console.log("oldElementData",oldElementData)
     if (oldElementData.type === "figure") {
         if (!(imageSource.includes(oldElementData.figuretype) && imageDestination.includes(newElementData['primaryOption']))){
             oldElementData.figuredata = {...figureDataBank[newElementData['primaryOption']]}
-            console.log("oldElementData.figuredata1",oldElementData.figuredata)
         }
         if(oldElementData.figuredata.srctype){
             oldElementData.figuredata.srctype=outputSubType['wipValue']
-            console.log("oldElementData.figuredata2",oldElementData.figuredata)
         }
         if(oldElementData.figuredata.interactivetype){
             oldElementData.figuredata.interactivetype=outputSubType['wipValue'];
-            console.log("oldElementData.figuredata3",oldElementData.figuredata)
         }
 
         /* on Conversion removing the tinymce instance for BCE element*/
