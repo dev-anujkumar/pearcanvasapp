6b8ee5d728bb3f362d60e8d2052a38da00b73ef5
Anuj Jena <anuj.jena@magicsw.com>
2019-12-03 12:20:02 +0530

    Revert "test cases for glossary Footnotes Popup file"
    This reverts commit 00cbc4cee55fae68186b3c647d04ebb261af4eff.

--- __test__/component/GlossaryFootnote/GlossaryFootnotePopup.test.js
+++ __test__/component/GlossaryFootnote/GlossaryFootnotePopup.test.js
@@ -7,38 +7,35 @@ const middlewares = [thunk];
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 const mockStore = configureMockStore(middlewares);
-import * as util from '../../../src/js/utils.js';
-import { exportAllDeclaration } from '../../../../../AppData/Local/Microsoft/TypeScript/3.6/node_modules/@babel/types/lib';
-
-util.checkforToolbarClick = (relatedTargets) => {
-    return true;
-};
-
-let props = {
-    glossaryFootnoteValue : {type: "Glossary"},
-    glossaryFootNoteCurrentValue: {"glossaryContentText": ""}
-};
-describe('Testing GlossaryFootnote component with props', () => {
+const store = mockStore({
+    glossaryFootnoteReducer:{glossaryFootnoteValue:{"type":"","popUpStatus":false}
+    }
+  
+  
+});
+xdescribe('Testing GlossaryFootnote component with props', () => {
     const div = document.createElement('div');
-    const wrapper = mount( <GlossaryFootnotePopup   {...props}/>)
+    const wrapper = mount( <GlossaryFootnotePopup   />)
     let GlossaryFootnotePopupInstance = wrapper.find('GlossaryFootnotePopup').instance();
     it('render Glossary Footnote component ', () => {
-        ReactDOM.render(<GlossaryFootnotePopup {...props}/>, div);
+        ReactDOM.render(<Provider store={store}><GlossaryFootnotePopup /></Provider>, div);
         ReactDOM.unmountComponentAtNode(div);
     })
-    it('componentWillUnmount Event', () => {
-        GlossaryFootnotePopupInstance.componentWillUnmount()
+
+    it('render Glossary ', () => {
+        ReactDOM.render(<Provider store={store}><GlossaryFootnotePopup glossaryFootnote='Glossary' /></Provider>, div);
+        ReactDOM.unmountComponentAtNode(div);
     })
-    it('toolbarHandling Event', () => {
-        const e = {
-            stopPropagation() { }
-        }
-        let action = "add";
-        const elementIdInfo = document.createElement('div');
-        elementIdInfo.setAttribute('id', 'toolbarGlossaryFootnote');
-        elementIdInfo.setAttribute('class', 'tox-toolbar');
-        document.body.appendChild(elementIdInfo);
-        GlossaryFootnotePopupInstance.toolbarHandling(e, action);
-        expect(wrapper.find('.tox-toolbar').length).toBe(0)
+
+    it('render Footnote ', () => {
+        ReactDOM.render(<Provider store={store}><GlossaryFootnotePopup glossaryFootnote='Footnote' /></Provider>, div);
+        ReactDOM.unmountComponentAtNode(div);
+    })
+
+    it('ComponetDidMount Event', () => {
+        GlossaryFootnotePopupInstance.componentDidMount()
+    })
+    it('ComponetDidUpdate Event', () => {
+        GlossaryFootnotePopupInstance.componentDidUpdate()
     })
 })
