6b012a0d2fbfd03cce90ca8b82260f19bd8d3e7a
Rajat Sehdev <rajat.sehdev@magicsw.com>
2019-12-11 12:44:34 +0530

    app coverage increased

--- __test__/App.test.js
+++ __test__/App.test.js
@@ -10,8 +10,10 @@ jest.mock('../src/auth/openam.js', () => {
 })
 describe('App component', () => { 
     it('renders without crashing', () => {
+        let location = 'https://example.com?projectUrn=urn:pearson:distributable:977c95a8-e16a-413c-bfd0-788fd2a3698d&projectEntityUrn=urn:pearson:entity:3d9363f1-36bb-47ea-8842-9b142027692c&slateEntityURN=326qrygrgfuydfhdfhfhdfdh&slateManifestURN=urn:pearson:manifest:d9023151-3417-4482-8175-fc965466220e&ssoToken=dklfndndfdf&';
         const div = document.createElement('div');
-        ReactDOM.render(<App />, div);
+        ReactDOM.render(<App testWindowMock = {location}/>, div); 
         ReactDOM.unmountComponentAtNode(div);
+        expect(typeof(ReactDOM.unmountComponentAtNode)).toEqual('function');
       });
 });
