f608b2b906c08541842670b721d46391fdeb1a63
Pratap Singh, Raghvendra <raghvendra.pratapsingh@pearson.com>
2019-12-13 17:15:18 +0530

    slate load infinite call fixes

--- src/App.jsx
+++ src/App.jsx
@@ -74,8 +74,7 @@ class App extends Component {
     modifyObjKeys = (obj, newObj) => {
         Object.keys(obj).forEach(function(key) {
           delete obj[key];
-        });
-      
+        });      
         Object.keys(newObj).forEach(function(key) {
           obj[key] = newObj[key];
         });
