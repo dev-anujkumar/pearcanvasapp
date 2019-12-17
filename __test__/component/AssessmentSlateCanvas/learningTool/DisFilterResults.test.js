fd0a7aad38128758c1508828a2513448b86d671c
Vaibhav Singhal <vaibhav.singhal@pearson.com>
2019-12-17 17:56:11 +0530

    dis filter results test cases

--- __test__/component/AssessmentSlateCanvas/learningTool/DisFilterResults.test.js
+++ __test__/component/AssessmentSlateCanvas/learningTool/DisFilterResults.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import DisFilterResults  from '../../../../src/component/AssessmentSlateCanvas/learningTool/DisFilterResults';
-import {tempFiguresForResults,disciplines,apiResultObject,selectedResult} from '../../../../fixtures/learningTool'
+import {tempFiguresForResults,disciplinesForLearningTool,apiResultObject,selectedResult} from '../../../../fixtures/learningTool'
 import { spy, stub } from 'sinon';
 const selectedFigure = new stub();
 describe('Testing Figure Card component with props', () => {
     let props = {
-        apiResponseForDis: disciplines,
+        apiResponseForDis: disciplinesForLearningTool,
         selectedResult:selectedResult
       }
     let wrapper = mount( < DisFilterResults   selectedFigure = {selectedFigure}
@@ -14,7 +14,19 @@ describe('Testing Figure Card component with props', () => {
     const instance = wrapper.find('DisFilterResults').instance();
 
      it('should have disApiResultsJsx  component', () => {
-        instance.disApiResultsJsx (disciplines);
+        expect(typeof(instance.disApiResultsJsx (disciplinesForLearningTool))).toBe('object');
     }) 
+})
+describe('Testing Figure Card component without props', () => {
+  let props = {
+      apiResponseForDis: [],
+      selectedResult:selectedResult
+    }
+  let wrapper = mount( < DisFilterResults   selectedFigure = {selectedFigure}
+       {...props} />) 
+  const instance = wrapper.find('DisFilterResults').instance();
 
-    })
+   it('should have disApiResultsJsx  component', () => {
+    expect(typeof(instance.disApiResultsJsx ([]))).toBe('object');
+  }) 
+})
