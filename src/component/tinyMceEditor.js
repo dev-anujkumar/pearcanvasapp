e95d1882a5bfcbefd65b011b8150f113be7d4712
Gupta, Ankit <ankit.gupta1@pearson.com>
2019-12-17 01:05:55 -0600

    tinyMceEditor.js edited online with Bitbucket

--- src/component/tinyMceEditor.js
+++ src/component/tinyMceEditor.js
@@ -1292,12 +1292,14 @@ export class TinyMceEditor extends Component {
      * @param {*} e  event object
      */
     handleBlur = (e, forceupdate) => {
-        let isBlockQuote = this.props.element && this.props.element.elementdata && (this.props.element.elementdata.type === "marginalia" || this.props.element.elementdata.type === "blockquote");
+      /*  
+      let isBlockQuote = this.props.element && this.props.element.elementdata && (this.props.element.elementdata.type === "marginalia" || this.props.element.elementdata.type === "blockquote");
         let tempdiv = document.createElement('div');
         tempdiv.innerHTML = tinymce.activeEditor.getContent()
         if (isBlockQuote && !tinymce.$(tempdiv).find('.paragraphNummerEins').length  || !tinymce.$(tempdiv).find('.paragraphNummerEins').text().length) {
             tinymce.activeEditor.setContent(this.lastContent);
         }
+        */
         let relatedTargets = (e&&e.relatedTarget&&e.relatedTarget.classList)?e.relatedTarget.classList : [];
         if(checkforToolbarClick(relatedTargets)){
             e.stopPropagation();
