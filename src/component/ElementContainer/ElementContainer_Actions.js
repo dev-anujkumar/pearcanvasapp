a4aa7aaa924e47b6d81cc167b605f128093e5803
Pratap Singh, Raghvendra <raghvendra.pratapsingh@pearson.com>
2019-12-17 15:58:52 +0530

    version for create delete update

--- src/component/ElementContainer/ElementContainer_Actions.js
+++ src/component/ElementContainer/ElementContainer_Actions.js
@@ -83,6 +83,22 @@ export const addComment = (commentString, elementId, asideData, parentUrn) => (d
         })
 }
 
+function createNewVersionOfSlate(){
+    fetch(`${config.STRUCTURE_API_URL}structure-api/context/v2/${config.projectUrn}/container/${config.slateEntityURN}/version`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "PearsonSSOSession": config.ssoToken,
+                "ApiKey": config.APO_API_KEY,
+            }
+        })
+            .then(res => res.json()) // OR res.json()
+            .then((res) => {
+                sendDataToIframe({ 'type': 'sendMessageForVersioning', 'message': 'updateSlate' });
+        })
+}
+
 export const deleteElement = (elmId, type, parentUrn, asideData, contentUrn, index) => (dispatch, getState) => {
 
     const prepareDeleteRequestData = (elementType) => {
@@ -100,6 +116,14 @@ export const deleteElement = (elmId, type, parentUrn, asideData, contentUrn, ind
                     "workUrn": elmId
                 }
         }
+    }
+
+    let parentData = getState().appStore.slateLevelData;
+    let currentParentData = JSON.parse(JSON.stringify(parentData));
+    let currentSlateData = currentParentData[config.slateManifestURN];
+    if (currentSlateData.status === 'approved') {
+        createNewVersionOfSlate()
+        return false;
     }
 
     let _requestData = prepareDeleteRequestData(type)
