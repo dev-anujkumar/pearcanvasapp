a5813b7095ec85f99c5794efec5f815c3d4554a0
Akansh <akansh.goel@pearson.com>
2019-12-20 15:10:40 +0530

    popup multiple toolbar issue

--- src/component/tinyMceEditor.js
+++ src/component/tinyMceEditor.js
@@ -1099,7 +1099,7 @@ export class TinyMceEditor extends Component {
          */
         for (let i = tinymce.editors.length - 1; i > -1; i--) {
             let ed_id = tinymce.editors[i].id;
-            if (!(ed_id.includes('glossary') || ed_id.includes('footnote') || (this.props.element &&this.props.element.type && this.props.element.type==="figure"))) {
+            if (!(ed_id.includes('glossary') || ed_id.includes('footnote') || (this.props.element &&this.props.element.type && this.props.element.type==="figure" && this.props.element.figuretype!=="interactive"))) {
                 removeTinyDefaultAttribute(tinymce.activeEditor.targetElm)
                 tinymce.remove(`#${ed_id}`)
                 tinymce.$('.wrs_modal_desktop').remove();
