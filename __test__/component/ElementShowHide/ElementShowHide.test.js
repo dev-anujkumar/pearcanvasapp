1b5ec043c5541d2d82a70e211d7c951fd05dd2f3
Chavi Srivastava <chavi.srivastava@magicsw.com>
2020-01-31 17:31:40 +0530

    elementShowHide test cases added

--- __test__/component/ElementShowHide/ElementShowHide.test.js
+++ __test__/component/ElementShowHide/ElementShowHide.test.js
@@ -19,7 +19,7 @@ jest.mock('../../../src/component/tinyMceEditor.js',()=>{
 
 describe('Testing Element Show Hide component', () => {
 
-    test('renders without crashing', () => {
+    it('renders without crashing', () => {
         let props = {
             model:{},
             index:"" ,
@@ -43,4 +43,114 @@ describe('Testing Element Show Hide component', () => {
         let instance = component.instance(); 
         expect(instance).toBeDefined();
     })
-})
+    it('renders with props', () => {
+        let props = {
+            model:{},
+            index:"" ,
+            slateLockInfo: {
+                isLocked: false,
+                userId: 'c5Test01'
+            },
+            handleFocus: function(){},
+            permissions: [
+                "login", "logout", "bookshelf_access", "generate_epub_output", "demand_on_print", "toggle_tcm", "content_preview", "add_instructor_resource_url", "grid_crud_access", "alfresco_crud_access", "set_favorite_project", "sort_projects",
+                "search_projects", "project_edit", "edit_project_title_author", "promote_review", "promote_live", "create_new_version", "project_add_delete_users", "create_custom_user", "toc_add_pages", "toc_delete_entry", "toc_rearrange_entry", "toc_edit_title", "elements_add_remove", "split_slate", "full_project_slate_preview", "access_formatting_bar",
+                "authoring_mathml", "slate_traversal", "trackchanges_edit", "trackchanges_approve_reject", "tcm_feedback", "notes_access_manager", "quad_create_edit_ia", "quad_linking_assessment", "add_multimedia_via_alfresco", "toggle_element_page_no", "toggle_element_borders", "global_search", "global_replace", "edit_print_page_no", "notes_adding", "notes_deleting", "notes_delete_others_comment", "note_viewer", "notes_assigning", "notes_resolving_closing", "notes_relpying",
+            ],
+            handleBlur: jest.fn(),
+            handleFocus: jest.fn(),
+            accessDenied: jest.fn(),
+            element:showHide
+        }
+        const component = mount(<Provider store={store}><ElementShowHide {...props} /></Provider>)
+        expect(component).toHaveLength(1);
+        let instance = component.instance(); 
+        expect(instance).toBeDefined();
+    })
+    it('Test-deleteShowHideUnit function', () => {
+        let props = {
+            model:{},
+            index:"" ,
+            slateLockInfo: {
+                isLocked: false,
+                userId: 'c5Test01'
+            },
+            handleFocus: function(){},
+            permissions: [
+                "login", "logout", "bookshelf_access", "generate_epub_output", "demand_on_print", "toggle_tcm", "content_preview", "add_instructor_resource_url", "grid_crud_access", "alfresco_crud_access", "set_favorite_project", "sort_projects",
+                "search_projects", "project_edit", "edit_project_title_author", "promote_review", "promote_live", "create_new_version", "project_add_delete_users", "create_custom_user", "toc_add_pages", "toc_delete_entry", "toc_rearrange_entry", "toc_edit_title", "elements_add_remove", "split_slate", "full_project_slate_preview", "access_formatting_bar",
+                "authoring_mathml", "slate_traversal", "trackchanges_edit", "trackchanges_approve_reject", "tcm_feedback", "notes_access_manager", "quad_create_edit_ia", "quad_linking_assessment", "add_multimedia_via_alfresco", "toggle_element_page_no", "toggle_element_borders", "global_search", "global_replace", "edit_print_page_no", "notes_adding", "notes_deleting", "notes_delete_others_comment", "note_viewer", "notes_assigning", "notes_resolving_closing", "notes_relpying",
+            ],
+            handleBlur: jest.fn(),
+            handleFocus: jest.fn(),
+            accessDenied: jest.fn(),
+            element:showHide,
+            deleteShowHideUnit: ()=>{}
+        }
+        const component = mount(<Provider store={store}><ElementShowHide {...props} /></Provider>)
+        let instance = component.find('ElementShowHide').instance(); 
+        const spydeleteShowHideUnit = jest.spyOn(instance, 'deleteShowHideUnit')
+        instance.deleteShowHideUnit("urn:pearson:work:32e659c2-e0bb-46e8-9605-b8433aa38362",'show',"urn:pearson:entity:096be0c4-5f3d-4ae0-a5b7-52571b51dbc6",'1','0-0-1',"urn:pearson:manifest:1aa555b9-8fa4-480e-a6b2-40c46315a8eb");
+        expect(spydeleteShowHideUnit).toHaveBeenCalledWith("urn:pearson:work:32e659c2-e0bb-46e8-9605-b8433aa38362",'show',"urn:pearson:entity:096be0c4-5f3d-4ae0-a5b7-52571b51dbc6",'1','0-0-1',"urn:pearson:manifest:1aa555b9-8fa4-480e-a6b2-40c46315a8eb");
+        spydeleteShowHideUnit.mockClear()
+    })
+    it('Test-activeShowHide function', () => {
+        let props = {
+            model:{},
+            index:"" ,
+            slateLockInfo: {
+                isLocked: false,
+                userId: 'c5Test01'
+            },
+            handleFocus: function(){},
+            permissions: [
+                "login", "logout", "bookshelf_access", "generate_epub_output", "demand_on_print", "toggle_tcm", "content_preview", "add_instructor_resource_url", "grid_crud_access", "alfresco_crud_access", "set_favorite_project", "sort_projects",
+                "search_projects", "project_edit", "edit_project_title_author", "promote_review", "promote_live", "create_new_version", "project_add_delete_users", "create_custom_user", "toc_add_pages", "toc_delete_entry", "toc_rearrange_entry", "toc_edit_title", "elements_add_remove", "split_slate", "full_project_slate_preview", "access_formatting_bar",
+                "authoring_mathml", "slate_traversal", "trackchanges_edit", "trackchanges_approve_reject", "tcm_feedback", "notes_access_manager", "quad_create_edit_ia", "quad_linking_assessment", "add_multimedia_via_alfresco", "toggle_element_page_no", "toggle_element_borders", "global_search", "global_replace", "edit_print_page_no", "notes_adding", "notes_deleting", "notes_delete_others_comment", "note_viewer", "notes_assigning", "notes_resolving_closing", "notes_relpying",
+            ],
+            handleBlur: jest.fn(),
+            handleFocus: jest.fn(),
+            accessDenied: jest.fn(),
+            element:showHide,
+            deleteShowHideUnit: jest.fn()
+        }
+        let event={
+            currentTarget: {closest:jest.fn()}
+        }
+        const component = mount(<Provider store={store}><ElementShowHide {...props} /></Provider>)
+        let instance = component.find('ElementShowHide').instance(); 
+        const spyactiveShowHide = jest.spyOn(instance, 'activeShowHide')
+        instance.activeShowHide(event);
+        expect(spyactiveShowHide).toHaveBeenCalledWith(event) 
+        spyactiveShowHide.mockClear()
+    })
+    it('Test-createShowHideElement function', () => {
+
+        let props = {
+            model:{},
+            index:"" ,
+            slateLockInfo: {
+                isLocked: false,
+                userId: 'c5Test01'
+            },
+            handleFocus: function(){},
+            permissions: [
+                "login", "logout", "bookshelf_access", "generate_epub_output", "demand_on_print", "toggle_tcm", "content_preview", "add_instructor_resource_url", "grid_crud_access", "alfresco_crud_access", "set_favorite_project", "sort_projects",
+                "search_projects", "project_edit", "edit_project_title_author", "promote_review", "promote_live", "create_new_version", "project_add_delete_users", "create_custom_user", "toc_add_pages", "toc_delete_entry", "toc_rearrange_entry", "toc_edit_title", "elements_add_remove", "split_slate", "full_project_slate_preview", "access_formatting_bar",
+                "authoring_mathml", "slate_traversal", "trackchanges_edit", "trackchanges_approve_reject", "tcm_feedback", "notes_access_manager", "quad_create_edit_ia", "quad_linking_assessment", "add_multimedia_via_alfresco", "toggle_element_page_no", "toggle_element_borders", "global_search", "global_replace", "edit_print_page_no", "notes_adding", "notes_deleting", "notes_delete_others_comment", "note_viewer", "notes_assigning", "notes_resolving_closing", "notes_relpying",
+            ],
+            handleBlur: jest.fn(),
+            handleFocus: jest.fn(),
+            accessDenied: jest.fn(),
+            element:showHide,
+            deleteShowHideUnit: jest.fn(),
+            createShowHideElement:  jest.fn(),
+        }
+        const component = mount(<Provider store={store}><ElementShowHide {...props} /></Provider>)
+        let instance = component.find('ElementShowHide').instance(); 
+        const spycreateShowHideElement = jest.spyOn(instance, 'createShowHideElement')
+        instance.createShowHideElement('show',"0-1-0","urn:pearson:work:32e659c2-e0bb-46e8-9605-b8433aa3836c");
+        expect(spycreateShowHideElement).toHaveBeenCalledWith('show',"0-1-0","urn:pearson:work:32e659c2-e0bb-46e8-9605-b8433aa3836c");
+        spycreateShowHideElement.mockClear()
+    })
+})  
