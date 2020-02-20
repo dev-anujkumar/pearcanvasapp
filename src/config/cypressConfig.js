166d7f835eebf1e0ce306ed8266dcb07ccbbd2dd
Akansh <akansh.goel@pearson.com>
2020-02-20 18:22:58 +0530

    bg-1558

--- src/config/cypressConfig.js
+++ src/config/cypressConfig.js
@@ -30,7 +30,7 @@ let cypressConfig = {
     headingToolbar : ['italic','clearformatting','increaseindent','footnote','mathml','chemml','superscript','subscript','specialcharactor','undo','redo','assetpopover','slatetag'],
     codeListingToolbar: ['bold','italic','underline','strikethrough','clearformatting','increaseindent','decreaseindent','footnote','glossary','orderedlist','unorderedlist','mathml','chemml','inlinecode','superscript','subscript','specialcharactor','undo','redo','assetpopover','slatetag'],
     asideToolbar: ['bold','italic','underline','strikethrough','clearformatting','increaseindent','decreaseindent','footnote','glossary','orderedlist','unorderedlist','mathml','chemml','inlinecode','superscript','subscript','specialcharactor','undo','redo','assetpopover','slatetag'],
-    labelToolbar:['footnote','decreaseindent','glossary'],
+    labelToolbar:['footnote','decreaseindent','glossary','assetpopover'],
     captionToolbar:['decreaseindent','glossary'],
     APO_API_KEY : '7ij8zrLkCNR9DtAXx3KJ6yutoYnk4SAx',
     editorRefID:"",
