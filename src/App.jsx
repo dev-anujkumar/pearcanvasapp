6b7f5771b3421ea3a40aa4fe4895b81f9dc5a236
Gupta, Ankit <ankit.gupta1@pearson.com>
2020-02-22 04:36:15 -0600

    update canvas-0.6.18.14 tag

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 // IMPORT - Assets // 
 import './styles/style.css';
 
- console.log("!!!!! ---- canvas-0.6.18.13  ---- !!!!!!")
+ console.log("!!!!! ---- canvas-0.6.18.14  ---- !!!!!!")
 
 class App extends Component {
     constructor(props) {
