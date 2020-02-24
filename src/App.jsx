55e6fcfdf2f19e9afbc5b4ce11fc35daf0dc0a87
Gupta, Ankit <ankit.gupta1@pearson.com>
2020-02-24 09:59:16 -0600

    Updating Tag to canvas-0.7.22

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 // IMPORT - Assets // 
 import './styles/style.css';
 
- console.log("!!!!! ---- canvas-0.7.21  ---- !!!!!!")
+ console.log("!!!!! ---- canvas-0.7.22  ---- !!!!!!")
 
 class App extends Component {
     constructor(props) {
