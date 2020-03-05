83fce2e14f0b343e6abaa1b81d732360eb382e21
Kanika Arora <kanika.arora@magicsw.com>
2020-03-05 15:26:11 +0530

    fixes of assessments

--- src/js/utils.js
+++ src/js/utils.js
@@ -97,7 +97,8 @@ export const utils = {
                 format = "video-mcq";
                 break;
             default:
-                    format = "fpo";
+                format = "fpo";
+                break;
         }
         return format;
     },
