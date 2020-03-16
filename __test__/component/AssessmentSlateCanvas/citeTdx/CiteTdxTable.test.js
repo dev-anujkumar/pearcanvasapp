9229339cd991e2afa6593e8878fc82d5a9f60947
Kanika Arora <kanika.arora@magicsw.com>
2020-03-16 15:25:12 +0530

    fixes of test cases

---
+++ __test__/component/AssessmentSlateCanvas/citeTdx/CiteTdxTable.test.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { mount } from 'enzyme';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+import CiteTdxTable from '../../../../src/component/AssessmentSlateCanvas/assessmentCiteTdx/Components/CiteTdxTable/CiteTdxTable'
+let initialState = {
+    citeTdxReducer:{ 
+        currentAssessmentSelected:{
+            "versionUrn":"dfer",
+            "name":"mmoi"
+        },
+        citeApiData:{assessments:{"dfsarfw":"Sdfa"}},
+        tdxApiData:{assessments:{"dfsarfw":"Sdfa"}},
+        mmiApiData:{assessments:{"dfsarfw":"Sdfa"}},
+        isLoading:false,
+        currentSingleAssessmentSelected:{},
+        citeErrorFlag:"",
+        assessmenterrFlag:false
+
+ },
+};
+describe('Table component', () => {
+    let props = {
+        assessmentType:"Full Assessment CITE",
+        assessmenterrFlag:false,
+        isLoading:false,
+        citeApiData:{assessments:{"dfsarfw":"Sdfa"}},
+        tdxApiData:{assessments:{"dfsarfw":"Sdfa"}},
+        mmiApiData:{assessments:{"dfsarfw":"Sdfa"}},
+
+    }
+    let store = mockStore(initialState);
+    const component = mount(<Provider store={store}><CiteTdxTable {...props} /></Provider>);
+    let componentInstance = component.find('CiteTdxTable').instance();
+    const spyaddAssessment = jest.spyOn(componentInstance, 'addAssessment')
+
+    it('renders without crashing', () => {
+        expect(component).toHaveLength(1);
+    })
+
+    it('add assessment', () => {
+        componentInstance.addAssessment();
+        expect(spyaddAssessment).toHaveBeenCalled()
+        spyaddAssessment.mockClear()
+    })
+
+    it('add assessment', () => {
+        componentInstance.addAssessment();
+        expect(spyaddAssessment).toHaveBeenCalled()
+        spyaddAssessment.mockClear()
+    })
+
+    it('TDX', () => {
+        let props = {
+            assessmentType:"Full Assessment TDX",
+        assessmenterrFlag:false,
+        isLoading:false,
+        citeApiData:{assessments:{"dfsarfw":"Sdfa"}},
+        tdxApiData:{assessments:{"dfsarfw":"Sdfa"}},
+        mmiApiData:{assessments:{"dfsarfw":"Sdfa"}},
+    
+        }
+        let store = mockStore(initialState);
+        const component = mount(<Provider store={store}><CiteTdxTable {...props} /></Provider>);
+        expect(component).toHaveLength(1);
+    })
+    
+    
+    it('MMI', () => {
+        let props = {
+            assessmentType:"Full Assessment MMI",
+        assessmenterrFlag:false,
+        isLoading:false,
+        citeApiData:{assessments:{"dfsarfw":"Sdfa"}},
+        tdxApiData:{assessments:{"dfsarfw":"Sdfa"}},
+        mmiApiData:{assessments:{"dfsarfw":"Sdfa"}},
+    
+        }
+        let store = mockStore(initialState);
+        const component = mount(<Provider store={store}><CiteTdxTable {...props} /></Provider>);
+        expect(component).toHaveLength(1);
+    })
+
+});
