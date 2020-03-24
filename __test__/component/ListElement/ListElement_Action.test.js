cb7383cf3143cf120ba259daef2cf2d51b847860
Akanksha Shakya <VGUPT19>
2020-03-24 14:45:22 +0530

    ListElement_Action.js test cases

--- __test__/component/ListElement/ListElement_Action.test.js
+++ __test__/component/ListElement/ListElement_Action.test.js
@@ -14,7 +14,7 @@ jest.mock('../../../src/config/config.js', () => ({
 
 const initialState = {
     appStore: {
-        slateLevelData: slateData,
+        slateLevelData: slateData.SlateData1,
         activeElement: {
             elementId: "urn:pearson:work:8a49e877-144a-4750-92d2-81d5188d8e1a",
             elementType: "element-authoredtext",
@@ -26,19 +26,10 @@ const initialState = {
     }
 }
 describe('Test convertElement- PARAGRAPH to LIST', () => {
-    let store = mockStore(() => initialState);
     it('Test convertElement  -PARAGRAPH to LIST', () => {
-        store = mockStore(() => initialState);
-        let elementData = {
-            elementId: "urn:pearson:work:8a49e877-144a-4750-92d2-81d5188d8e1a",
-            elementType: "element-authoredtext",
-            labelText: "UL",
-            primaryOption: "primary-list",
-            secondaryOption: "secondary-list-1",
-        }
         let store = mockStore(() => initialState);
         const spyconvertToListElement = jest.spyOn(listAction, 'convertToListElement')
-        store.dispatch(listAction.convertToListElement('disc',true));
+        store.dispatch(listAction.convertToListElement('disc',1));
         expect(spyconvertToListElement).toHaveBeenCalled()
         spyconvertToListElement.mockClear()
     });
