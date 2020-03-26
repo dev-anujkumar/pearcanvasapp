cd54e46cbca05836a41f2c10b0cefa9278444edd
Ankit Aggarwal <ankit.aggarwal@pearson.com>
2020-03-26 17:19:09 +0530

    commit message update.

--- src/component/tinyMceEditor.js
+++ src/component/tinyMceEditor.js
@@ -1529,7 +1529,7 @@ export class TinyMceEditor extends Component {
         showHideType = showHideType === "revel" ? "postertextobject" : showHideType
 
         if(!this.fromtinyInitBlur && !config.savingInProgress){
-            let elemNode = document.getElementById(`cypress-${this.props.index}`)
+            let elemNode = document.getElementById(`cypress-${this.props.index}`) 
             elemNode.innerHTML = elemNode.innerHTML.replace(/<br data-mce-bogus="1">/g, "")
             if(this.props.element && this.props.element.type === "popup" && !this.props.currentElement && elemNode && elemNode.innerHTML !== ""){
                 this.props.createPopupUnit(this.props.popupField, forceupdate, this.props.index, this.props.element)
