b5a3186f5d2ffe1c5e917da3d76257c09d230160
Ankit Gupta <ankit.gupta1@pearson.com>
2020-04-01 18:42:32 +0530

    resolve cursor issue

--- src/component/ElementContainer/ElementContainer.jsx
+++ src/component/ElementContainer/ElementContainer.jsx
@@ -1157,7 +1157,7 @@ ElementContainer.defaultProps = {
 ElementContainer.propTypes = {
     /** Detail of element in JSON object */
     element: PropTypes.object,
-    elemBorderToggle: PropTypes.string
+    elemBorderToggle: PropTypes.bool
 }
 
 const mapDispatchToProps = (dispatch) => {
