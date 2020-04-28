2242dd8500ecaeb2bbe8b297610f6df70b4d2c6b
Kanika Arora <kanika.arora@magicsw.com>
2020-04-28 19:41:19 +0530

    fixes

--- src/component/AssessmentSlateCanvas/singleAssessmentCiteTdx/CiteTdxSingleAssessmentTable.jsx
+++ src/component/AssessmentSlateCanvas/singleAssessmentCiteTdx/CiteTdxSingleAssessmentTable.jsx
@@ -18,9 +18,10 @@ class CiteTdxTable extends Component {
 
     }
     componentDidMount(){
-        if(this.props.currentSingleAssessmentSelected){
+        if(this.props.currentSingleAssessmentSelected && this.props.currentSingleAssessmentSelected.versionUrn){
+            console.log("kanika")
             this.setState({
-                activeRow: this.props.currentSingleAssessmentSelected.versionUrn
+                activeRow: this.props.currentSingleAssessmentSelected.versionUrn ? this.props.currentSingleAssessmentSelected.versionUrn:""
             })
         }
        
