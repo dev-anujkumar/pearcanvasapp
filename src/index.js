3428fe8a9e85acb048c12ea7e116bd555cd475c6
Ankit Gupta <ankit.gupta1@pearson.com>
2020-05-01 17:12:44 +0530

    remove commented code

--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-// Needed for Hot Module Replacement //
-/* if (typeof (module.hot) !== 'undefined') {
-    module.hot.accept() // eslint-disable-line no-undef  
-} */
-
 ReactDOM.render(<App />, document.getElementById('root'));
