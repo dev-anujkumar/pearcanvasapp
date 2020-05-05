5d2857ced6dec11f13c6f8bd400576debf746178
Shivam Gupta <shivam.gupta1@pearson.com>
2020-05-05 21:03:42 +0530

    Major issue fixed

--- src/component/ElementContainer/ElementContainer.jsx
+++ src/component/ElementContainer/ElementContainer.jsx
@@ -486,7 +486,7 @@ class ElementContainer extends Component {
                         contentUrn : parentElement.contentUrn           
                     };
                 }
-                if((parentElement.type === "poetry" && previousElementData.type !== "stanza") || parentElement.type === "citations"){
+                if((parentElement.type === "poetry" && previousElementData.type !== "stanza" && !(parentElement.contents["creditsarray"] && parentElement.contents["creditsarray"].length && parentElement.contents.creditsarray[0].id === previousElementData.id)) || parentElement.type === "citations"){
                     let titleHTML = document.getElementById(`cypress-${this.props.index}-0`).innerHTML,
                         subtitleHTML = document.getElementById(`cypress-${this.props.index}-1`).innerHTML
                     if(parentElement.type === "poetry"){
@@ -499,7 +499,7 @@ class ElementContainer extends Component {
                     html = html.replace(/<br data-mce-bogus="1">/g, "<br>")
                     parentElement["index"] = this.props.index
                 }
-                else if(parentElement.type === "popup"){
+                else if(parentElement.type === "popup" || (parentElement.type === "poetry" && parentElement.contents["creditsarray"] && parentElement.contents["creditsarray"].length && parentElement.contents.creditsarray[0].id === previousElementData.id)){
                     tempDiv.innerHTML = matchHTMLwithRegex(tempDiv.innerHTML) ? tempDiv.innerHTML : `<p class="paragraphNumeroUno">${tempDiv.innerHTML}</p>`
                     html = html.replace(/<br data-mce-bogus="1">/g, "<br>")
                     html = matchHTMLwithRegex(html) ? html : `<p class="paragraphNumeroUno">${html}</p>`
