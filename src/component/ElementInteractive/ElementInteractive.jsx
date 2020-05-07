c1ea65e5f418f92e4e68a57641d46a8591439692
Kanika Arora <kanika.arora@magicsw.com>
2020-05-07 12:20:46 +0530

    revert MMI Change

--- src/component/ElementInteractive/ElementInteractive.jsx
+++ src/component/ElementInteractive/ElementInteractive.jsx
@@ -40,8 +40,7 @@ class Interactive extends React.Component {
             searchTitle : '',
             filterUUID : '',
             itemParentID: this.props.model.figuredata && this.props.model.figuredata.interactiveparentid ? this.props.model.figuredata.interactiveparentid : "",
-            openedFrom:'',
-            interactiveTitle: this.props.model.figuredata && this.props.model.figuredata.interactivetitle? this.props.model.figuredata.interactivetitle : "",
+
         };
 
     }
@@ -66,7 +65,6 @@ class Interactive extends React.Component {
             itemID : this.props.model.figuredata && this.props.model.figuredata.interactiveid ? this.props.model.figuredata.interactiveid : "",
             posterImage : this.props.model.figuredata && this.props.model.figuredata.posterimage && this.props.model.figuredata.posterimage.path ? this.props.model.figuredata.posterimage.path : "", 
             itemParentID: this.props.model.figuredata && this.props.model.figuredata.interactiveparentid ? this.props.model.figuredata.interactiveparentid : "",
-            interactiveTitle: this.props.model.figuredata && this.props.model.figuredata.interactivetitle? this.props.model.figuredata.interactivetitle : "",
         })
     }
     /**
@@ -82,7 +80,6 @@ class Interactive extends React.Component {
                 imagePath : nextProps.model.figuredata && nextProps.model.figuredata.posterimage && nextProps.model.figuredata.posterimage.path ? nextProps.model.figuredata.posterimage.path : "",
                 elementType: nextProps.model.figuredata.interactivetype || "",
                 itemParentID:nextProps.model.figuredata && nextProps.model.figuredata.interactiveparentid ? nextProps.model.figuredata.interactiveparentid : "",
-                interactiveTitle: nextProps.model.figuredata && nextProps.model.figuredata.interactivetitle? nextProps.model.figuredata.interactivetitle : "",
             };
         }
 
@@ -461,21 +458,19 @@ class Interactive extends React.Component {
             this.props.showBlocker(value);
             disableHeader(value);
             this.props.handleFocus();
-            if (this.state.itemParentID && this.state.itemID) {
-                this.props.setCurrentCiteTdx({
-                    "versionUrn": this.state.itemParentID,
-                    "name": this.state.interactiveTitle
+            if (this.state.assessmentId && this.state.assessmentItemId ) {
+                this.props.setCurrentCiteTdx({ 
+                    "versionUrn": this.state.itemParentID, 
                 });
-                this.props.setCurrentInnerCiteTdx({
+                this.props.setCurrentInnerCiteTdx({ 
                     "versionUrn": this.state.itemID
                 });
                 this.setState({
                     showSinglePopup: value,
                     setCurrentAssessment: {
                         id: this.state.itemParentID,
-                        title: this.state.interactiveTitle,
                     },
-                    openedFrom: 'singleAssessment'
+                    openedFrom:'singleAssessment'
                 });
             }
             else{
@@ -700,13 +695,9 @@ class Interactive extends React.Component {
     }
     assessmentNavigateBack = () => {
         this.props.setCurrentInnerCiteTdx({});
-        if(this.state.openedFrom === "singleAssessment"){
-            this.props.setCurrentCiteTdx({});
-        }
         this.setState({
             showAssessmentPopup: true,
             showSinglePopup:false,
-            openedFrom:''
         });
     }
 
@@ -744,7 +735,6 @@ class Interactive extends React.Component {
                let figureData = {
                    schema: "http://schemas.pearson.com/wip-authoring/interactive/1#/definitions/interactive",
                    interactiveparentid:citeTdxObj.id,
-                   interactivetitle:citeTdxObj.title,
                    interactiveid: citeTdxObj.singleAssessmentID.versionUrn,
                    interactivetype: tempInteractiveType,
                    interactiveformat: "mmi"
