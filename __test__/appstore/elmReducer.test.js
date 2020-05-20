1a8ca8ef8474574477c4f485dc24a020f41f607c
Chavi Srivastava <chavi.srivastava@magicedtech.com>
2020-05-20 16:01:38 +0530

    Test Cases for learnosity v2

--- __test__/appstore/elmReducer.test.js
+++ __test__/appstore/elmReducer.test.js
@@ -3,6 +3,8 @@ import reducer from '../../src/appstore/elmReducer';
 const INITIAL_STATE = {
     isLoading:true,
     elmLoading:true,
+    openSearch:false,
+    searchTerm:''
 }
 
 const mockELMData = {
@@ -18,43 +20,94 @@ describe('testing elm reducer cases --', () => {
         expect(reducer(undefined, {})).toEqual(INITIAL_STATE);
     });
     it('get elm resources from api', () => {
-        reducer(INITIAL_STATE, {
+        expect(reducer(INITIAL_STATE, {
             type: 'GET_ELM_RESOURCES',
             payload: {
-                elmData: mockELMData,
+                data: {},
                 errFlag: false,
-                apiStatus: 200
+                apiStatus: "200",
+                // elmLoading:false
             }
+        })).toEqual({
+            elmData: {},
+            errFlag: false,
+            apiStatus: "200",
+            elmLoading:true,
+            openSearch:false,
+            searchTerm:'',
+            isLoading: true,
         })
     })
     it('get elm items from api', () => {
-        reducer(INITIAL_STATE, {
+        expect(reducer(INITIAL_STATE, {
             type: 'GET_ELM_ITEMS',
             payload: {
-                elmData: mockElmItemData,
+                data: {},
                 errFlag: false,
                 apiStatus: 200,
-                isLoading:true
+                isLoading:false,
+                openSearch:false
             }
+        })).toEqual({
+            elmItemData:{},
+            itemErrorFlag:false,
+            itemApiStatus:200,
+            isLoading:false,
+            openSearch:false,
+            searchTerm:'',
+            elmLoading:true
         })
     })
-    it('set isLoading true', () => {
+    it('set SET_LOADING_TRUE true', () => {
         expect(reducer(INITIAL_STATE, {
             type: 'SET_LOADING_TRUE',
             payload: {
                 isLoading:true
             }
-        })).toEqual({   isLoading:true,
-            elmLoading:true,})
+        })).toEqual({
+            isLoading: true,
+            elmLoading: true,
+            openSearch: false,
+            searchTerm:''
+        })
     })
-    it('set elmLoading true', () => {
+    it('set SET_ELM_LOADING_TRUE true', () => {
         expect(reducer(INITIAL_STATE, {
             type: 'SET_ELM_LOADING_TRUE',
             payload: {
                 elmLoading:true
             }
-        })).toEqual({   isLoading:true,
-            elmLoading:true,})
+        })).toEqual({
+            isLoading: true,
+            elmLoading: true,
+            openSearch: false,
+            searchTerm:''
+        })
     })
-
+    it('set SET_SEARCH_FLAG true', () => {
+        expect(reducer(INITIAL_STATE, {
+            type: 'SET_SEARCH_FLAG',
+            payload: {
+                openSearch:true
+            }
+        })).toEqual({
+            elmLoading: true,
+            isLoading: true,
+            openSearch: true,
+            searchTerm:''
+        })
+    })
+    it('set SET_SEARCH_TERM true', () => {
+        expect(reducer(INITIAL_STATE, {
+            type: 'SET_SEARCH_TERM',
+            payload: {
+                searchTerm: 'test'
+            }
+        })).toEqual({
+            elmLoading: true,
+            isLoading: true,
+            openSearch: false,
+            searchTerm: 'test'
+        })
+    })
 });
