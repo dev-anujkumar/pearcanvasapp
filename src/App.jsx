9ea856c4818666e44f5e02ee24af41c3035bd703
Jena, Anuj <anuj.jena@pearson.com>
2020-05-26 01:03:12 -0500

    Updating tag to canvas-1.3.29

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 // IMPORT - Assets // 
 import './styles/style.css';
 
- console.log("!!!!!! ---- canvas-1.3.28 ---- !!!!!!")
+ console.log("!!!!!! ---- canvas-1.3.29 ---- !!!!!!")
 
 class App extends Component {
     constructor(props) {
