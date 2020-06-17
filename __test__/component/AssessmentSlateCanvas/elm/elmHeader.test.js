aa211406bc0ed05db64fa94cc277034f914f4574
Chavi Srivastava <chavi.srivastava@magicedtech.com>
2020-06-17 13:31:52 +0530

    Picker Header updated for ELM/Learnosity Assessments

--- __test__/component/AssessmentSlateCanvas/elm/elmHeader.test.js
+++ __test__/component/AssessmentSlateCanvas/elm/elmHeader.test.js
@@ -4,11 +4,20 @@ import { mount } from 'enzyme';
 import ElmHeader from ' ../../../src/component/AssessmentSlateCanvas/elm/Components/ElmHeader';
 
 describe('Testing ELM Header component', () => {
-    let props={
-        elmHeaderProps : 'Title',
-        closeElmWindow : function(){}
-    }
-    test('renders without crashing', () => {
+    it('renders without crashing', () => {
+        let props={
+            activeAssessmentType : 'learnosity',
+            closeElmWindow : function(){}
+        }
+        const component = mount(<ElmHeader {...props}/>)       
+        expect(component).toHaveLength(1);
+      
+    })
+    it('renders without crashing', () => {
+        let props={
+            activeAssessmentType : 'puf',
+            closeElmWindow : function(){}
+        }
         const component = mount(<ElmHeader {...props}/>)       
         expect(component).toHaveLength(1);
       
