fbd98a2f42fbb52c0426841567b309c6c343b60e
Jena, Anuj <anuj.jena@pearson.com>
2020-06-18 10:21:53 -0500

    Updating Tag To canvas-1.5.15

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 // IMPORT - Assets // 
 import './styles/style.css';
 
- console.log("!!!!!! ---- canvas-1.5.14 ---- !!!!!!")
+ console.log("!!!!!! ---- canvas-1.5.15 ---- !!!!!!")
 
 class App extends Component {
     constructor(props) {
