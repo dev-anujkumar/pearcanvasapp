dbc4d6fa2a573cfb5d2e087bc5526fb911f928ec
Akanksha Shakya <akanksha.shakya@pearson.com>
2020-07-10 20:03:52 +0530

    BG-2708

--- src/component/tinyMceEditor.js
+++ src/component/tinyMceEditor.js
@@ -538,7 +538,8 @@ export class TinyMceEditor extends Component {
 
             if(linkTitle == "Slate Link") {
                 let linkId = (e.target.attributes['id'] && e.target.attributes['id'].nodeValue) || e.target.parentNode.attributes['id'].nodeValue;
-                let elementId = (e.target.attributes['element-id'] && e.target.attributes['element-id'].nodeValue) || e.target.parentNode.attributes['element-id'].nodeValue;
+                let elementId = this.props.element && this.props.element.id
+                // (e.target.attributes['element-id'] && e.target.attributes['element-id'].nodeValue) || e.target.parentNode.attributes['element-id'].nodeValue;
                 let pageId = (e.target.attributes['data-uri'] && e.target.attributes['data-uri'].nodeValue) || e.target.parentNode.attributes['data-uri'].nodeValue;
 
                 sendDataToIframe({ 'type': LaunchTOCForCrossLinking, 'message': { open: true, case: 'update', link: linkId, element: elementId, page: pageId, blockCanvas: true, crossLink: true } });
