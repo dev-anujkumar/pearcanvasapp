7fd19cb5dd9d603c9bc06eb68fcf3ea9f89e6d70
Gupta, Ankit <ankit.gupta1@pearson.com>
2020-08-04 00:48:24 -0500

    update canvas-1.8.4 tag

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 import './styles/style.css';
 
 
-console.log("!!!!!! ---- canvas-1.8.3 ---- !!!!!!")
+console.log("!!!!!! ---- canvas-1.8.4 ---- !!!!!!")
 
 class App extends Component {
     constructor(props) {
