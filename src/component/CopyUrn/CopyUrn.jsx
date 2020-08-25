b717b3dfbbd67c597ba96eed5ff6a9d2c4e6e1ec
Anuj Jena <anuj.jena@magicsw.com>
2020-08-25 16:15:26 +0530

    Fixing issues - bg-3048, bg-3050

--- src/component/CopyUrn/CopyUrn.jsx
+++ src/component/CopyUrn/CopyUrn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CopyUrn = props => {
 
-    const copyToClipBoard = (e,text) => {
+    const copyToClipBoard = (e, text) => {
         e.stopPropagation();
         const tempElement = document.createElement('textarea');
         tempElement.value = text;
@@ -11,10 +11,21 @@ const CopyUrn = props => {
         document.execCommand('copy');
         document.body.removeChild(tempElement);
         props.toggleCopyMenu(false);
+        let linkNotification = document.getElementById('link-notification');
+        if(text.includes('work')) {
+            linkNotification.innerText = "Work URN copied to clipboard";
+        } else {
+            linkNotification.innerText = "Manifest URN copied to clipboard";
+        }
+        linkNotification.style.display = "block";
+        setTimeout(() => {
+            linkNotification.style.display = "none";
+            linkNotification.innerText = "";
+        }, 4000);
     }
 
     return (
-        <div style={{left : `${props.copyClickedX}px` , top : `${props.copyClickedY}px`}} className={'copyUrn'} onClick={(e) => { copyToClipBoard(e,props.elementId) }}>
+        <div style={{ left: `${props.copyClickedX}px`, top: `${props.copyClickedY}px` }} className={'copyUrn'} onClick={(e) => { copyToClipBoard(e, props.elementId) }}>
             Copy {props.elementId.includes('work') ? 'Work' : 'Manifest'} URN
         </div>
     )
