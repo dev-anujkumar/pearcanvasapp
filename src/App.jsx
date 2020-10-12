a84e31dd75389d3f12fed65d3f0fa25ca336a77d
Gupta, Ankit <ankit.gupta1@pearson.com>
2020-10-12 07:56:57 +0000

    App.jsx edited online with Bitbucket

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 import './styles/style.css';
 
 
-console.log("!!!!!! ---- canvas-1.13.13.1 ---- !!!!!!")
+console.log("!!!!!! ---- canvas-1.13.14 ---- !!!!!!")
 
 
 class App extends Component {
