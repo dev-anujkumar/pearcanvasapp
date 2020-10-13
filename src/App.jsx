1dbe00fd53833851eb0dbeff81a6b336aed581a0
Aggarwal, Ankit <ankit.aggarwal@pearson.com>
2020-10-13 14:53:01 +0000

    updated canvas-1.13.21 tag

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 import './styles/style.css';
 
 
-console.log("!!!!!! ---- canvas-1.13.20 ---- !!!!!!")
+console.log("!!!!!! ---- canvas-1.13.21 ---- !!!!!!")
 
 
 class App extends Component {
