21b98d0989ef2cd5e0412158f6c4a82b5e17037a
Jena, Anuj <anuj.jena@pearson.com>
2020-10-14 13:17:23 +0000

    Updating Tag To canvas-1.13.24

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 import './styles/style.css';
 
 
-console.log("!!!!!! ---- canvas-1.13.23 ---- !!!!!!")
+console.log("!!!!!! ---- canvas-1.13.24 ---- !!!!!!")
 
 
 class App extends Component {
