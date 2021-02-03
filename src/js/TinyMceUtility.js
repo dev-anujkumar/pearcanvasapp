5718c37bd9e0f2b1699da3bc8475e76b05884d77
Chavi Srivastava <chavi.srivastava@magicsw.com>
2021-02-03 20:57:38 +0530

    BG-3903 fixes

--- src/js/TinyMceUtility.js
+++ src/js/TinyMceUtility.js
@@ -22,9 +22,13 @@ const dataFromAlfresco = (data, editor, imageArgs) => {
     const imageID = `imageAssetContent:${uniqID}:${Math.floor(1000 + Math.random() * 9000)}`
     const imgData = `<img src=${epsURL} data-id="${imageID}" class="imageAssetContent" width="112" height="150" imageid="urn:pearson:alfresco:${uniqID}" alt="${altText}" longdescription="${longDesc}"/>`;
     if (imageArgs) {
-        let getImgNode = document.querySelector(`img[data-id="${imageArgs.id}"]`);
-        getImgNode.outerHTML = imgData;
-        imageArgs.handleBlur(null,true);
+        if (imageArgs.id && editor?.targetElm) {
+            let getImgNode = editor.targetElm.querySelector(`img[data-id="${imageArgs.id}"]`);
+            if (getImgNode) {
+                getImgNode.outerHTML = imgData;
+                imageArgs.handleBlur(null, true);
+            }
+        }
     }
     else {
         editor.insertContent(`${imgData}<br>`);
