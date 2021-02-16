434f92d28ce198790c266d4b38cda64d7799d1c5
Kazim Raja <kazim.raja@magicsw.com>
2021-02-16 19:20:12 +0530

    few more test case

--- __test__/component/OpenerElement/OpenerElement.test.js
+++ __test__/component/OpenerElement/OpenerElement.test.js
@@ -446,5 +446,121 @@ describe('Testing Opener component with props', () => {
           expect(componentDidMount).toHaveBeenCalled()
         })
       })
+    describe('testing handleClickOutside',()=>{
+        it('Test-handleClickOutside Function', () => {
+          const openerComponent = mount(<Provider store={store}><OpenerElement {...props} /></Provider>)
+          const OpenerInstance = openerComponent.find('OpenerElement').instance()
+          const handleClickOutside  = jest.spyOn(OpenerInstance, 'handleClickOutside')
+          OpenerInstance.handleClickOutside();
+          expect(handleClickOutside).toHaveBeenCalled()
+        })
+    })
+    describe('Test-handleC2GlobalCO function',()=>{
+        const props = {
+            slateLockInfo: {
+                isLocked: false,
+                userId: 'c5Test01'
+            },
+            element : openerElementData,
+            onClick : ()=>{},
+            permissions: [],
+            updateElement: ()=>{},
+            accessDenied: jest.fn(),
+            permissions: [
+                "login", "logout", "bookshelf_access", "generate_epub_output", "demand_on_print", "toggle_tcm", "content_preview", "add_instructor_resource_url", "grid_crud_access", "alfresco_crud_access", "set_favorite_project", "sort_projects",
+                "search_projects", "project_edit", "edit_project_title_author", "promote_review", "promote_live", "create_new_version", "project_add_delete_users", "create_custom_user", "toc_add_pages", "toc_delete_entry", "toc_rearrange_entry", "toc_edit_title", "elements_add_remove", "split_slate", "full_project_slate_preview", "access_formatting_bar",
+                "authoring_mathml", "slate_traversal", "trackchanges_edit", "trackchanges_approve_reject", "tcm_feedback", "notes_access_manager", "quad_create_edit_ia", "quad_linking_assessment", "add_multimedia_via_alfresco", "toggle_element_page_no", "toggle_element_borders", "global_search", "global_replace", "edit_print_page_no", "notes_adding", "notes_deleting", "notes_delete_others_comment", "note_viewer", "notes_assigning", "notes_resolving_closing", "notes_relpying",
+            ]
+        }
+        let globalAlfrescoPath = {
+            nodeRef: "ebaaf975-a68b-4ca6-9604-3d37111b847a",
+            "repoInstance": "https://staging.api.pearson.com/content/cmis/uswip-aws",
+            "repoName": "AWS US"
+        }
+        const openerComponent = mount(<OpenerElement {...props} />)
+        let openerElementInstance =   openerComponent.find('OpenerElement').instance()
+        it('handleC2GlobalCO-default case', () => {
+            const event = {
+                target:{
+                    tagName: "p"
+                },
+                stopPropagation() { }
+            }
+            const spyhandleC2GlobalCO = jest.spyOn(openerElementInstance, 'handleC2GlobalCO') 
+            openerElementInstance.handleC2GlobalCO(event);
+            openerElementInstance.forceUpdate();
+            openerComponent.update();
+            expect(spyhandleC2GlobalCO).toHaveBeenCalledWith(event)
+            spyhandleC2GlobalCO.mockClear()
+        }) 
+
+        it('handleC2GlobalCO-if case', () => {
+            const event = {
+                target:{
+                    tagName: "g"
+                },
+                stopPropagation() { }
+            }
+            const props = {
+                slateLockInfo: {
+                    isLocked: false,
+                    userId: 'c5Test01'
+                },
+                element : openerElementData,
+                onClick : ()=>{},
+                permissions: [],
+                updateElement: ()=>{},
+                accessDenied: jest.fn(),
+                permissions: [
+                    "login", "logout", "bookshelf_access", "generate_epub_output", "demand_on_print", "toggle_tcm", "content_preview", "add_instructor_resource_url", "grid_crud_access", "alfresco_crud_access", "set_favorite_project", "sort_projects",
+                    "search_projects", "project_edit", "edit_project_title_author", "promote_review", "promote_live", "create_new_version", "project_add_delete_users", "create_custom_user", "toc_add_pages", "toc_delete_entry", "toc_rearrange_entry", "toc_edit_title", "elements_add_remove", "split_slate", "full_project_slate_preview", "access_formatting_bar",
+                    "authoring_mathml", "slate_traversal", "trackchanges_edit", "trackchanges_approve_reject", "tcm_feedback", "notes_access_manager", "quad_create_edit_ia", "quad_linking_assessment", "add_multimedia_via_alfresco", "toggle_element_page_no", "toggle_element_borders", "global_search", "global_replace", "edit_print_page_no", "notes_adding", "notes_deleting", "notes_delete_others_comment", "note_viewer", "notes_assigning", "notes_resolving_closing", "notes_relpying",
+                ]
+            }
+            const openerComponent = mount(<OpenerElement {...props} />)
+            let openerElementInstance =   openerComponent.find('OpenerElement').instance()
+            openerElementInstance.forceUpdate();
+            openerComponent.update();
+            const spyhandleC2GlobalCO = jest.spyOn(openerElementInstance, 'handleC2GlobalCO') 
+            openerElementInstance.handleC2GlobalCO(event);
+            openerElementInstance.forceUpdate();
+            openerComponent.update();
+            expect(spyhandleC2GlobalCO).toHaveBeenCalledWith(event);
+            spyhandleC2GlobalCO.mockClear()
+        }) 
+
+        it('handleC2GlobalCO-if(1)-if(2) case', () => {
+            const event = {
+                target:{
+                    tagName: "g"
+                },
+                stopPropagation() { }
+            }
+            const props = {
+                slateLockInfo: {
+                    isLocked: false,
+                    userId: 'c5Test01'
+                },
+                element : openerElementData,
+                onClick : ()=>{},
+                updateElement: ()=>{},
+                accessDenied: jest.fn(),
+                permissions: [
+                    "login", "logout", "bookshelf_access", "generate_epub_output", "demand_on_print", "toggle_tcm", "content_preview", "add_instructor_resource_url", "grid_crud_access", "alfresco_crud_access", "set_favorite_project", "sort_projects",
+                    "search_projects", "project_edit", "edit_project_title_author", "promote_review", "promote_live", "create_new_version", "project_add_delete_users", "create_custom_user", "toc_add_pages", "toc_delete_entry", "toc_rearrange_entry", "toc_edit_title", "elements_add_remove", "split_slate", "full_project_slate_preview", "access_formatting_bar",
+                    "authoring_mathml", "slate_traversal", "trackchanges_edit", "trackchanges_approve_reject", "tcm_feedback", "notes_access_manager", "quad_create_edit_ia", "quad_linking_assessment", "add_multimedia_via_alfresco", "toggle_element_page_no", "toggle_element_borders", "global_search", "global_replace", "edit_print_page_no", "notes_adding", "notes_deleting", "notes_delete_others_comment", "note_viewer", "notes_assigning", "notes_resolving_closing", "notes_relpying",
+                ]
+            }
+            const openerComponent = mount(<OpenerElement {...props} />)
+            let openerElementInstance =   openerComponent.find('OpenerElement').instance()
+            config.GLOBAL_CO = globalAlfrescoPath
+            const spyhandleC2GlobalCO = jest.spyOn(openerElementInstance, 'handleC2GlobalCO') 
+            openerElementInstance.handleC2GlobalCO(event);
+            openerElementInstance.forceUpdate();
+            openerComponent.update();
+            expect(spyhandleC2GlobalCO).toHaveBeenCalledWith(event);
+            spyhandleC2GlobalCO.mockClear()
+        })
+    })
 
 })
