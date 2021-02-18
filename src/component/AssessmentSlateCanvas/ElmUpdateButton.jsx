0519a9b0be12e85845077874a572a4fb4e6d9145
Kapil Dev Arya <kapil.arya@magicsw.com>
2021-02-18 15:32:46 +0530

    changed the alignment of status and icon in elm int

--- src/component/AssessmentSlateCanvas/ElmUpdateButton.jsx
+++ src/component/AssessmentSlateCanvas/ElmUpdateButton.jsx
@@ -15,21 +15,26 @@ const ElmUpdateButton = (props) => {
     const setUpdateDiv = (assessment) => {
         let updateDiv;
         const { assessmentStatus, showUpdateStatus } = assessment
-        if (showUpdateStatus === true) {
-            const approveText = assessmentStatus == 'final' ? "Approved" : "Unapproved"
-            const approveIconClass = assessmentStatus == 'final' ? "enable" : "disable"
+        const approveText = assessmentStatus == 'final' ? "Approved" : "Unapproved"
+        const approveIconClass = assessmentStatus == 'final' ? "enable" : "disable"
+
+        if (showUpdateStatus === true) {  
             updateDiv = <div className="cls-display-flex">
-                { (elementType === ELM_INT) && <div className={`elm-status-div ${embeddedElmClass}`}>
+                {(elementType === ELM_INT) && <div className={`elm-status-div ${embeddedElmClass}`}>
+                        <p className="eml-int-status-text-tm">{approveText}</p>
                         <span className={"approved-button " + approveIconClass}>{approvedIcon}</span>
-                        <p className={"approved-button-text " + approveIconClass}>{approveText}</p>
                     </div>
                 }
                 <div className={`elm-update-button ${embeddedElmClass}`} onClick={updateElmVersion}><b className='elm-update-button-text'>{buttonText}</b></div>
             </div>       
         } else {
-            const approveText = assessmentStatus == 'final' ? "Approved" : "Unapproved"
-            const approveIconClass = assessmentStatus == 'final' ? "enable" : "disable"
-            updateDiv = <div className={`elm-status-div ${embeddedElmClass}`}><span className={"approved-button " + approveIconClass}>{approvedIcon}</span><p className={"approved-button-text " + approveIconClass}>{approveText}</p></div>
+            updateDiv = (elementType === ELM_INT) ? 
+                <div className={`elm-status-div ${embeddedElmClass}`}>
+                    <p className="eml-int-status-text-tm">{approveText}</p>
+                    <span className={"approved-button " + approveIconClass}>{approvedIcon}</span>
+                </div> :
+                <div className={`elm-status-div ${embeddedElmClass}`}><span className={"approved-button " + approveIconClass}>{approvedIcon}</span><p className={"approved-button-text " + approveIconClass}>{approveText}</p></div>
+        
         }
         return updateDiv
     }
