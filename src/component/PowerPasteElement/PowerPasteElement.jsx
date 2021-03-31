c97432c455b3f7dc40146b55c8a7a365852e760d
Rahul Maheshwari <rahul.maheshwari@magicedtech.com>
2021-03-31 18:32:10 +0530

    Editor css applied from external file

--- src/component/PowerPasteElement/PowerPasteElement.jsx
+++ src/component/PowerPasteElement/PowerPasteElement.jsx
@@ -11,6 +11,7 @@ import "tinymce/plugins/powerpaste/plugin.min.js"
 import "tinymce/plugins/powerpaste/js/wordimport.js"
 // import "tinymce/plugins/paste";
 import './../../styles/ElementAuthoring/ElementAuthoring.css';
+import { powerpaste_list_content_style } from '../../config/PowerPasteListElementCss';
 
 const PowerPasteElement = (props) => {
 
@@ -31,25 +32,7 @@ const PowerPasteElement = (props) => {
   }, [])
 
   const editorConfig = {
-    content_style: 'ol, ul { margin-left: 2em !important; margin-bottom: 1em !important; padding-left: 1em !important; }' +
-                 'ol li, ul li {list-style-type: none !important; font-family: Georgia, serif, Palatino, Palatino Linotype, Palatino LT STD !important; font-weight: normal; line-height: 1.90em; letter-spacing: 0.01em; margin-left: 0em !important; margin-bottom: .65em !important; color: black !important; font-size: 1em !important; } ' +
-                 'ol, li ol, ul, li ul {counter-reset: section !important; }' +
-                 'ol li.listItemNumeroUnoUpperAlpha::before {counter-increment: section; content: counter(section, upper-alpha)". "; } ' +
-                 'ol li.listItemNumeroUnoLowerAlpha::before {counter-increment: section; content: counter(section, lower-alpha)". "; } ' +
-                 'ol li.listItemNumeroUnoNumber::before {counter-increment: section; content: counter(section, decimal)". "; } ' +
-                 'ol li.listItemNumeroUnoUpperRoman::before {counter-increment: section; content: counter(section, upper-roman)". "; } ' +
-                 'ol li.listItemNumeroUnoLowerRoman::before {counter-increment: section; content: counter(section, lower-roman)". "; } ' +
-                 'li.listItemNumeroUnoBullet {color: #000; font-family: Georgia, serif, Palatino, Palatino Linotype, Palatino LT STD !important; font-size: 1.180em; font-weight: normal; line-height: 1.70em; letter-spacing: 0.01em; margin-top: 0em; margin-bottom: 0.5em; } ' +
-                 'li.listItemNumeroUnoBullet::before {content: counter(item) ". "; counter-increment: item; font-weight: bold; padding-right: 0.5em; text-align: right; position: absolute; width: 10px; transform: translateX(-100%); } ' +
-                 'ul li.listItemNumeroUnoBullet::before {counter-increment: section; content: counter(section, disc); font-size: 1.6rem; } ' +
-                 '.disabledListBtn { background: #b3d8e1!important; cursor: not-allowed; pointer-events: none; }' +
-                 'span.list-opt-tooltip {position: absolute;  padding: 0 8px; border-radius: 2px; background: #222; -moz-border-radius: 2px; -webkit-border-radius: 2px; -moz-background-clip: padding; -webkit-background-clip: padding-box; background-clip: padding-box; -webkit-box-shadow: 0 3px 6px rgba(0, 0, 0, .16), 0 2px 2px 1px rgba(0, 0, 0, .14); -moz-box-shadow: 0 3px 6px rgba(0, 0, 0, .16), 0 2px 2px 1px rgba(0, 0, 0, .14); box-shadow: 0 3px 6px rgba(0, 0, 0, .16), 0 2px 2px 1px rgba(0, 0, 0, .14); color: #fff; font-size: 11px; line-height: 22px; font-family: Arial, Helvetica, sans-serif; transition: opacity .2s ease 0s; -webkit-transition: opacity .2s ease 0s; -moz-transition: opacity .2s ease 0s; -ms-transition: opacity .2s ease 0s; -o-transition: opacity .2s ease 0s; -webkit-opacity: 0; -moz-opacity: 0; opacity: 0; -ms-filter: "alpha(Opacity=0)"; left: 50%; user-select: none; -o-user-select: none; -moz-user-select: none; -khtml-user-select: none; -webkit-user-select: none; -ms-user-select: none; text-rendering: optimizelegibility; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; bottom: 0; transform: translateY(calc(100% + 5px)) translateX(-50%); z-index: 1; }' +
-                 '#listDropWrapper .list-options {position: relative; }' +
-                 '#listDropWrapper .list-options:hover>.list-opt-tooltip {opacity: 1; }' +
-                 '.element-container li {min-height: 22px; }' +
-                 'table ul li {color: inherit !important; }' +
-                 'table ol li {color: inherit !important; }' +
-                 'table ol li:before {color: inherit !important; }',
+    content_style: powerpaste_list_content_style,
     height: 420,
     plugins: [
       'advlist lists',
