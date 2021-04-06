3cb05eb4f5cbc0dcc30371b7f75a812eb361c112
sparsh <sparshgr8@yahoo.com>
2021-04-06 11:48:17 +0530

    Line No. inside multi column

--- src/component/ElementContainer/ElementContainerUpdate_helpers.js
+++ src/component/ElementContainer/ElementContainerUpdate_helpers.js
@@ -101,6 +101,8 @@ export const updateElementInStore = (paramsObj) => {
             ...updatedData,
             elementdata: {
                 ...element.elementdata,
+                startNumber: updatedData.elementdata ? updatedData.elementdata.startNumber : null,
+                numberedlines: updatedData.elementdata ? updatedData.elementdata.numberedlines : null,
                 text: updatedData.elementdata ? updatedData.elementdata.text : null
             },
             tcm: _slateObject.tcm ? true : false
