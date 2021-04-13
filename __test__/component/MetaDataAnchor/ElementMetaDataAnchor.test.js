f4b52691f5647c78609dfd5487a873e1cf2d00c2
Chavi Srivastava <chavi.srivastava@magicsw.com>
2021-04-13 11:39:26 +0530

    PCAT-9522 test cases

--- __test__/component/MetaDataAnchor/ElementMetaDataAnchor.test.js
+++ __test__/component/MetaDataAnchor/ElementMetaDataAnchor.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import config from '../../../src/config/config';
 import ElementMetaDataAnchor from '../../../src/component/ElementMetaDataAnchor/ElementMetaDataAnchor';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
@@ -16,7 +17,8 @@ const store = mockStore({
         currentSlateLOData: {
             "lourn": "urn:5567809876",
             "label": { en: "test data" }
-        }
+        },
+        currentSlateLF: 'cypressLF'
     }
 });
 let props = {
@@ -32,12 +34,13 @@ let props = {
     handleFocus: jest.fn(),
     showBlocker: jest.fn()
 }
-let wrapper = mount(<Provider store={store}><ElementMetaDataAnchor  {...props} /> </Provider>)
-let elementMetaAnchorInstance = wrapper.find('ElementMetaDataAnchor').instance();
 
 //Rendering component
+/** Test cases for Cypress LO */
 describe('Test Rendering of metadaanchor on slate', () => {
-
+    let wrapper = mount(<Provider store={store}><ElementMetaDataAnchor  {...props} /> </Provider>)
+    let elementMetaAnchorInstance = wrapper.find('ElementMetaDataAnchor').instance();
+    
     it('render component', () => {
         expect(wrapper.find('ElementMetaDataAnchor')).toHaveLength(1);
     })
@@ -62,4 +65,108 @@ describe('Test Rendering of metadaanchor on slate', () => {
         elementMetaAnchorInstance.prepareLOData(loData);
     })
 
+});
+
+/** Test cases for External LO */
+describe('External LO -Test Rendering of metadaanchor on slate', () => {
+    const newStore = mockStore({
+        metadataReducer: {
+            currentSlateLOData: [{
+                "lourn": "urn:5567809876",
+                "label": { en: "test data" }
+            }],
+            currentSlateLF: 'externalLF',
+            projectLearningFrameworks: {
+                externalLF: [{ urn: "external:urn" }]
+            }
+        }
+    });
+    let externalLOprops = {
+        slateLockInfo: {
+            isLocked: false
+        },
+        permissions: ['lo_edit_metadata'],
+        currentSlateLOData: [{
+            "lourn": "urn:5567809876",
+            "label": { en: "test data" }
+        }],
+        model: { text: "<p>Meatada</p>" },
+        handleFocus: jest.fn(),
+        showBlocker: jest.fn(),
+        element: { elementdata: { loref: "urn:5567809876" } }
+    }
+    let wrapper = mount(<Provider store={newStore}><ElementMetaDataAnchor  {...externalLOprops} /> </Provider>)
+    let elementMetaAnchorInstance = wrapper.find('ElementMetaDataAnchor').instance();
+    it('render component', () => {
+        expect(wrapper.find('ElementMetaDataAnchor')).toHaveLength(1);
+    })
+    it('onLOClickHandle config.editorRefID == e.target.id', () => {
+        config.editorRefID = "aefeqrwq"
+        let data =
+            [{
+                "lourn": "urn:5567809876",
+                "label": { en: "test data" }
+            }]
+        let event = { target: { id: "aefeqrwq" }, stopPropagation: jest.fn() }
+        elementMetaAnchorInstance.onLOClickHandle(props.currentSlateLOData, event)
+        expect(elementMetaAnchorInstance.props.currentSlateLOData).toEqual(data);
+    })
+    it('onLOClickHandle ', () => {
+        let data =
+            [{
+                "lourn": "urn:5567809876",
+                "label": { en: "test data" }
+            }]
+        let event = { target: { id: "aefeqrwq" }, stopPropagation: jest.fn() }
+        elementMetaAnchorInstance.onLOClickHandle(data, event)
+        expect(elementMetaAnchorInstance.props.currentSlateLOData).toEqual(data);
+    })
+    it('on call of prepareLOData', () => {
+        let loData = {
+            "lourn": "urn:5567809876",
+            "label": { en: "test data" }
+        };
+        let div = document.createElement('div');
+        div.setAttribute('class', 'learningObjectiveinnerText');
+        document.body.appendChild(div);
+        const spyFunction = jest.spyOn(elementMetaAnchorInstance, 'prepareLOData');
+        elementMetaAnchorInstance.prepareLOData(loData);
+        expect(spyFunction).toHaveBeenCalled();
+        spyFunction.mockClear();
+    })
+    it("externalLF list not present", () => {
+        const newStore1 = mockStore({
+            metadataReducer: {
+                currentSlateLOData: [{
+                    "lourn": "urn:5567809876",
+                    "label": { en: "test data" }
+                }],
+                currentSlateLF: 'externalLF',
+            }
+        });
+        let externalLOprops2 = {
+            slateLockInfo: {
+                isLocked: false
+            },
+            permissions: ['lo_edit_metadata'],
+            model: { text: "<p>Meatada</p>" },
+            handleFocus: jest.fn(),
+            showBlocker: jest.fn(),
+            element: { elementdata: { loref: "urn:5567809876" } }
+        }
+        let wrapper2 = mount(<Provider store={newStore1}><ElementMetaDataAnchor  {...externalLOprops2} /> </Provider>)
+        let elementMetaAnchorInstance2 = wrapper2.find('ElementMetaDataAnchor').instance();
+        expect(wrapper2.find('ElementMetaDataAnchor')).toHaveLength(1);
+        let loData = {
+            "loUrn": "urn:5567809876",
+            "label": { en: "test data" }
+        };
+        let div = document.createElement('div');
+        div.setAttribute('class', 'learningObjectiveinnerText');
+        document.body.appendChild(div);
+        const spyFunction = jest.spyOn(elementMetaAnchorInstance2, 'prepareLOData');
+        elementMetaAnchorInstance2.prepareLOData(loData);
+        expect(spyFunction).toHaveBeenCalled();
+        spyFunction.mockClear();
+    })
 });
