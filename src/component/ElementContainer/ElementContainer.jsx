efce8202c9babc7e55928901493b4a7fb20a46aa
Kapil Dev Arya <kapil.arya@magicsw.com>
2021-05-07 14:47:14 +0530

    minor fix

--- src/component/ElementContainer/ElementContainer.jsx
+++ src/component/ElementContainer/ElementContainer.jsx
@@ -1825,7 +1825,7 @@ class ElementContainer extends Component {
      */
     handleEditButton = (event) => {
         event.stopPropagation();
-        
+        const { element } = this.props;
         // const Url = `${config.ALFRESCO_EDIT_ENDPOINT}${metadataUrl}`
         const Url  = `https://alfresco-qa.cms.pearson.com/share/page/site/r34-pdf-splitter/document-details?nodeRef=workspace://SpacesStore/736b9be5-acbd-4344-b83f-fbd1a099967a&parentNodeRef=workspace://SpacesStore/2c239df5-01ae-4f66-a5f8-29e7552b10eb&path=%252F`
         if(element?.type === 'figure' && element.figuretype === 'image'){
