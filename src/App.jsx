a2773b938b039ac63cbaa365d636de0f7095dff7
Srivastava, Chavi <chavi.srivastava@pearson.com>
2021-05-28 07:08:16 +0000

    update tag canvas-1.23.17.05

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import './styles/style.css';
 
 
 
-console.log("!!!!! ---- canvas-1.23.17.04 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.23.17.05 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
