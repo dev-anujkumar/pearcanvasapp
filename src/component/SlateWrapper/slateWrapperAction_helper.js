03d443ed3fa5ca631c6e866cff5e8db51ae1c434
Kapil Dev Arya <abc@gmail.com>
2021-06-02 16:44:39 +0530

    fixed the issue of cut/copy elements  inside 2c:we

--- src/component/SlateWrapper/slateWrapperAction_helper.js
+++ src/component/SlateWrapper/slateWrapperAction_helper.js
@@ -29,10 +29,12 @@ export const onPasteSuccess = async (params) => {
     replaceWirisClassAndAttr(activeEditorId)
     // Store Update on Paste Element
     let operationType = '';
+    let sourceElementIndex = "";
     let elmSelection = {};
     if (Object.keys(getState().selectionReducer.selection).length > 0 && 'operationType' in getState().selectionReducer.selection) {
         elmSelection = getState().selectionReducer.selection;
         operationType = elmSelection.operationType;
+        sourceElementIndex = elmSelection?.sourceElementIndex;
     }
 
     /** Create Snapshot for cut action on different slate */
@@ -160,22 +162,14 @@ export const onPasteSuccess = async (params) => {
                 })
             } else if(asideData?.parent?.type === "groupedcontent" && item.id === asideData?.parent?.id) {
                 /* 2C:ASIDE/WE:Elements; Update the store */
-                //item?.groupeddata?.bodymatter?.map((item_L2) => {
-                //    item_L2?.groupdata?.bodymatter?.map(item_L3 => {
-                //        if (item_L3?.id === parentUrn.manifestUrn) {
-                //            item_L3?.elementdata?.bodymatter?.splice(cutIndex, 0, responseData);
-                //        } else if(item_L3?.subtype === "workedexample"){
-                //            item_L3?.elementdata?.bodymatter?.map(item_L4 => {
-                //                if (item_L4?.id === parentUrn.manifestUrn) {
-                //                    item_L4?.contents?.bodymatter?.splice(cutIndex, 0, responseData);
-                //                }
-                //            })
-                //        }
-                //    })
-                //})
                 const indexs = asideData?.index?.split("-") || [];
                 if(indexs.length === 3) { /* Inside 2C:AS; COPY-PASTE elements */
-                    item?.groupeddata?.bodymatter[indexs[1]]?.groupdata?.bodymatter[indexs[2]]?.elementdata?.bodymatter?.splice(cutIndex, 0, responseData)
+                    const selcetIndex = sourceElementIndex.split("-") || [];
+                    if(asideData?.subtype === "workedexample" && parentUrn?.elementType === "manifest" && selcetIndex.length === 5 ) {
+                        item?.groupeddata?.bodymatter[selcetIndex[1]]?.groupdata?.bodymatter[selcetIndex[2]]?.elementdata?.bodymatter[selcetIndex[3]]?.contents.bodymatter?.splice(cutIndex, 0, responseData);
+                    } else {
+                        item?.groupeddata?.bodymatter[indexs[1]]?.groupdata?.bodymatter[indexs[2]]?.elementdata?.bodymatter?.splice(cutIndex, 0, responseData)
+                    }
                 }
             }
         })
