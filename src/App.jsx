87420c68ff5a1a5fd70efa13dad249192ee0f328
Gupta, Ankit <ankit.gupta1@pearson.com>
2021-06-21 12:33:14 +0000

    update canvas-1.25.02.01 tag

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.25.03 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.25.02.01 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
