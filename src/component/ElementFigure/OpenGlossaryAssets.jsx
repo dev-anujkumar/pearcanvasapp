e0e4e7db78b587e2a6baa5617c2926c5047d311e
Rahul Maheshwari <rahul.maheshwari@magicedtech.com>
2021-06-23 20:30:47 +0530

    OpenAudio book import removed, wip

--- src/component/ElementFigure/OpenGlossaryAssets.jsx
+++ src/component/ElementFigure/OpenGlossaryAssets.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { hideTocBlocker } from '../../js/toggleLoader';
 import { audioNarrationCloseIcon } from '../../images/TinyMce/TinyMce.jsx'
 import AddFigureImage from './AddFigureImage.jsx';
-import OpenAudioBook from '../AudioNarration/OpenAudioBook.jsx';
+import AddAudioBook from '../AudioNarration/AddAudioBook.jsx';
 import { hasReviewerRole } from '../../constants/utility.js'
 import '../../styles/AudioNarration/AudioNarration.css';
 /**
@@ -14,7 +14,8 @@ class OpenGlossaryAssets extends Component {
     constructor(props) {
         super(props);
         this.state ={
-            replaceToggle:false,
+            replaceAudioToggle: false,
+            replaceImageToggle: false,
             tabValue: ""
         }
     }
@@ -22,8 +23,8 @@ class OpenGlossaryAssets extends Component {
     /**
     * @description - openConfirmationBox function responsible for opening confirmation popupfor removing the figure image.
     */
-    openConfirmationBox = (isGlossary) => {
-        this.props.showAudioRemovePopup(true,isGlossary)
+     openAudioConfirmationBox = (isGlossary) => {
+        this.props.showAudioRemovePopup(true, isGlossary)
     }
 
 
@@ -40,17 +41,23 @@ class OpenGlossaryAssets extends Component {
     /**
     * @description - handleClick function responsible for closing the dropdown whenever clicked outside.
     */
-    // handleClick = (e) => {
-    //     // alert("1");
-    //     if (this.node?.contains(e.target)) {
-    //         return;
-    //     }
-    //     this.props.closeAssetsPopup()
-    // }
+    handleClick = (e) => {
+        alert("handleclickkkkkkkkkkkkkkkkkkk", this.state.tabValue, this.node?.contains(e.target));
+        if (this.node?.contains(e.target)) {
+            return;
+        }
+        this.props.closeAssetsPopup();
+    }
 
-    handleReplaceButton =()=>{
+    handleReplaceAudioButton = () => {
         this.setState({
-            replaceToggle:!this.state.replaceToggle
+            replaceAudioToggle: !this.state.replaceAudioToggle
+        })
+    }
+
+    handleReplaceImageButton = () => {
+        this.setState({
+            replaceImageToggle: !this.state.replaceImageToggle
         })
     }
 
@@ -68,19 +75,21 @@ class OpenGlossaryAssets extends Component {
 
     render = () => {
         const {  figureGlossaryData, audioGlossaryData, position } = this.props;
-        const { tabValue, replaceToggle } = this.state;
-        let mediaSrc = "";
-        let mediaTitle = "";
+        const { tabValue, replaceAudioToggle, replaceImageToggle } = this.state;
+        let imageMediaSrc, imageMediaTitle, audioMediaSrc, audioMediaTitle = "";
         
         if (figureGlossaryData && Object.keys(figureGlossaryData).length > 0) {
-            mediaSrc = figureGlossaryData.path;
-            mediaTitle = figureGlossaryData.imageid;
+            imageMediaSrc = figureGlossaryData.path;
+            imageMediaTitle = figureGlossaryData.imageid;
+        }
+        if (audioGlossaryData && Object.keys(audioGlossaryData).length > 0) {
+            audioMediaSrc = audioGlossaryData.location;
+            audioMediaTitle = audioGlossaryData.title.en;
         }
         
         return (
-            <div className={'glossary-figuredropdown'} style={this.props.position} id='openFigureGlossary' >
-                <div className="audio-close">
-                    <div className="tabs-container">
+            <div className={'glossary-figuredropdown'} style={position} id='openFigureGlossary' ref={node => this.node = node} onBlur={() => this.handleClick(this)}>
+                <div className="tabs-container">
                         {
                             audioGlossaryData && Object.keys(audioGlossaryData).length > 0 &&
                             <div onClick={() => this.handleTab('audio')} className={`tabs ${tabValue === 'audio' ? "active-tabs" : ""}`}>Audio</div>
@@ -90,40 +99,64 @@ class OpenGlossaryAssets extends Component {
                             <div onClick={() => this.handleTab('image')} className={`tabs ${tabValue === 'image' ? "active-tabs" : ""}`}>Image</div>
                         }
                     </div>
-                </div>
+                {/* <div className="audio-close">
+                    <span className="close-icon-image" onClick={() => this.props.closeAssetsPopup()}>{audioNarrationCloseIcon}</span>
+                </div> */}
 
                 {
                     audioGlossaryData && Object.keys(audioGlossaryData).length > 0 &&
                     <div className={tabValue === 'audio' ? "showconetnt" : "content"}>
-                        <OpenAudioBook isGlossary ={true} closeAudioBookDialog={this.props.closeAssetsPopup} />
+                        <h2 className="audio-book-text">Audio Book</h2>
+                        <figure>
+                            <figcaption className="media-title">{audioMediaTitle}</figcaption>
+                            <audio
+                                controls
+                                controlsList="nodownload"
+                                src={audioMediaSrc}>
+                                Your browser does not support the
+                                <code>audio</code> element.
+                            </audio>
+                        </figure>
+
+                        {replaceAudioToggle && <AddAudioBook isGlossary={true} closeAddAudioBook={this.closeAddAudioBook} />}
+
+                        <div className="remove-button">
+                            {!hasReviewerRole() &&
+                                <button className="remove-text" onClick={() => this.openAudioConfirmationBox(true)} className="audioRemoveButton audioRemoveRound">Remove</button>
+                            }
+                            {
+                                <button className="remove-text" onClick={() => this.handleReplaceAudioButton()} className="audioReplaceeButton audioRemoveRound">Replace</button>
+                            }
+                        </div>
+
                     </div>
                 }
 
                 {
                     figureGlossaryData && Object.keys(figureGlossaryData).length > 0 &&
                     <div className={tabValue === 'image' ? "showconetnt" : "content"}>
-                        {/* <span className="close-icon-image" onClick={() => this.props.closeAssetsPopup()}>{audioNarrationCloseIcon}</span> */}
+                        <h2 className="audio-book-text">Image Book</h2>
                         <figure>
-                            <figcaption className="media-title">{mediaTitle}</figcaption>
-                            <img src={mediaSrc}
-                                data-src={mediaSrc}
-                                title=""
-                                alt=""
-                                width='300px'
-                                height='113px'
+                            <figcaption className="media-title">{imageMediaTitle}</figcaption>
+                            <img src={imageMediaSrc}
+                                data-src={imageMediaSrc}
+                                title = ""
+                                alt = ""
+                                width = '300px'
+                                height = '113px'
                                 // className={imageDimension + ' lazyload'}
-                                draggable="false" />
+                                draggable = "false" />
                         </figure>
 
-                        {replaceToggle && <AddFigureImage isGlossary={true} closeFigurePopup={this.closeFigurePopup} />}
+                        {replaceImageToggle && <AddFigureImage isGlossary={true} closeFigurePopup={this.closeFigurePopup} />}
 
                         <div className="remove-button">
 
                             {!hasReviewerRole() &&
-                                <button className="remove-text" onClick={() => this.openConfirmationBox(isGlossary)} className="audioRemoveButton audioRemoveRound">Remove</button>
+                                <button className="remove-text" onClick={() => this.openConfirmationBox(true)} className="audioRemoveButton audioRemoveRound">Remove</button>
                             }
                             {
-                                <button className="remove-text" onClick={() => this.handleReplaceButton()} className="audioReplaceeButton audioRemoveRound">Replace</button>
+                                <button className="remove-text" onClick={() => this.handleReplaceImageButton()} className="audioReplaceeButton audioRemoveRound">Replace</button>
                             }
                         </div>
                     </div>
