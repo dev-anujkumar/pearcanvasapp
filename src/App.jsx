a77feefb23f490795c281b7dadf56d490258616a
sparsh <sparshgr8@yahoo.com>
2021-06-28 14:21:50 +0530

    Version Update

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.25.08 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.25.09 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
