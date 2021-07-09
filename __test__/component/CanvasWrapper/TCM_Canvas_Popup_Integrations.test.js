39f742a8ea86336b04a27c96638af036d88f2107
Jaya Singh <jaya.singh@magicsw.com>
2021-07-09 15:02:46 +0530

    added changes related to pending and accepted data

--- __test__/component/CanvasWrapper/TCM_Canvas_Popup_Integrations.test.js
+++ __test__/component/CanvasWrapper/TCM_Canvas_Popup_Integrations.test.js
@@ -1,4 +1,3 @@
-import FetchAllDataMapper from '../../../src/component/TcmSnapshots/FetchAllDataMapper/FetchTcmDataMapper'
 import axios from "axios"
 import { handleTCM, tcmButtonHandler, closeTcmPopup } from '../../../src/component/CanvasWrapper/TCM_Canvas_Popup_Integrations';
 import thunk from 'redux-thunk';
@@ -7,7 +6,8 @@ const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 let initialState = {}
 let store = mockStore(() => initialState);
-const element = {id: "urn:pearson:work:c5f26743-d2d2-4664-b0df-9ac59e11642b"}
+const element = {"id":"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45","type":"element-authoredtext","schema":"http://schemas.pearson.com/wip-authoring/element/1","elementdata":{"schema":"http://schemas.pearson.com/wip-authoring/authoredtext/1#/definitions/authoredtext","text":"dsads"},"html":{"text":"<p class=\"paragraphNumeroUno\">dsads</p>","footnotes":{},"glossaryentries":{}},"versionUrn":"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45","contentUrn":"urn:pearson:entity:39946858-117e-405c-8c69-e1afb2302442"}
+const index = 0
 
 jest.mock('../../../src/config/config.js', () => ({
     currentSlateUrn: "urn:pearson:manifest:d9023151-3417-4482-8175-fc965466220e",
@@ -17,22 +17,26 @@ jest.mock('../../../src/config/config.js', () => ({
 }))
 jest.mock('axios');
 
-describe('Testing handleTCM function', () => {
-    it('Test-handleTCM', () => {
-        const res = { data : [{
+describe('Testing handleTCM function to call fetchAllDataMapper function', () => {
+    it('Test-handleTCM if latestPendingTransaction data is present', () => {
+        const res = {data : [{"elemURN":"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45","latestPendingTransaction":{"elemSURN":"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45","elemSnapshot":"{\"contentSnapshot\":\"<p class=\\\"paragraphNumeroUno\\\">dsads</p>\",\"glossorySnapshot\":\"[]\",\"footnoteSnapshot\":\"[]\",\"assetPopOverSnapshot\":\"[]\"}","elemWIPData":"{\"id\":\"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45\",\"type\":\"element-authoredtext\",\"schema\":\"http://schemas.pearson.com/wip-authoring/element/1\",\"elementdata\":{\"schema\":\"http://schemas.pearson.com/wip-authoring/authoredtext/1#/definitions/authoredtext\",\"text\":\"dsads\"},\"versionUrn\":\"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45\",\"contentUrn\":\"urn:pearson:entity:39946858-117e-405c-8c69-e1afb2302442\"}","elementType":"P","elementEditor":"C5, C5 Test02","changeType":"Update","changeStatus":"Pending","changeTime":"1625820466940","lastUpdatedTimestamp":"1625820466940","slateType":"slate","slateID":"urn:pearson:manifest:2cf2dc69-3e52-43f9-a412-737ff7d8f55b"}}]}
+        axios.get = jest.fn(() => Promise.resolve(res));
+        store.dispatch(handleTCM(element, index))       
+    })
+    it('Test-handleTCM if latestPendingTransaction and latestAcceptedTransaction data is present', () => {
+        const res = {data : [{"elemURN":"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45","latestPendingTransaction":{"elemSURN":"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45","elemSnapshot":"{\"contentSnapshot\":\"<p class=\\\"paragraphNumeroUno\\\">dsadsa</p>\",\"glossorySnapshot\":\"[]\",\"footnoteSnapshot\":\"[]\",\"assetPopOverSnapshot\":\"[]\"}","elemWIPData":"{\"id\":\"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45\",\"type\":\"element-authoredtext\",\"schema\":\"http://schemas.pearson.com/wip-authoring/element/1\",\"elementdata\":{\"schema\":\"http://schemas.pearson.com/wip-authoring/authoredtext/1#/definitions/authoredtext\",\"text\":\"dsadsa\"},\"versionUrn\":\"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45\",\"contentUrn\":\"urn:pearson:entity:39946858-117e-405c-8c69-e1afb2302442\"}","elementType":"P","elementEditor":"C5, C5 Test02","changeType":"Update","changeStatus":"Pending","changeTime":"1625820563474","lastUpdatedTimestamp":"1625820563474","slateType":"slate","slateID":"urn:pearson:manifest:2cf2dc69-3e52-43f9-a412-737ff7d8f55b"},"latestAcceptedTransaction":{"elemSURN":"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45","elemSnapshot":"{\"contentSnapshot\":\"<p class=\\\"paragraphNumeroUno\\\">dsads</p>\",\"glossorySnapshot\":\"[]\",\"footnoteSnapshot\":\"[]\",\"assetPopOverSnapshot\":\"[]\"}","elemWIPData":"{\"id\":\"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45\",\"type\":\"element-authoredtext\",\"schema\":\"http://schemas.pearson.com/wip-authoring/element/1\",\"elementdata\":{\"schema\":\"http://schemas.pearson.com/wip-authoring/authoredtext/1#/definitions/authoredtext\",\"text\":\"dsads\"},\"versionUrn\":\"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45\",\"contentUrn\":\"urn:pearson:entity:39946858-117e-405c-8c69-e1afb2302442\"}","elementType":"P","trackChangeApprover":"C5 Test02 C5","elementEditor":"C5, C5 Test02","changeType":"Update","changeStatus":"Accepted","changeTime":"1625820466940","lastUpdatedTimestamp":"1625820551433","slateType":"slate","slateID":"urn:pearson:manifest:2cf2dc69-3e52-43f9-a412-737ff7d8f55b"}}]}
+        axios.get = jest.fn(() => Promise.resolve(res));
+        store.dispatch(handleTCM(element, index))       
+    })
 
-        }]
-        }
-        const elementData = {elemURN: 'urn:pearson:work:c5f26743-d2d2-4664-b0df-9ac59e11642b'}
-        const id = element.id
-        expect(elementData.elemURN).toEqual(id)
-        axios.get = jest.fn(() => Promise.resolve(res)); 
-        store.dispatch(handleTCM(element))       
+    it('Test-handleTCM if latestAcceptedTransaction data is present', () => {
+        const res = {data : [{"elemURN":"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45","latestAcceptedTransaction":{"elemSURN":"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45","elemSnapshot":"{\"contentSnapshot\":\"<p class=\\\"paragraphNumeroUno\\\">dsadsa</p>\",\"glossorySnapshot\":\"[]\",\"footnoteSnapshot\":\"[]\",\"assetPopOverSnapshot\":\"[]\"}","elemWIPData":"{\"id\":\"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45\",\"type\":\"element-authoredtext\",\"schema\":\"http://schemas.pearson.com/wip-authoring/element/1\",\"elementdata\":{\"schema\":\"http://schemas.pearson.com/wip-authoring/authoredtext/1#/definitions/authoredtext\",\"text\":\"dsadsa\"},\"versionUrn\":\"urn:pearson:work:cde78307-24c9-4e0d-b676-05231ee69b45\",\"contentUrn\":\"urn:pearson:entity:39946858-117e-405c-8c69-e1afb2302442\"}","elementType":"P","trackChangeApprover":"C5 Test02 C5","elementEditor":"C5, C5 Test02","changeType":"Update","changeStatus":"Accepted","changeTime":"1625820563474","lastUpdatedTimestamp":"1625820789835","slateType":"slate","slateID":"urn:pearson:manifest:2cf2dc69-3e52-43f9-a412-737ff7d8f55b"}}]}
+        axios.get = jest.fn(() => Promise.resolve(res));
+        store.dispatch(handleTCM(element, index))       
     })
 
     it('Test-tcmButtonHandler function', () => {
-        const res = {
-        }
+        const res = {}
         axios.patch = jest.fn(() => Promise.resolve(res)); 
         store.dispatch(tcmButtonHandler())
     })
