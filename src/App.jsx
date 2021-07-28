3b4a06aec08533f301436bd5ce1602975e54e2e1
Jain, Sparsh <sparsh.jain@pearson.com>
2021-07-28 09:40:25 +0000

    App.jsx version Update

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.28.4 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.28.5 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
