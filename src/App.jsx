ebe4f0bc7c27622ce81bf9c8719dda627fbb4200
Srivastava, Chavi <chavi.srivastava@pearson.com>
2021-08-03 14:12:11 +0000

    update canvas tag canvas-1.28.12

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import CanvasWrapper from './component/CanvasWrapper';
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.28.11 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.28.12 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
