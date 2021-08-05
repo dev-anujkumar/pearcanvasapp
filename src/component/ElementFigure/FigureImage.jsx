bcd8a0c9cc7ad81e8eda7abaa4961d71bb8ef52b
Pratik Valvi <pratik.valvi@pearson.com>
2021-08-05 10:51:53 +0530

    Created custom TextField

--- src/component/ElementFigure/FigureImage.jsx
+++ src/component/ElementFigure/FigureImage.jsx
@@ -1,43 +1,23 @@
 import React from 'react'
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import { makeStyles } from '@material-ui/core/styles';
+import TinyMceEditor from "../tinyMceEditor";
 
-const useStyles = makeStyles((theme) => ({
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120
-    }
-}));
+const TextField = (props) => {
+    return (
+        <div className='textfield-container'>
+            <input placeholder=" " />
+            <label className='textfield-label'>{props.label}</label>
+            <div className='textfield-border-bottom'></div>
+        </div>
+    )
+}
 
 const FigureImage = () => {
-    const classes = useStyles();
-    const [label, setLabel] = React.useState('Label');
-
     return (
         <>
-        <div className='image-header'>
-        <FormControl className={classes.formControl}>
-            <InputLabel id="demo-simple-select-label">Label</InputLabel>
-            <Select
-                labelId="select-label"
-                id="simple-select"
-                value={label}
-                onChange={(event) => {
-                    setLabel(event.target.value);
-                }}
-            >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-            </Select>   
-        </FormControl>
-        <TextField className='textfield-name' id="name" label="Label Name" />
-        <TextField className='textfield-number' id="number" label="Number" />
-        </div>
+            <div className='image-header'>
+                <TextField label='Label' />
+                <TextField label='Number' />
+            </div>
         </>
     )
 }
