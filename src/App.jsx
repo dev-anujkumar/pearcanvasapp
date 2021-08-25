c11cc40ec90e8c10da46f72a273af280e08e6b4d
Srivastava, Chavi <chavi.srivastava@pearson.com>
2021-08-25 12:44:43 +0000

    update tag to resolve conflict canvas-1.29.11

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { modifyObjKeys, loadPatternScripts } from './js/appUtils'
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.29.10.02 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.29.11 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
