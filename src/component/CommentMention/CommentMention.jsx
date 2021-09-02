af67f73a1cb64895c5d4e29b3b904bf8c4ccb2f4
sparsh <sparshgr8@yahoo.com>
2021-09-02 12:30:55 +0530

    BG 5140: Get users if length is 0

--- src/component/CommentMention/CommentMention.jsx
+++ src/component/CommentMention/CommentMention.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { MentionsInput, Mention } from 'react-mentions';
+import { useDispatch } from 'react-redux';
+import { getProjectUsers } from '../CommentsPanel/CommentsPanel_Action';
 import './CommentMention.css'
 
 const CommentMention = (props) => {
+  const dispatch = useDispatch();
   const onCommentChange = (event, value) => {
     if (!props.readOnly) {
       props.handleCommentChange(value);
@@ -15,10 +18,19 @@ const CommentMention = (props) => {
     display: `@${myUser.lastName}, ${myUser.firstName}`
   }));
 
+  const getUsers = () => {
+    // length is 0 if users clicks view in cypress 
+    // from comments manager
+    if(props.projectUsers.length === 0) {
+      dispatch(getProjectUsers());
+    }
+  }
+
   
     return (
       <div className="comment-mentions">
         <MentionsInput
+          onFocus={getUsers}
           readOnly={props.readOnly}
           value={props.comment}
           onChange={onCommentChange}
