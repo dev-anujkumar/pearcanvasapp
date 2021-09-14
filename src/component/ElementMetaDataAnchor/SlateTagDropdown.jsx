c5b92ac558cbc92c47e1392976ab7b80bf2b3d65
Chavi Srivastava <chavi.srivastava@magicsw.com>
2021-09-14 14:00:23 +0530

    myCloudProxySession key passed to Table and CE SPAs

--- src/component/ElementMetaDataAnchor/SlateTagDropdown.jsx
+++ src/component/ElementMetaDataAnchor/SlateTagDropdown.jsx
@@ -94,7 +94,8 @@ class SlateTagDropdown extends React.Component {
             'mathmlImagePath': config.S3MathImagePath ? config.S3MathImagePath : defaultMathImagePath,
             'productApiUrl': config.PRODUCTAPI_ENDPOINT,
             'manifestApiUrl': config.ASSET_POPOVER_ENDPOINT,
-            'assessmentApiUrl': config.ASSESSMENT_ENDPOINT
+            'assessmentApiUrl': config.ASSESSMENT_ENDPOINT,
+            'myCloudProxySession': config.myCloudProxySession
         };
         this.warningActionIntiator = e.target.innerText;
         if (e.target.innerText == ViewLearningObjectiveSlateDropdown && config.slateType !== 'assessment') {
@@ -156,7 +157,8 @@ class SlateTagDropdown extends React.Component {
       'strApiKey': config.STRUCTURE_APIKEY,
       'productApiUrl': config.PRODUCTAPI_ENDPOINT,
       'manifestApiUrl': config.ASSET_POPOVER_ENDPOINT,
-      'assessmentApiUrl': config.ASSESSMENT_ENDPOINT
+      'assessmentApiUrl': config.ASSESSMENT_ENDPOINT,
+      'myCloudProxySession': config.myCloudProxySession
     };
     const selectedLOs = this.props.currentSlateLOData;
     let externalLFUrn = '';
