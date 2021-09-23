49ce31105543dd0c927b614044dff9a641b1db4b
Ranbir <ranbir.singh@magicedtech.com>
2021-09-23 18:03:34 +0530

    Test cases for search file

--- __test__/component/Toolbar/Search/Search.test.js
+++ __test__/component/Toolbar/Search/Search.test.js
@@ -4,7 +4,6 @@ import thunk from 'redux-thunk';
 const middlewares = [thunk];
 import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
-
 import { searchClose, searchUp, searchDown } from './../../../../src/images/TinyMce/TinyMce.jsx';
 import SearchComponent from './../../../../src/component/Toolbar/Search/Search';
 
@@ -18,11 +17,25 @@ let props = {
     icons: { searchClose, searchUp, searchDown }
 }
 
-let searchObj = mount(<Provider store={store}><SearchComponent {...props} /></Provider>);
-
+let wrapper = mount(<Provider store={store}><SearchComponent {...props} /></Provider>);
 describe('Toolbar testing', () => {
     it('Handle Search content change', () => {
-        let instance = searchObj.find('SearchComponent').instance();
-        // wrapper.find('.collapse-header').simulate('click')
+        wrapper.find('SearchComponent').instance();
+     })
+    it('Simulate search Up change', () => {
+        let event = {}
+        wrapper.find('.actions').childAt(0).simulate('click', event,'prev')
+     })
+     it('Simulate search Down change', () => {
+        let event = {}
+        wrapper.find('.actions').childAt(1).simulate('click', event,'next')
+     })
+     it('Simulate search Close change', () => {
+        let event = {}
+        wrapper.find('.actions').childAt(2).simulate('click', event)
+     })
+     it('Simulate onchange on input', () => {
+        let event = {}
+        wrapper.find('input').simulate('change', event)
      })
 });
