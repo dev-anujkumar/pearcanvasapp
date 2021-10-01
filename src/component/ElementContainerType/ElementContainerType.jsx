77ddace573c81e700d6a595185f35de917ec6c55
Jaya Singh <jaya.singh@magicsw.com>
2021-10-01 12:53:14 +0530

    added changes related to BP into aside inside multi column

--- src/component/ElementContainerType/ElementContainerType.jsx
+++ src/component/ElementContainerType/ElementContainerType.jsx
@@ -53,8 +53,7 @@ export default function ElementContainerType(props) {
         const hideElementList = ["poetry-elem", "show-hide-elem", "popup-elem", "elm-interactive-elem", "interactive-elem", "element-discussion"];
         const hideElementListMulticolumn = ["Add Pop Up","Add Discussion"]
         return propsData && propsData.map((item, index) => {
-            if (((elementType === "group") && text === "block-text-button" && item.text === "Block Poetry") ||
-            (text === "interactive-elem-button" && (elementType === "group" && hideElementListMulticolumn.includes(item.text)))
+            if ((text === "interactive-elem-button" && (elementType === "group" && hideElementListMulticolumn.includes(item.text)))
             || (config.isPopupSlate && item.text === "Add Pop Up") ||
             /* Not show poetry/Popup/SH inside SH interactive Picker */
             (elementType === SHOW_HIDE && (hideElementList.includes(item?.buttonType)))) {
