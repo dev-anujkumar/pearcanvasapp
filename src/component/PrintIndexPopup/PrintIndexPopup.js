0491c9ec3c9a58a573dd6f3173d481a1704f486c
Ankur Khandelwal <ankur.khandelwal@magicsw.com>
2021-10-05 15:09:23 +0530

    Created index setting UI

--- src/component/PrintIndexPopup/PrintIndexPopup.js
+++ src/component/PrintIndexPopup/PrintIndexPopup.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Divider,
          Box,
          Drawer,
          TextField,
-         styled,
+         styled
          } from '@material-ui/core';
 import {Close, ErrorOutline} from '@material-ui/icons'
 import { useSelector, useDispatch} from 'react-redux';
 import { printIndexPopup } from './PrintIndex_Action';
 import '../../styles/PrintIndexPopup/PrintIndexPopup.css';
-import '../../styles/GlossaryFootnotePopup/GlossaryFootnotePopup.css';
+// import '../../styles/GlossaryFootnotePopup/GlossaryFootnotePopup.css';
 
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
@@ -19,34 +19,77 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
+
 export default function PrintIndexPopup(){
+  const [indexEntry, setIndexEntry] = useState('');
+  const [subEntry, setSubEntry] = useState('');
+
   const state = useSelector(state => state.printIndexValue);
   const dispatch = useDispatch();
 
   const handleDrawerClose = value => {
-        dispatch(printIndexPopup(value));
+    dispatch(printIndexPopup(value));
   };
+
+  const changeIndexEntry = e => {
+    setIndexEntry(e.target.value);
+  }
+
+  const changeSubEntry = e => {
+    setSubEntry(e.target.value);
+  }
   
     return (
       <div>
-          <Drawer
-            anchor='right'
-            variant="persistent"
-            open={state.popUpStatus}
+        <Drawer
+          anchor='right'
+          variant="persistent"
+          open={state.popUpStatus}
+        >
+          <DrawerHeader>
+            <span className="printIndex-label">Index Settings</span>
+            <Close onClick={() => handleDrawerClose(false)}/> 
+          </DrawerHeader>
+          <Divider />
+          <Box
+            component="form"
+            sx={{
+              '& .MuiTextField-root': { m: 1, width: '25ch' },
+              width: 300
+            }}
           >
-            <DrawerHeader>
-              <span className="printIndex-label">Index Settings</span>
-                <Close onClick={() => handleDrawerClose(false)}/> 
-            </DrawerHeader>
-            <Divider />
-            <Box
-              sx={{ width: 300 }}
-              role="presentation"
-            >
-                              
-
-            </Box>
-          </Drawer>
+            <div>
+              <ErrorOutline />
+              <span>This data is used for print only</span>
+            </div>
+            <div>
+              <TextField
+                id="indexEntry"
+                label="Index Entry"
+                defaultValue=""
+                variant="filled"
+                margin="normal"
+                size="normal"
+                value={indexEntry}
+                onChange={changeIndexEntry}
+              />
+              <TextField
+                id="filled-required"
+                label="Sub-Entry (optional)"
+                defaultValue=""
+                variant="filled"
+                margin="normal"
+                size="normal"
+                value={subEntry}
+                onChange={changeSubEntry}
+              />
+            </div>
+            <div>
+              <span className="printIndx-cancel-button" onClick={() =>{}}>Cancel</span>
+              <span className="printIndex-save-button" disabled={false} onClick={() => {}}>Save</span>
+            </div>
+          </Box>
+        </Drawer>
       </div>
     );
 }
