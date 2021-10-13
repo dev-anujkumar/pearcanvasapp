06f923bb7b8e52c626638f4f02da6c4da0fb1968
Kazim Raja <kazim.raja@magicsw.com>
2021-10-13 19:20:39 +0530

    Updated code for normal marked index

--- src/component/PrintIndexPopup/PrintIndexPopup.js
+++ src/component/PrintIndexPopup/PrintIndexPopup.js
@@ -1,8 +1,6 @@
-// import React, { useState,useRef, useEffect } from 'react';
 import React, { Component } from 'react';
-import {Close, ErrorOutline} from '@material-ui/icons'
+import { Close, ErrorOutline } from '@material-ui/icons'
 import '../../styles/PrintIndexPopup/PrintIndexPopup.css';
-import TextField from '@material-ui/core/TextField';
 import { ShowLoader } from '../../constants/IFrameMessageTypes';
 import { sendDataToIframe, hasReviewerRole } from '../../constants/utility.js';
 import config from '../../config/config';
@@ -10,163 +8,103 @@ import { setFormattingToolbar } from '../GlossaryFootnotePopup/GlossaryFootnote_
 import { saveGlossaryAndFootnote } from "../GlossaryFootnotePopup/GlossaryFootnote_Actions";
 import ReactMarkedIndexEditor from "../tinyMceMarkedIndexEditor"
 import { checkforToolbarClick } from '../../js/utils'
-import { render } from 'enzyme';
 
-
-
-
-
-  class PrintIndexPopup extends Component {
-    constructor(props) {
-      super(props);
-      //context=this;
-      this.wrapperRef = null;
+class PrintIndexPopup extends Component {
+  constructor(props) {
+    super(props);
   }
 
+  markedIndexValueDifference = (newEntry, newSubEntry, oldEntry, oldSubEntry) => {
+    let domparser, newEntryDom, newSubEntryDom, oldEntryDom, oldSubEntryDom, tempVar, tempTerm;
+    tempVar = oldSubEntry;
+    tempTerm = oldEntry;
+    tempVar = tempVar && tempVar.replace(/\sdata-mathml/g, 'data-temp-mathml')
+    domparser = new DOMParser()
+    newEntryDom = domparser.parseFromString(newEntry, "text/html")
+    oldEntryDom = domparser.parseFromString(tempTerm, "text/html")
+    oldSubEntryDom = domparser.parseFromString(tempVar, "text/html")
+    newSubEntryDom = domparser.parseFromString(newSubEntry, "text/html")
 
-  handleClickOutside = (event) => {
-    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-        /** Case - event target is not even wiris modal */
-        if (!(document.querySelector('.wrs_modal_dialogContainer:not(.wrs_closed)') && document.querySelector('.wrs_modal_dialogContainer:not(.wrs_closed)').contains(event.target)) && !document.getElementById('openAudioBook') && !document.getElementById('openFigureGlossary') && !document.getElementById('ext_AddAnAsset') && !document.getElementById('ext_ProductLink') && !document.getElementById('popup') && !document.querySelector('div.modal-content.wiris-alt-text-popup')?.contains(event.target) && !document.getElementById('alfresco-picker')) {
-            this.saveContent()
-        }
-    }
-}
+    return !(newEntryDom.isEqualNode(oldEntryDom) && newSubEntryDom.isEqualNode(oldSubEntryDom))
+  }
 
-componentWillMount() {
-    document.addEventListener('mousedown', this.handleClickOutside);
-    //this.setWrapperRef(this);
-}
-
-componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
-}
-/**
-  * Set the wrapper ref
-  */
-setWrapperRef = (node) => {
-    this.wrapperRef = node;
-}
-
-  // const wrapperRef = useRef(null);
-  // useOutsideAlerter(wrapperRef);
-  // let propMarkIndexvalue = props.markIndexCurrentValue;
-  // let firstLevel = (propMarkIndexvalue.firstLevel && tinyMCE.$(propMarkIndexvalue.firstLevel.trim()).length) ? (tinyMCE.$(propMarkIndexvalue.firstLevel))[0].innerHTML : propMarkIndexvalue.firstLevel;
-  // let secondLevel = (propMarkIndexvalue.secondLevel && tinyMCE.$(propMarkIndexvalue.secondLevel.trim()).length) ? (tinyMCE.$(propMarkIndexvalue.secondLevel))[0].innerHTML : propMarkIndexvalue.secondLevel;
-
-  // const [indexEntry, setIndexEntry] = useState(firstLevel || '');
-  // const [subEntry, setSubEntry] = useState(secondLevel || '');
-
-  // const handleDrawerClose = value => {
-  //   props.showPrintIndexPopup(value);
-  // };
-
-  // const changeIndexEntry = e => {
-  //   setIndexEntry(e.target.value);
-  // }
-
-  
-
-  // const changeSubEntry = e => {
-  //   setSubEntry(e.target.value);
-  // }
- 
-glossaryFootnoteDifference = (newTerm, newDef, oldTerm, oldDef) => {
-  let domparser, newTermDom, newDefDom, oldTermDom, oldDefDom, tempVar, tempTerm;
-  tempVar = oldDef;
-  tempTerm = oldTerm;
-  tempVar = tempVar && tempVar.replace(/\sdata-mathml/g, 'data-temp-mathml')
-  domparser = new DOMParser()
-  newTermDom = domparser.parseFromString(newTerm, "text/html")
-  oldTermDom = domparser.parseFromString(tempTerm, "text/html")
-  oldDefDom = domparser.parseFromString(tempVar, "text/html")
-  newDefDom = domparser.parseFromString(newDef, "text/html")
-
-  // if(type==='markedindex'){
-  return !(newTermDom.isEqualNode(oldTermDom) && newDefDom.isEqualNode(oldDefDom))
-  // }
-  // switch(type){
-  //     case "glossary":
-  //         return !(newTermDom.isEqualNode(oldTermDom) && newDefDom.isEqualNode(oldDefDom))
-  //     case "footnote":
-  //         return !newDefDom.isEqualNode(oldDefDom)
-  // }
-}
-
-saveContent = () => {
-  if (!hasReviewerRole()) {
-    const { markedIndexValue } = this.props;
-    let { elementWorkId, elementType, markIndexid, type, elementSubType, typeWithPopup, poetryField } = markedIndexValue;
-    let firstLevel = null;
-    let secondLevel = null;
-    let defaultValue = document.createElement('p')
-    firstLevel = document.querySelector('#markedindex-editor > div > p') || defaultValue;
-    tinymce.$(firstLevel).find('[data-mce-bogus]').each(function () {
+  saveContent = () => {
+    if (!hasReviewerRole()) {
+      const { markedIndexValue } = this.props;
+      let { elementWorkId, elementType, markIndexid, type, elementSubType, typeWithPopup, poetryField } = markedIndexValue;
+      let firstLevel = null;
+      let secondLevel = null;
+      let defaultValue = document.createElement('p')
+      firstLevel = document.querySelector('#markedindex-editor > div > p') || defaultValue;
+      tinymce.$(firstLevel).find('[data-mce-bogus]').each(function () {
         let innerHtml = innerHTML;
         outerHTML = innerHtml;
-    })
-    secondLevel = document.querySelector('#index-secondlevel-attacher > div > p') || defaultValue;
-    tinymce.$(secondLevel).find('[data-mce-bogus]').each(function () {
+      })
+      secondLevel = document.querySelector('#index-secondlevel-attacher > div > p') || defaultValue;
+      tinymce.$(secondLevel).find('[data-mce-bogus]').each(function () {
         let innerHtml = this.innerHTML;
         this.outerHTML = innerHtml;
-    })
+      })
+      firstLevel = firstLevel.innerHTML.match(/<p>/g) ? firstLevel.innerHTML.replace(/<br data-mce-bogus="1">/g, "")
+        : `<p>${firstLevel.innerHTML.replace(/<br data-mce-bogus="1">/g, "")}</p>`
+      secondLevel = secondLevel.innerHTML.match(/<p>/g) ? secondLevel.innerHTML.replace(/<br data-mce-bogus="1">/g, "")
+        : `<p>${secondLevel.innerHTML.replace(/<br data-mce-bogus="1">/g, "")}</p>`
 
-    // if (glossaryFootnoteDifference(firstLevel, secondLevel, props.markIndexCurrentValue.firstLevel, props.markIndexCurrentValue.secondLevel)) {
-      config.isGlossarySaving = true;
-      sendDataToIframe({ 'type': ShowLoader, 'message': { status: true } });
-      saveGlossaryAndFootnote(elementWorkId, elementType, markIndexid, type, firstLevel, secondLevel, elementSubType, typeWithPopup, poetryField)
-    // }
+      if (this.markedIndexValueDifference(firstLevel, secondLevel, this.props.markedIndexCurrentValue.firstLevel, this.props.markedIndexCurrentValue.secondLevel)) {
+        config.isGlossarySaving = true;
+        sendDataToIframe({ 'type': ShowLoader, 'message': { status: true } });
+        saveGlossaryAndFootnote(elementWorkId, elementType, markIndexid, type, firstLevel, secondLevel, elementSubType, typeWithPopup, poetryField)
+      }
+    }
+    this.props.showMarkedIndexPopup(false, '');
   }
-  props.showMarkedIndexPopup(false, '');
-}
 
 
-toolbarHandling = (e, action = "") => {
-  let relatedTargets = (e && e.relatedTarget && e.relatedTarget.classList) ? e.relatedTarget.classList : [];
-  if (e && checkforToolbarClick(relatedTargets)) {
+  toolbarHandling = (e, action = "") => {
+    let relatedTargets = (e && e.relatedTarget && e.relatedTarget.classList) ? e.relatedTarget.classList : [];
+    if (e && checkforToolbarClick(relatedTargets)) {
       e.stopPropagation();
       return;
-  }
-  if (e && e.type == 'blur') {
+    }
+    if (e && e.type == 'blur') {
       /** to disable both toolbars on glossary editor blur */
       setFormattingToolbar('disableTinymceToolbar')
-      // setFormattingToolbar('disableGlossaryFootnoteToolbar')
+      setFormattingToolbar('disableGlossaryFootnoteToolbar')
+    }
+    if (document.querySelector('div#toolbarGlossaryFootnote .tox-toolbar')) {
+      if (action === "add") {
+        setFormattingToolbar('disableGlossaryFootnoteToolbar')
+      } else if (action === "remove") {
+        setFormattingToolbar('disableTinymceToolbar')
+        setFormattingToolbar('enableGlossaryFootnoteToolbar')
+      }
+    }
   }
-  // if (document.querySelector('div#toolbarGlossaryFootnote .tox-toolbar')) {
-  //     if (action === "add") {
-  //         setFormattingToolbar('disableGlossaryFootnoteToolbar')
-  //     } else if (action === "remove") {
-  //         setFormattingToolbar('disableTinymceToolbar')
-  //         setFormattingToolbar('enableGlossaryFootnoteToolbar')
-  //     }
-  // }
-}
 
-render(){
+  render() {
 
-  console.log("this.props.markedIndexCurrentValu", this.props.markedIndexCurrentValue)
-  
     return (
       <div>
-      <div ref={this.wrapperRef} className='index-container'>
-        <div className="index-setting">
-          <span className="printIndex-label">Index Settings</span>
-          <span><Close onClick={() => this.props.showMarkedIndexPopup(false)} /></span>
-        </div>
-        <div className="index-body">
-          <div className="index-text">
-            <ErrorOutline />
-            <span>This data is used for print only</span>
+        <div className='index-container'>
+          <div className="index-setting">
+            <span className="printIndex-label">Index Settings</span>
+            <span><Close onClick={() => this.props.showMarkedIndexPopup(false)} /></span>
           </div>
-
-            
-          <div className="markedindex-word-header">
-            <div className="markedindex-word-title">Index Entry</div>
-            <div className="markedindex-word-name markedindex-word-description" id='markedindex-editor' onFocus={() => this.toolbarHandling(null, 'remove')} onBlur={(e) => this.toolbarHandling(e, 'add')}>
-              <ReactMarkedIndexEditor permissions={this.props.permissions} markIndexCurrentValue={this.props.markedIndexCurrentValue?.firstLevel}  className='definition-editor place-holder' placeholder="Type Something" id='markedindex-0' />
+          <div className="index-body">
+            <div className="index-text">
+              <ErrorOutline />
+              <span>This data is used for print only</span>
             </div>
-          </div>
+            <div className="hide-markedinde-toolbar">
+              <div id="toolbarGlossaryFootnote"></div>
+            </div>
 
+            <div className="markedindex-word-header">
+              <div className="markedindex-word-title">Index Entry</div>
+              <div className="markedindex-word-name markedindex-word-description" id='markedindex-editor' onFocus={() => this.toolbarHandling(null, 'remove')} onBlur={(e) => this.toolbarHandling(e, 'add')}>
+                <ReactMarkedIndexEditor permissions={this.props.permissions} markIndexCurrentValue={this.props.markedIndexCurrentValue?.firstLevel} className='definition-editor place-holder' placeholder="Type Something" id='markedindex-0' />
+              </div>
+            </div>
 
             <div className="markedindex-secondlevel-header">
               <div className="markedindex-secondlevel-label">Sub-Entry (optional)</div>
@@ -175,43 +113,16 @@ render(){
               </div>
             </div>
 
-
-          {/* <div>
-            <TextField
-              // id="filled-size-normal"
-              label="Index Entry"
-              // defaultValue="1234"
-              variant="filled"
-              // margin="normal"
-              // size="normal"
-              value={indexEntry}
-              onChange={changeIndexEntry}
-              fullWidth 
-              multiline={true}
-              autoComplete="off"
-            />
-            <TextField
-              id="filled-required"
-              label="Sub-Entry (optional)"
-              // defaultValue=""
-              variant="filled"
-              margin="normal"
-              size="normal"
-              value={subEntry}
-              onChange={changeSubEntry}
-              fullWidth
-            />
-          </div> */}
-          <div className="button-group">
-            <span className="printIndx-cancel-button" onClick={() => this.props.showMarkedIndexPopup(false,'')}>Cancel</span>
-            <span className="printIndex-save-button" disabled={false} onClick={this.saveContent}>Save</span>
+            <div className="button-group">
+              <span className="printIndx-cancel-button" onClick={() => this.props.showMarkedIndexPopup(false, '')}>Cancel</span>
+              <span className="printIndex-save-button" disabled={false} onClick={this.saveContent}>Save</span>
+            </div>
           </div>
-        </div>
 
-      </div>
+        </div>
       </div>
     );
-}
+  }
 }
 
 export default PrintIndexPopup;
--- src/component/PrintIndexPopup/PrintIndex_Action.js
+++ src/component/PrintIndexPopup/PrintIndex_Action.js
@@ -1,30 +1,9 @@
-import {OPEN_MARKED_INDEX} from '../../constants/Action_Constants';
-
-// export const printIndexPopup = (value) => (dispatch) => {
-//     dispatch({
-//         type: OPEN_MARKED_INDEX,
-//         payload: value
-//     });
-// };
-
-
-import axios from 'axios';
+import { OPEN_MARKED_INDEX } from '../../constants/Action_Constants';
 import config from '../../config/config';
 import store from '../../appstore/store.js'
-import { sendDataToIframe, createTitleSubtitleModel, matchHTMLwithRegex, createLabelNumberTitleModel } from '../../constants/utility.js';
-import { replaceUnwantedtags } from '../ElementContainer/UpdateElements';
-import { HideLoader } from '../../constants/IFrameMessageTypes.js';
-import { hideTocBlocker } from '../../js/toggleLoader'
-import { tcmSnapshotsForUpdate, fetchParentData, fetchElementWipData } from '../TcmSnapshots/TcmSnapshots_Utility.js';
-const {
-    REACT_APP_API_URL
-} = config
-import { allowedFigureTypesForTCM } from "../ElementContainer/ElementConstants";
-import {ADD_AUDIO_GLOSSARY_POPUP,OPEN_GLOSSARY_FOOTNOTE, UPDATE_FOOTNOTEGLOSSARY, ERROR_POPUP, GET_TCM_RESOURCES,HANDLE_GLOSSARY_AUDIO_DATA, ADD_FIGURE_GLOSSARY_POPUP, SET_FIGURE_GLOSSARY, WRONG_IMAGE_POPUP, SHOW_REMOVE_GLOSSARY_IMAGE} from "./../../constants/Action_Constants";
-import { handleElementsInShowHide, getShowHideIndex, onGlossaryFnUpdateSuccessInShowHide, findSectionType, getShowHideElement } from '../ShowHide/ShowHide_Helper.js';
-const elementTypeData = ['element-authoredtext', 'element-list', 'element-blockfeature', 'element-learningobjectives', 'element-citation', 'stanza', 'figure'];
+import { onGlossaryFnUpdateSuccessInShowHide } from '../ShowHide/ShowHide_Helper.js';
 
-    export const markedIndexPopup = (status, popupType, markIndexid, elementWorkId, elementType, index, elementSubType, markIndexText, typeWithPopup, poetryField) => async (dispatch) => {
+export const markedIndexPopup = (status, popupType, markIndexid, elementWorkId, elementType, index, elementSubType, markIndexText, typeWithPopup, poetryField) => async (dispatch) => {
     let markedIndexValue = {
         type: popupType,
         popUpStatus: status,
@@ -33,8 +12,8 @@ const elementTypeData = ['element-authoredtext', 'element-list', 'element-blockf
         markIndexid,
         elementSubType,
         markIndexText,
-        typeWithPopup : typeWithPopup ? typeWithPopup : undefined,
-        poetryField : poetryField ? poetryField : undefined
+        typeWithPopup: typeWithPopup ? typeWithPopup : undefined,
+        poetryField: poetryField ? poetryField : undefined
     }
 
     if (status === true) {
@@ -44,7 +23,7 @@ const elementTypeData = ['element-authoredtext', 'element-list', 'element-blockf
         let currentSlateData = newParentData[slateId];
         const showHideElement = store.getState().appStore?.showHideObj;
 
-        if(currentSlateData.type==="popup" && currentSlateData.status === "approved" && (config.isCreateFootnote || config.isCreateGlossary)){
+        if (currentSlateData.type === "popup" && currentSlateData.status === "approved" && (config.isCreateFootnote || config.isCreateGlossary)) {
             return false;
         }
         let newBodymatter = currentSlateData.contents.bodymatter;
@@ -55,118 +34,46 @@ const elementTypeData = ['element-authoredtext', 'element-list', 'element-blockf
 
             markedIndexElem = onGlossaryFnUpdateSuccessInShowHide("GetElementWithFnGlry_SH", newBodymatter, elementType, asideParent?.sectionType, tempIndex)
         }
-        // else if (typeWithPopup && typeWithPopup === "popup" ){
-        //     // let tempIndex = index.split('-');
-        //     let indexesLen = tempIndex.length;
-        //     switch (indexesLen){
-        //         case 2:
-        //             glossaryFootElem = newBodymatter[tempIndex[0]].popupdata["formatted-title"];
-        //             break;
-
-        //         case 3:
-        //             glossaryFootElem = newBodymatter[tempIndex[0]].elementdata.bodymatter[tempIndex[1]].popupdata["formatted-title"];
-        //             break;
-
-        //         case 4:
-        //             glossaryFootElem = newBodymatter[tempIndex[0]].elementdata.bodymatter[tempIndex[1]].contents.bodymatter[tempIndex[2]].popupdata["formatted-title"];
-        //             break;
-        //         /*
-        //             footnote for popup title inside aside element inside multicolumn BG-4750
-        //         */
-        //        case 5:
-        //             glossaryFootElem = newBodymatter[tempIndex[0]].groupeddata.bodymatter[tempIndex[1]].groupdata.bodymatter[tempIndex[2]].elementdata.bodymatter[tempIndex[3]].popupdata["formatted-title"];
-        //             break;
-        //     }
-        // }
-        // else if (typeWithPopup && typeWithPopup === 'poetry') {
-        //     // let tempIndex = index.split('-');
-        //     let indexesLen = tempIndex.length;
-        //     if (indexesLen === 2) {
-        //         switch (tempIndex[1]) {
-        //             case "1":
-        //                 glossaryFootElem = newBodymatter[tempIndex[0]].contents['formatted-title'] || {};
-        //                 break;
-        //             // case "3":
-        //             //     glossaryFootElem = newBodymatter[tempIndex[0]].contents['formatted-caption'] || {};
-        //             //     break;
-        //             case "4":
-        //                 glossaryFootElem = (newBodymatter[tempIndex[0]].contents['creditsarray'] ? newBodymatter[tempIndex[0]].contents['creditsarray'][0] : {});
-        //                 break;
-        //         }
-        //     }
-        // } else if ((tempIndex.length >= 4 && tempIndex.length <= 7) && elementType === "element-dialogue" && newBodymatter[tempIndex[0]].type === "groupedcontent") { // MultiColumn->PS or MultiColumn->As->PS or MultiColumn->WE->PS
-        //     let elementInside2C = newBodymatter[tempIndex[0]].groupeddata.bodymatter[tempIndex[1]].groupdata.bodymatter[tempIndex[2]];
-        //     if (elementInside2C.type === "element-aside" && elementInside2C.subtype === "sidebar") {
-        //         glossaryFootElem = newBodymatter[tempIndex[0]].groupeddata.bodymatter[tempIndex[1]].groupdata.bodymatter[tempIndex[2]].elementdata.bodymatter[tempIndex[3]];
-        //     } else if (elementInside2C.type === "element-aside" && elementInside2C.subtype === "workedexample") {
-        //         glossaryFootElem = newBodymatter[tempIndex[0]].groupeddata.bodymatter[tempIndex[1]].groupdata.bodymatter[tempIndex[2]].elementdata.bodymatter[tempIndex[3]];
-        //         if (glossaryFootElem.type === 'manifest') {
-        //             glossaryFootElem = glossaryFootElem.contents.bodymatter[tempIndex[4]];
-        //         }
-        //     } else {
-        //         glossaryFootElem = newBodymatter[tempIndex[0]].groupeddata.bodymatter[tempIndex[1]].groupdata.bodymatter[tempIndex[2]];
-        //     }
-
-
-
-        // } else if (elementType === "element-dialogue" && newBodymatter[tempIndex[0]].type === "element-aside" && newBodymatter[tempIndex[0]].subtype === "workedexample") { //Playscript inside we element
-        //     glossaryFootElem = newBodymatter[tempIndex[0]].elementdata.bodymatter[tempIndex[1]];
-        //     if (glossaryFootElem.type === 'manifest') {
-        //         glossaryFootElem = glossaryFootElem.contents.bodymatter[tempIndex[2]];
-        //     }
-        // } else if (elementType === "element-dialogue" && newBodymatter[tempIndex[0]].type === "element-aside" && newBodymatter[tempIndex[0]].subtype === "sidebar") { //Playscript inside aside element
-        //     glossaryFootElem = newBodymatter[tempIndex[0]].elementdata.bodymatter[tempIndex[1]];
-        // } else if (elementType === "element-dialogue") {
-        //     glossaryFootElem = newBodymatter[tempIndex[0]];
-        // }
         else {
             if (typeof (index) == 'number') {
                 if (newBodymatter[index].versionUrn == elementWorkId) {
                     markedIndexElem = newBodymatter[index]
                 }
-            } //else {
-        //         let indexes = index.split('-');
-        //         let indexesLen = indexes.length, condition;
-        //         if (indexesLen == 2) {
-        //             condition = newBodymatter[indexes[0]].elementdata.bodymatter[indexes[1]]
-        //             if (condition.versionUrn == elementWorkId) {
-        //                 glossaryFootElem = condition
-        //             }
-        //         } else if (indexesLen == 3) {
-        //             if(elementType==='stanza'){
-        //                 condition = newBodymatter[indexes[0]].contents.bodymatter[indexes[2]]
-        //             } else if (newBodymatter[indexes[0]].type === "groupedcontent") { //All elements inside multi-column except figure
-        //                 condition = newBodymatter[indexes[0]].groupeddata.bodymatter[indexes[1]].groupdata.bodymatter[indexes[2]]
-        //             } else {
-        //                 condition = newBodymatter[indexes[0]].elementdata.bodymatter[indexes[1]].contents.bodymatter[indexes[2]]
-        //             }
-        //             if (condition.versionUrn == elementWorkId) {
-        //                 glossaryFootElem = condition
-        //             }
-        //         }
-           
-        //         else if (indexesLen == 4) {  // to support glossary in text elements inside WE/AS of MultiColumn
-        //             glossaryFootElem = newBodymatter[tempIndex[0]].groupeddata.bodymatter[indexes[1]].groupdata.bodymatter[indexes[2]].elementdata.bodymatter[indexes[3]];
-                    
-        //         } else if (indexesLen == 5) { // to support glossary in section break inside WE of MultiColumn
-        //             glossaryFootElem = newBodymatter[tempIndex[0]].groupeddata.bodymatter[tempIndex[1]].groupdata.bodymatter[tempIndex[2]].elementdata.bodymatter[indexes[3]].contents.bodymatter[indexes[4]]
-        //         }
+            } else {
+                let indexes = index.split('-');
+                let indexesLen = indexes.length, condition;
+                if (indexesLen == 2) {
+                    condition = newBodymatter[indexes[0]].elementdata.bodymatter[indexes[1]]
+                    if (condition.versionUrn == elementWorkId) {
+                        markedIndexElem = condition
+                    }
+                } else if (indexesLen == 3) {
+                    if (elementType === 'stanza') {
+                        condition = newBodymatter[indexes[0]].contents.bodymatter[indexes[2]]
+                    } else if (newBodymatter[indexes[0]].type === "groupedcontent") { //All elements inside multi-column except figure
+                        condition = newBodymatter[indexes[0]].groupeddata.bodymatter[indexes[1]].groupdata.bodymatter[indexes[2]]
+                    } else {
+                        condition = newBodymatter[indexes[0]].elementdata.bodymatter[indexes[1]].contents.bodymatter[indexes[2]]
+                    }
+                    if (condition.versionUrn == elementWorkId) {
+                        markedIndexElem = condition
+                    }
+                }
 
-        //     }
+                else if (indexesLen == 4) {  // to support glossary in text elements inside WE/AS of MultiColumn
+                    markedIndexElem = newBodymatter[tempIndex[0]].groupeddata.bodymatter[indexes[1]].groupdata.bodymatter[indexes[2]].elementdata.bodymatter[indexes[3]];
+
+                } else if (indexesLen == 5) { // to support glossary in section break inside WE of MultiColumn
+                    markedIndexElem = newBodymatter[tempIndex[0]].groupeddata.bodymatter[tempIndex[1]].groupdata.bodymatter[tempIndex[2]].elementdata.bodymatter[indexes[3]].contents.bodymatter[indexes[4]]
+                }
+
+            }
         }
-        // switch (semanticType) {
-        //     case 'FOOTNOTE':
-        //         break;
-        //     case 'GLOSSARY':
-        //         tempGlossaryContentText = glossaryFootElem && glossaryFootElem.html['glossaryentries'] && glossaryFootElem.html['glossaryentries'][markIndexid]
-        //         footnoteContentText = tempGlossaryContentText && JSON.parse(tempGlossaryContentText).definition
-        //         glossaryContentText = tempGlossaryContentText && JSON.parse(tempGlossaryContentText).term || glossaryTermText
-        // }
-    }
 
-    tempMarkedIndexContentText = markedIndexElem && markedIndexElem.html['indexEntries'] && markedIndexElem.html['indexEntries'][markIndexid];
-    markedIndexTextFirstLvl = tempMarkedIndexContentText && JSON.parse(tempMarkedIndexContentText).firstLevelEntry || markIndexText;
-    markedIndexTextSecondLvl = tempMarkedIndexContentText && JSON.parse(tempMarkedIndexContentText).secondLevelEntry ;
+        tempMarkedIndexContentText = markedIndexElem && markedIndexElem.html['indexEntries'] && markedIndexElem.html['indexEntries'][markIndexid];
+        markedIndexTextFirstLvl = tempMarkedIndexContentText && JSON.parse(tempMarkedIndexContentText).firstLevelEntry || markIndexText;
+        markedIndexTextSecondLvl = tempMarkedIndexContentText && JSON.parse(tempMarkedIndexContentText).secondLevelEntry;
+    }
 
     return await dispatch({
         type: OPEN_MARKED_INDEX,
