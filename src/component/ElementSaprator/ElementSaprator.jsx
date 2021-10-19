98acc790e289b24b4fccbbba58a1e4b250c24d0d
Rahul Maheshwari <rahul.maheshwari@magicedtech.com>
2021-10-19 14:48:24 +0530

    element conversion done and restriction for block poetry, block list implemented

--- src/component/ElementSaprator/ElementSaprator.jsx
+++ src/component/ElementSaprator/ElementSaprator.jsx
@@ -365,12 +365,23 @@ export function typeOfContainerElements(elem, props) {
     /* Do not show Citation Group option if inside Multicolumn  */
     newData = (elem?.buttonType === "container-elem-button" && asideData?.type === "groupedcontent") ? {["Add Aside"]: newData["Add Aside"]} : newData;
     /* Do not show SH and Pop up option if Aside/WE is inside SH  */
-    if (elem?.buttonType === "interactive-elem-button" && asideData?.type === ELEMENT_ASIDE && asideData?.parent?.type === SHOW_HIDE) {
-        newData = {
-            ["Add Elm Interactive"]: newData["Add Elm Interactive"],
-            ["Add Quad Interactive"]: newData["Add Quad Interactive"],
-            ["Add Smart Link"]: newData["Add Smart Link"],
-            ["Add Discussion"]: newData["Add Discussion"]
+    if (asideData?.type === ELEMENT_ASIDE && asideData?.parent?.type === SHOW_HIDE) {
+        switch (elem?.buttonType) {
+            case "interactive-elem-button" :
+                newData = {
+                    ["Add Elm Interactive"]: newData["Add Elm Interactive"],
+                    ["Add Quad Interactive"]: newData["Add Quad Interactive"],
+                    ["Add Smart Link"]: newData["Add Smart Link"],
+                    ["Add Discussion"]: newData["Add Discussion"]
+                }
+                break;
+            case "block-text-button" :
+                newData = {
+                    ["Block Math"]: newData["Block Math"],
+                    ["Block Code"]: newData["Block Code"],
+                    ["Playscript"]: newData["Playscript"]
+                }
+                break;
         }
     }
     if(newData){
