4dc51da8e47078fec18eab37a670824a6f7ee440
sparsh <sparshgr8@yahoo.com>
2021-10-19 21:35:06 +0530

    cut paste scenerios in block poetry tcm

--- src/component/TcmSnapshots/TcmSnapshots_Utility.js
+++ src/component/TcmSnapshots/TcmSnapshots_Utility.js
@@ -425,8 +425,6 @@ const tcmSnapshotsPoetry = (snapshotsData, defaultKeys, index, isPopupSlate, { a
    const poetryElement = {
         element: wipData
     }
-    tag.childTag = 'ST'
-    elementId.childId = wipData.contents.bodymatter.find((item, i) => i===0).id;
     let isHead = "";
     if(asideData?.type === ELEMENT_ASIDE && asideData?.subtype === WORKED_EXAMPLE) {
         if(parentUrn?.manifestUrn == asideData?.id) {
@@ -436,11 +434,17 @@ const tcmSnapshotsPoetry = (snapshotsData, defaultKeys, index, isPopupSlate, { a
             isHead = "BODY"
         }
     }
-    const elementDetails = setElementTypeAndUrn(elementId, tag, isHead, parentUrn?.manifestUrn ? parentUrn.manifestUrn : "", undefined, popupInContainer, slateManifestVersioning, isPopupSlate, poetryElement, { asideData, parentUrn });
-    const stanzaData = {html: {text:'<p class="paragraphNumeroUno"><br></p>'}};
-    console.log("Poetry Snapshot Create 6 ", elementDetails, stanzaData);
-        
-    prepareAndSendTcmData(elementDetails, stanzaData, defaultKeys, actionStatus, index);
+    console.log("Poetry Snapshot Create 6 ", asideData, isHead);
+    // always one stanza will be created in poetry but in
+    // case of cut paste all of them have to be pasted
+    // so multiple snapshots will go 
+    wipData.contents.bodymatter.map((item) => {
+        elementId.childId = item.id;
+        tag.childTag = fetchElementsTag(item); 
+        const elementDetails = setElementTypeAndUrn(elementId, tag, isHead, parentUrn?.manifestUrn ? parentUrn.manifestUrn : "", undefined, popupInContainer, slateManifestVersioning, isPopupSlate, poetryElement, { asideData, parentUrn });
+        console.log("Poetry Snapshot Create 7 ", elementDetails);
+        prepareAndSendTcmData(elementDetails, item, defaultKeys, actionStatus,index);
+    })
 }
 
 /**
