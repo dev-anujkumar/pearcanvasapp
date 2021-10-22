b7e8d2c78930a7dbb26a860a954291fd84437610
sparsh <sparshgr8@yahoo.com>
2021-10-23 01:02:32 +0530

    Intilizing the wipdata in case of versioning.

--- src/component/TcmSnapshots/TcmSnapshots_Utility.js
+++ src/component/TcmSnapshots/TcmSnapshots_Utility.js
@@ -1371,8 +1371,10 @@ export const tcmSnapshotsForUpdate = async (elementUpdateData, elementIndex, con
         wipData = containerElement?.showHideObj?.currentElement || {};
     } 
     else if(typeOfElement === POETRY_ELEMENT) {
-        containerElement = prepareSnaphotPoetry(containerElement, response, elementIndex, currentSlateData);   
-        // wipData = containerElement?.poetryData?.currentElement || {};
+        containerElement = prepareSnaphotPoetry(containerElement, response, elementIndex, currentSlateData);
+        // for versioning case, we get the last data from wip
+        // so initilizing the wip
+        wipData = containerElement?.poetryData?.currentElement || {};
         // wipData = fetchElementWipData(updateBodymatter, elementIndex, response.type, "", actionStatus.action)
    
     }
