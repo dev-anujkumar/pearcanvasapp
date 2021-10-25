a5aeb5d07fb2db6c5d21ebc985af50c84fe5454d
Srivastava, Chavi <chavi.srivastava@pearson.com>
2021-10-25 11:00:35 +0000

    revert tag to canvas-1.30.63 to resolve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { modifyObjKeys, loadPatternScripts } from './js/appUtils'
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.30.63.1 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.30.63 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
