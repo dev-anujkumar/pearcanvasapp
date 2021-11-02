479240574c96c368384d0ba3462ad62685baf9e9
Jain, Sparsh <sparsh.jain@pearson.com>
2021-11-02 09:05:02 +0000

    App.jsx version udate

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { modifyObjKeys, loadPatternScripts } from './js/appUtils'
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.30.83 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.30.84 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
