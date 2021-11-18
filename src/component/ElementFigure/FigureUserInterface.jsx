52f439167d54dfb22acb6d03b737355c75a9bfc8
arpitmagic <71261885+arpitmagic@users.noreply.github.com>
2021-11-18 14:17:33 +0530

    Empty text updated

--- src/component/ElementFigure/FigureUserInterface.jsx
+++ src/component/ElementFigure/FigureUserInterface.jsx
@@ -341,7 +341,7 @@ class FigureUserInterface extends Component {
                             :
                             element.figuredata.interactiveformat === ELM_INT ?
                                 assetId ?
-                                    this.generateUpdateAssetJSX(element, this.props.elmInteractiveTitle, smartlinkIcon, assetPath, slPosterImage, "Update Interactive", "Item ID:", assetId, "Version:", alfrescoSite, imageDimension, ELM_INT)
+                                    this.generateUpdateAssetJSX(element, (this.props.elmInteractiveTitle || assetTitleText), smartlinkIcon, assetPath, slPosterImage, "Update Interactive", "Item ID:", assetId, "Version:", alfrescoSite, imageDimension, ELM_INT)
                                     :
                                     this.generateAddAssetJSX(smartlinkIcon, "Elm Interactive", "Add an Interactive", slPosterImage, "Item ID:", "Version:", ELM_INT)
                                 :
@@ -445,7 +445,7 @@ class FigureUserInterface extends Component {
                 break;
             case INTERACTIVE:
                 assetId = element.figuredata.interactiveid ? element.figuredata.interactiveid : '';
-                assetTitleText = element.figuredata?.interactivetitle ? element.figuredata?.interactivetitle : element.figuredata.interactiveformat === 'mmi' ? "QUAD" : element.figuredata.interactiveformat === 'mmi-elm' ? "ELM" : 'Smart link';
+                assetTitleText = element.figuredata?.interactivetitle ? element.figuredata?.interactivetitle : element.figuredata.interactiveformat === 'mmi' ? "QUAD" : element.figuredata.interactiveformat === 'mmi-elm' ? "ELM Interactive" : 'Smart link';
                 addButtonText = "Add a Smart Link";
                 assetIdText = "Asset ID:";
                 assetPathText = "Asset Path:";
