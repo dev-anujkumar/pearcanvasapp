c57aace8043e5166ba37b852eb487a2afff549ed
Jain, Sparsh <sparsh.jain@pearson.com>
2021-11-23 12:58:07 +0000

    App.jsx version update

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { modifyObjKeys } from './js/appUtils'
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.31.18 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.31.19 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
