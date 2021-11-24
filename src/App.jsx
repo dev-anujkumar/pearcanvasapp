03b3630053a0ed5f112075b65e2dd635d67f0363
Kaushik, Arpit <arpit.kaushik@pearson.com>
2021-11-24 10:20:21 +0000

    Tag updated - canvas-1.31.21

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { modifyObjKeys } from './js/appUtils'
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.31.20 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.31.21 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
