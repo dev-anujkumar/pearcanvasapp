fcdc13268c1510445f3e4b3aaae45f5a94f6db18
Pratik Valvi <pratik.valvi@magicedtech.com>
2021-11-29 18:51:01 +0530

    ApiResults test cases fixed

--- __test__/component/AssetPopover/ApiResults.test.js
+++ __test__/component/AssetPopover/ApiResults.test.js
@@ -89,10 +89,14 @@ const figures = [
     }
 ]
 
+let assetPopoverData = {
+    figures: [...figures]
+}
+
 let wrapper;
 
 beforeEach(() => {
-    wrapper = mount(<ApiResults  figures = {figures} ValueToBeSearch = 'i' selectedFigure = {selectedFigure}/>)
+    wrapper = mount(<ApiResults  figures = {figures} ValueToBeSearch = 'i' selectedFigure = {selectedFigure} assetPopoverData={assetPopoverData}/>)
 })
 
 //ApiResults test cases
@@ -102,7 +106,7 @@ describe('Test ApiResults', () => {
             figureDataLength : 2
         });
         expect(wrapper.find('FigureCard').length).toEqual(6); 
-    }),
+    })
     it('cover else case', () => {
         let tempWrapper = mount(<ApiResults  figures = {figures} ValueToBeSearch = 'ioioioi' selectedFigure = {selectedFigure}/>);
         expect(tempWrapper.find('FigureCard').length).toEqual(0); 
