7bc60efcf4bd4112eee7e85ffdd148a94e5ec4ba
Arpit Kaushik <arpit.kaushik@pearson.com>
2021-12-03 13:29:40 +0530

    PCAT-12401 fixed.

--- src/component/tinyMceEditor.js
+++ src/component/tinyMceEditor.js
@@ -968,8 +968,7 @@ export class TinyMceEditor extends Component {
             this.isctrlPlusV = false;
             let activeElement = editor.dom.getParent(editor.selection.getStart(), '.cypress-editable');
             let isMediaElement = tinymce.$(tinymce.activeEditor.selection.getStart()).parents('.figureElement,.interactive-element').length;
-            let isContainsMath = (activeElement && activeElement.innerHTML.match(/<img/)) ? (activeElement.innerHTML.match(/<img/).input.includes('class="Wirisformula') || activeElement.innerHTML.match(/<img/).input.includes('class="temp_Wirisformula')) : false;
-            let isAssetInsideBlockList = (activeElement.innerHTML && activeElement?.innerHTML?.match(/<img/)?.input.includes('class="imageAssetContent'));
+            let isContainsMath = (activeElement && activeElement.innerHTML.match(/<img/)) ? (activeElement.innerHTML.match(/<img/).input.includes('class="Wirisformula') || activeElement.innerHTML.match(/<img/).input.includes('class="temp_Wirisformula')) || activeElement?.innerHTML?.match(/<img/)?.input.includes('class="imageAssetContent') : false;
             let isContainsBlankLine = (activeElement && activeElement.innerHTML.match(/<span/)) ? activeElement.innerHTML.match(/<span/).input.includes('class="answerLineContent') : false;
             if(this.props.element && this.props.element.type==="element-blockfeature" && e.target &&  e.target.className==="blockquoteTextCredit"){
                 setFormattingToolbar('disableTinymceToolbar')
@@ -1007,9 +1006,6 @@ export class TinyMceEditor extends Component {
                     activeElement.classList.remove('place-holder')
                 } else if ((this.props.element.type === 'figure' || this.props.element.type ==="element-aside") && config.figureFieldsPlaceholders.includes(this.props.placeholder)) {
                     activeElement.classList.remove('place-holder');
-                }
-                else if(isAssetInsideBlockList){
-                    activeElement.classList.remove('place-holder')
                 }
                 else {
                     activeElement.classList.add('place-holder')
@@ -1225,11 +1221,6 @@ export class TinyMceEditor extends Component {
                 this.props.createShowHideElement(this.props.showHideType, this.props.index, this.props.id);
             });
             let activeElement = editor.dom.getParent(editor.selection.getStart(), '.cypress-editable');
-            let isAssetInsideBlockList = (activeElement.innerHTML && activeElement?.innerHTML?.match(/<img/)?.input.includes('class="imageAssetContent'));
-
-            if(isAssetInsideBlockList && blockListData && Object.keys(blockListData).length !== 0){
-                activeElement.classList.remove('place-holder')
-            }
             /** [BG-2134] | This block is to clear selection when CT element is blank before paste process*/
             if ((e.keyCode == 86 || e.key == 'v') && e.ctrlKey && this.props.currentElement && this.props.currentElement.type == 'element-citation' && activeElement) {
                 if (activeElement.innerText && activeElement.innerText.trim() == "" && window.getSelection().toString().trim() == '') {        // Other Browsers
