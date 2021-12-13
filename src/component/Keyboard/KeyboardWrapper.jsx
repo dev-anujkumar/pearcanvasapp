48ed6c5a3b613aa073cda4e4e14d7a815437d771
sparsh <sparshgr8@yahoo.com>
2021-12-13 15:29:52 +0530

    Keyboard Consoles removed, Code Refractor.

--- src/component/Keyboard/KeyboardWrapper.jsx
+++ src/component/Keyboard/KeyboardWrapper.jsx
@@ -2,10 +2,12 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectElement } from '../../appstore/keyboardReducer';
 
+export const QUERY_SELECTOR = `cypress-keyboard`;
+
 const KeyboardWrapper = (props) => {
     const dispatch = useDispatch();
     // React.memo vs React.useCallback
-    const id = `cypress-keyboard-${props.index}`;
+    const id = `${QUERY_SELECTOR}-${props.index}`;
     return <div onFocus={() => {
         dispatch(selectElement(id));
     }} id={id}> {props.children} </div>
