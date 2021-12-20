4ca247901f75deeab32ed9c0ab1ffc12df33fb23
Srivastava, Chavi <chavi.srivastava@pearson.com>
2021-12-20 15:36:57 +0000

    update canvas tag canvas-1.32.12.1

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { modifyObjKeys } from './js/appUtils'
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.32.12 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.32.12.1 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
