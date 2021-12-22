d2582d0b1f0414f5378d17b16d41d60db19ef562
Chavi Srivastava <chavi.srivastava@magicsw.com>
2021-12-22 17:30:04 +0530

    remove consoles and work on PR comments

--- src/component/CanvasWrapper/CanvasWrapper_Actions.js
+++ src/component/CanvasWrapper/CanvasWrapper_Actions.js
@@ -535,7 +535,6 @@ export const fetchSlateData = (manifestURN, entityURN, page, versioning, calledF
         dispatch(handleTCMData(tcmManifestUrn));
     }
     dispatch(resetAssessmentStore());//reset Assessment Store
-    config.imageCount = 1
     const elementCount = getState().appStore.slateLength;
     let apiUrl = `${config.REACT_APP_API_URL}v1/slate/content/${config.projectUrn}/${entityURN}/${manifestURN}?page=${page}&elementCount=${elementCount}`
     if (versionPopupReload) {
