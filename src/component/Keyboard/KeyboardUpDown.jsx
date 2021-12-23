7e975aebb155dc781b7511a874c95c3d20a0d012
sparsh <sparshgr8@yahoo.com>
2021-12-23 11:16:37 +0530

    Toolbar Issue Resolved

--- src/component/Keyboard/KeyboardUpDown.jsx
+++ src/component/Keyboard/KeyboardUpDown.jsx
@@ -12,11 +12,7 @@ const KeyboardUpDown = (props) => {
             dispatch(selectElement(element.id));
             const childElement = element.childNodes[1];
             childElement.click();
-            childElement.scrollIntoView({
-                behavior: 'auto',
-                block: 'center',
-                inline: 'center'
-            });
+            // childElement.scrollIntoView();
 
 
         }
