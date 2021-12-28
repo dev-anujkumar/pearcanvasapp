e43df383101d2dbc9d1ff05aeb95de3f0030f7fd
sparsh <sparshgr8@yahoo.com>
2021-12-28 19:38:42 +0530

    Cursor approach change

--- src/component/Keyboard/KeyboardWrapper.jsx
+++ src/component/Keyboard/KeyboardWrapper.jsx
@@ -4,35 +4,109 @@ import { selectElement } from '../../appstore/keyboardReducer';
 
 export const QUERY_SELECTOR = `cypress-keyboard`;
 
+
+const updateCursor = (e, move) => {
+    if (move) {
+        e.preventDefault();
+    }
+    else {
+        e.stopPropagation()
+    }
+}
+
+export const moveCursor = (e, node, tinymceOffset) => {
+    let move;
+    if (e.keyCode === 38) {
+        move = isFirtstChild(node, tinymceOffset);
+        updateCursor(e, move);
+    }
+    else if (e.keyCode === 40) {
+        move = isLastChild(node, tinymceOffset);
+        updateCursor(e, move)
+    }
+    else {
+        e.stopPropagation();
+    }
+}
+
+
+export const isFirtstChild = (node, tinymceOffset) => {
+    console.log("KeyDown Test 51: ", tinymceOffset, node, node?.parentNode, node?.parentNode?.firstChild)
+    if (node === node.parentNode.firstChild) {
+        return tinymceOffset === 0;
+    }
+    else return false;
+}
+
+export const isLastChild = (node, tinymceOffset) => {
+    const isKChild = isKWChild(node);
+    if (isKChild.isChild) {
+        // if (isKChild.index === 1) {
+        console.log("KeyDown Test 21 ", node, isKChild, tinymceOffset);
+        console.log("KeyDown Test 22 ", node.textContent, node.textContent.length, tinymceOffset);
+        console.log("KeyDown Test 23 ", node.firstChild);
+        if (node.parentNode.firstChild === node) {
+            return node.textContent?.length === tinymceOffset
+        }
+        else {
+            console.log("KeyDown Test Other Complex case 31", node, isKChild, tinymceOffset);
+            console.log("KeyDown Test 32 ", node.textContent.length, tinymceOffset);
+            return false;
+            // check if there is no other child
+            // if there is child
+            // 
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+
+
+export const isKWChild = (node, index = 0) => {
+    if (index === 6) {
+        return { isChild: false, index };
+    }
+    else if (node.parentNode.id.startsWith(QUERY_SELECTOR)) {
+        return { isChild: true, index };
+    }
+    else {
+        return isKWChild(node.parentNode, index + 1);
+    }
+}
+
+
+
 const replaceNbsps = (text) => {
-   const removedSpaces  = text.replace(/&nbsp;/g, ' ');
-   return removedSpaces.replace(/<br>/g, '');
+    const removedSpaces = text.replace(/&nbsp;/g, ' ');
+    return removedSpaces.replace(/<br>/g, '');
 }
 
 const getLastString = (text) => {
-    
+
     const lastIndex = text.lastIndexOf("</");
-    if(lastIndex > -1) {
+    if (lastIndex > -1) {
         const lastIndex2 = text.lastIndexOf(">");
-        const lastTag = text.substring(lastIndex, lastIndex2+1);
+        const lastTag = text.substring(lastIndex, lastIndex2 + 1);
         const string = text.split(lastTag);
         // console.log("the string is ", string, string.length, string[0], string[1]);
-        const lastText = string[string.length-1];
+        const lastText = string[string.length - 1];
         return lastText;
     }
     else {
         return text;
     }
     // console.log("Last index is ", lastIndex, lastIndex2, lastTag, lastText);
- }
+}
 
 export const getDataFromLastTag = (text) => {
-//    console.log("the text is ", text.innerHTML, text.firstChild, text.firstChild.innerHTML);
-   const mainString = replaceNbsps(text.innerHTML);
-//    console.log("the main string is ", mainString);
-   return getLastString(mainString);
- 
-//    return text.innerText;
+    //    console.log("the text is ", text.innerHTML, text.firstChild, text.firstChild.innerHTML);
+    const mainString = replaceNbsps(text.innerHTML);
+    //    console.log("the main string is ", mainString);
+    return getLastString(mainString);
+
+    //    return text.innerText;
 }
 
 
@@ -55,11 +129,11 @@ const KeyboardWrapper = (props) => {
 
     // alphanumeric, id should be unique for all the elements.
     const id = `${QUERY_SELECTOR}-${props.index}`;
-    if(props.enable)
-    return <div onFocus={() => {
-        // element is directly clicked via mouse
-        dispatch(selectElement(id));
-    }} id={id}> {props.children} </div>
+    if (props.enable)
+        return <div onFocus={() => {
+            // element is directly clicked via mouse
+            dispatch(selectElement(id));
+        }} id={id}> {props.children} </div>
     else return <>{props.children}</>
 }
 
--- src/component/tinyMceEditor.js
+++ src/component/tinyMceEditor.js
@@ -35,7 +35,7 @@ import { handleC2MediaClick, dataFromAlfresco, checkForDataIdAttribute, checkBlo
 import { saveInlineImageData ,saveSelectedAlfrescoElement } from "../component/AlfrescoPopup/Alfresco_Action.js"
 import { ELEMENT_TYPE_PDF } from './AssessmentSlateCanvas/AssessmentSlateConstants';
 import ElementConstants from './ElementContainer/ElementConstants';
-import { getDataFromLastTag, supportedClasses } from './Keyboard/KeyboardWrapper.jsx';
+import { getDataFromLastTag, isKWChild, isLastChild, moveCursor, supportedClasses } from './Keyboard/KeyboardWrapper.jsx';
 let context = {};
 let clickedX = 0;
 let clickedY = 0;
@@ -1205,79 +1205,40 @@ export class TinyMceEditor extends Component {
             // if current node does not have next element
             // and cursor length is qual to 
             const currentSelection = tinymce?.activeEditor?.selection;
+            const tinymceOffset = currentSelection.getRng().endOffset;
             const currentSelectedNode = currentSelection?.getNode();
-
+            const selectionNode = window.getSelection().anchorNode;
              const parentClass = currentSelectedNode.classList;
             //  console.log("Parent class", parentClass, currentSelection.getRng());
-            if(!this.props.isBlockList) {
-             if(supportedClasses.some(item => parentClass.contains(item))) {
-                 const windowSelection = currentSelection.getRng().endOffset;
-                 const selectionText = currentSelectedNode?.textContent;
-                 const innerHtml = currentSelectedNode.innerText;
-                //  console.log("Selection is 00", selectionText, " inner html ", innerHtml);
-                //  console.log("windowSelection is 00", windowSelection, " inner html ", selectionText.length);
-                if(windowSelection === 0 && e.keyCode === 38) {
-                    
-                    // check if text content matches with starting of inner html
-                    // if yes: prevent default
-                    // if no : stop propogation
-                    // console.log("Inner html ", innerHtml, " Selection ", selectionText)
-                    if(innerHtml.startsWith(selectionText)) {
-                        // console.log("start matched");
-                        e.preventDefault();
-                    }
-                    else {
-                        e.stopPropagation();
-                    }
-                }
-                else if (e.keyCode === 40) {
-                    const lastString= getDataFromLastTag(currentSelectedNode);
-                    // console.log("Inner html 333",lastString, lastString.length, windowSelection);
-                    if(lastString.length === windowSelection) {
-                        // console.log("last matched");
-                        if(windowSelection === 0 && innerHtml.length > 0) {
-                            e.stopPropagation();
-                        }
-                        else {
-                            e.preventDefault();
-                        }
-                    }
-                    else {
-                        e.stopPropagation();
-                    }
-                    // check if text matches the end of inner html
-                    // if yes : prevent default
-                    // if no : stop propogation
-                }
-                else {
-                    e.stopPropagation();
-                }
+           
+                console.log("KeyDown Test 1, active node ", currentSelectedNode);
+                console.log("KeyDown Test 2, active node ", selectionNode);
+                console.log("KeyDown Test 3, child nodes ", currentSelectedNode.childNodes);
+                console.log("KeyDown Test 4, Selected node", selectionNode.childNodes);
 
-                // selection is inside text;
-             }
-             else {
-                 // selection is in other elements
-                //  console.log("Seleciton is in other elements", currentSelectedNode.parentNode);
-                const parent = currentSelectedNode.parentNode;
-                const parentHTML = parent.innerText;
-                const childHTML = currentSelectedNode.innerText;
-                // console.log("Parent ",parentHTML , "child innder html ", childHTML, " ", e.keyCode);
+    
+                console.log("KeyDown Test 8, Parent node", selectionNode.parentNode);
+                console.log("KeyDown Test 9, Parent node", currentSelectedNode.parentNode);
+
+                console.log("KeyDown Test 10, Parent node", currentSelectedNode.parentNode.id);
+                console.log("KeyDown Test 11, Parent node", selectionNode.parentNode.id);
                 
-                if(parentHTML.endsWith(childHTML) && e.keyCode === 40) {
-                    e.preventDefault();
-                }
-                else if (parentHTML.startsWith(childHTML) && e.keyCode === 38){
-                    // move to previous element
-                    e.preventDefault();
-                }
-                else {
-                    // do normal flow
-                    e.stopPropagation();
-                }
-             }
-            }else {
-                e.stopPropagation();
-            }
+                console.log("KeyDown Test 12, Grand Parent node", currentSelectedNode.parentNode.parentNode.id);
+                console.log("KeyDown Test 13, Grand Parent node", selectionNode.parentNode.parentNode.id);
+
+
+                console.log("KeyDown Test 14, selectin value", currentSelection.getRng().endOffset);
+                console.log("KeyDown Test 15, selectionvalue", window.getSelection().anchorOffset);
+
+                // check if node and all the parents of node are end child
+                
+                moveCursor(e, currentSelectedNode, tinymceOffset);
+                // if(isKWChild(currentSelectedNode)) {
+                //     e.preventDefault();
+                // }
+                // else {
+                //     e.stopPropagation();
+                // }
             
             /* xxxxxxxxxxxxxxxxx Prevent CTA button keyboard formatting START xxxxxxxxxxxxxxxxx */
             if (config.ctaButtonSmartlinkContexts.includes(this.props?.element?.figuredata?.interactivetype) && this.props?.className === "actionPU hyperLinkText" && this.props?.placeholder === "Enter Button Label") {
