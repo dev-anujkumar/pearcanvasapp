978c765da5e91662cc848015dfb77782336bd509
sparsh <sparshgr8@yahoo.com>
2021-12-30 00:11:41 +0530

    Scroll logic improved. and empty spaces removed

--- src/component/Keyboard/KeyboardUpDown.jsx
+++ src/component/Keyboard/KeyboardUpDown.jsx
@@ -7,27 +7,46 @@ const KeyboardUpDown = (props) => {
     const keyboardUpDown = useRef(null);
     const activeElement = useSelector(state => state.keyboardReducer.selectedElement);
     const dispatch = useDispatch();
-    const getChildAndClick = (element, enableScroll) => {
+
+    const isNodeAvaiable = (index) => {
+        const allInteractiveElements = document.querySelectorAll(`[id^='${QUERY_SELECTOR}-']`);
+        return allInteractiveElements[index];
+    }
+
+    const getChildAndClick = (element, index) => {
+
+        const parentNode = keyboardUpDown.current.parentNode;
+        const divHeight = parentNode.getBoundingClientRect().height;
         if (element) {
             dispatch(selectElement(element.id));
             const childElement = element.childNodes[1];
-            if(enableScroll) {
-                const rect = element.getBoundingClientRect();
-                const parentNode = keyboardUpDown.current.parentNode;
-                const divHeight = parentNode.getBoundingClientRect().height;
-                parentNode.scroll(0, parentNode.top + divHeight);
-            }
+            const scrollTo = element.getBoundingClientRect().top - divHeight / 3;
+            parentNode.scrollBy(0, scrollTo);
             childElement.click();
-          
+
+        }
+        else {
+            // element not there 
+            // in case of down arrow press
+            if (parentNode.scrollHeight > parentNode.scrollTop + divHeight) {
+                parentNode.scrollBy(0, divHeight);
+                // check if there is a new node avaiable now,
+                // if yes click it.
+                const newNode = isNodeAvaiable(index + 1);
+                if(newNode) {
+                    getChildAndClick(newNode, index+1)
+                }
+            }
         }
     }
 
     const shouldEnableScroll = (selectedNodeIndex, allElementLength) => {
-        return allElementLength ;
+        return allElementLength;
     }
 
     const handleKeyDown = (event) => {
         if (event.keyCode === 38 || event.keyCode === 40) {
+
             const allInteractiveElements = document.querySelectorAll(`[id^='${QUERY_SELECTOR}-']`);
             if (activeElement) {
                 let selectedNodeIndex = 0;
@@ -42,7 +61,7 @@ const KeyboardUpDown = (props) => {
                 }
                 else if (event.keyCode === 40 && selectedNodeIndex !== allInteractiveElements.length) {
                     const enableScroll = shouldEnableScroll(selectedNodeIndex, allInteractiveElements.length);
-                    getChildAndClick(allInteractiveElements[selectedNodeIndex + 1], enableScroll);
+                    getChildAndClick(allInteractiveElements[selectedNodeIndex + 1], enableScroll, selectedNodeIndex);
                 }
             }
         }
