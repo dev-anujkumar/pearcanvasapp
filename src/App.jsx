b940b436bc7fe9caee063629c181cdc246b66dfa
Shukla, Akash <akash.shukla2@pearson.com>
2022-01-05 10:39:50 +0000

    Tag updated - canvas-1.32.46.2

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { modifyObjKeys } from './js/appUtils'
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.32.46.1 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.32.46.2 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
