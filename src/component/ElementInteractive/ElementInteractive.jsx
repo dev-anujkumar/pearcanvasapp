068667a898e7143d5a4ba85a922a234719b35a51
Chavi Srivastava <chavi.srivastava@magicsw.com>
2022-01-20 15:11:25 +0530

    PCAT-12866 handle avs-jsonstring as both string and object in smartlinks alfresco

--- src/component/ElementInteractive/ElementInteractive.jsx
+++ src/component/ElementInteractive/ElementInteractive.jsx
@@ -529,7 +529,8 @@ class Interactive extends React.Component {
         let smartLinkPath = imageData.properties["avs:url"] ? imageData.properties["avs:url"] : "";
         let smartLinkString = (imageData.properties["cm:description"] && imageData.properties["cm:description"].toLowerCase() !== "eps media") ? imageData.properties["cm:description"] : "{}";
         let isSmartLinkAsset = smartLinkString !== "{}" && (smartLinkString.includes("smartLinkType") || !(imageData.hasOwnProperty('content'))) ? true :  false
-        let smartLinkDesc = (isSmartLinkAsset === true)? JSON.parse(smartLinkString) : "";
+        let smartlinkAvsString = (isSmartLinkAsset === true) ? smartLinkString : {}
+        let smartLinkDesc = (typeof smartlinkAvsString === 'string')? JSON.parse(smartlinkAvsString) : smartlinkAvsString;
         let smartLinkType = smartLinkDesc !== "" ? smartLinkDesc.smartLinkType : "";
         let avsStringData =imageData.properties["avs:jsonString"]&& JSON.parse(imageData.properties["avs:jsonString"]);
         let altText = avsStringData?.imageAltText ? avsStringData.imageAltText : "";
