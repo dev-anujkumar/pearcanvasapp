d5a85b93e5e75d90ec0f7e47c37cf7440f05c2a5
tusharkhandejod <tusharkhandejod@gmail.com>
2022-02-18 17:19:39 +0530

    test cases for CiteTdxTable file is added

--- __test__/component/AssessmentSlateCanvas/citeTdx/CiteTdxTable.test.js
+++ __test__/component/AssessmentSlateCanvas/citeTdx/CiteTdxTable.test.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Provider } from 'react-redux';
 import { mount } from 'enzyme';
@@ -12,9 +13,9 @@ let initialState = {
             "versionUrn":"dfer",
             "name":"mmoi"
         },
-        citeApiData:{assessments:{"dfsarfw":"Sdfa"}},
-        tdxApiData:{assessments:{"dfsarfw":"Sdfa"}},
-        mmiApiData:{assessments:{"dfsarfw":"Sdfa"}},
+        citeApiData:{assessments:["dfsarfw","Sdfa"]},
+        tdxApiData:{assessments:["dfsarfw","Sdfa"]},
+        mmiApiData:{assessments:["dfsarfw","Sdfa"]},
         isLoading:false,
         currentSingleAssessmentSelected:{},
         citeErrorFlag:"",
@@ -23,14 +24,16 @@ let initialState = {
 
  },
 };
+
+
 describe('Table component', () => {
     let props = {
         assessmentType:"Full Assessment QuAD CITE",
         assessmenterrFlag:false,
         isLoading:false,
-        citeApiData:{assessments:{"dfsarfw":"Sdfa"}},
-        tdxApiData:{assessments:{"dfsarfw":"Sdfa"}},
-        mmiApiData:{assessments:{"dfsarfw":"Sdfa"}},
+        citeApiData:{assessments:["dfsarfw","Sdfa"]},
+        tdxApiData:{assessments:["dfsarfw","Sdfa"]},
+        mmiApiData:{assessments:["dfsarfw","Sdfa"]},
         sortingData:{sortBy:"modifiedDate"}
 
     }
@@ -60,6 +63,14 @@ describe('Table component', () => {
         expect(spysetSorting).toHaveBeenCalled()
         spysetSorting.mockClear()
     })
+ 
+    it('checking sorting false case', () => { 
+        const spysetSorting = jest.spyOn(componentInstance, 'setSort')
+        componentInstance.setSort("");
+       expect(spysetSorting).toHaveBeenCalled()
+       spysetSorting.mockClear()
+   })
+
 
     it('TDX', () => {
         let props = {
@@ -93,6 +104,180 @@ describe('Table component', () => {
         const component = mount(<Provider store={store}><CiteTdxTable {...props} /></Provider>);
         expect(component).toHaveLength(1);
     })
+
+    it("Assign Click checking whether setSort is called", () => {
+        let addValue = {};
+        const wrapperInstance = component.find('CiteTdxTable').instance();
+        const spyOnupdateAssignee = jest.spyOn(wrapperInstance, 'setSort')
+        wrapperInstance.setSort(addValue);
+        expect(spyOnupdateAssignee).toHaveBeenCalled()
+    });
     
+    it("Assign Click checking whether addAssessment is called", () => {
+        let addValue = {};
+        const wrapperInstance = component.find('CiteTdxTable').instance();
+        const spyOnupdateAssignee = jest.spyOn(wrapperInstance, 'addAssessment')
+        wrapperInstance.addAssessment(addValue);
+        expect(spyOnupdateAssignee).toHaveBeenCalled()
+    });
+
+
+    let initialState1 = {
+        citeTdxReducer:{ 
+            currentAssessmentSelected:{
+                "versionUrn":"dfer",
+                "name":"mmoi"
+            },
+            citeData: {
+                page: 0,
+                pageSize: 25,
+                numberOfRemainingElements: 22142,
+                assessments: [
+                    {
+                        "versionUrn": "urn:pearson:work:45a9474c-d21f-4d4e-9ff0-49727e06e8d8",
+                        "name": "ZXzXCzx",
+                        "modifiedDate": "2021-06-28T12:36:30.408Z",
+                        "modifiedBy": "LOMT-ADMIN"
+                    },
+                    {
+                        "versionUrn": "urn:pearson:work:71554927-8614-46a1-8235-b988b250eb44",
+                        "name": "ZXzXCzx",
+                        "modifiedDate": "2022-02-15T11:29:25.844Z",
+                        "modifiedBy": "SERV.C4SMWIPNONPROD"
+                    }
+                ],
+            },
+            tdxData : {
+                page: 1,
+                pageSize: 20,
+                numberOfRemainingElements: 22142,
+                assessments: [
+                    {
+                        "versionUrn": "urn:pearson:work:45a9474c-d21f-4d4e-9ff0-49727e06e8d8",
+                        "name": "ZXzXCzx",
+                        "modifiedDate": "2021-06-28T12:36:30.408Z",
+                        "modifiedBy": "LOMT-ADMIN"
+                    },
+                    {
+                        "versionUrn": "urn:pearson:work:71554927-8614-46a1-8235-b988b250eb44",
+                        "name": "ZXzXCzx",
+                        "modifiedDate": "2022-02-15T11:29:25.844Z",
+                        "modifiedBy": "SERV.C4SMWIPNONPROD"
+                    }
+                ],
+            },
+            mmiData: {
+                page: 2,
+                pageSize: 15,
+                numberOfRemainingElements: 22142,
+                assessments: [
+                    {
+                        "versionUrn": "urn:pearson:work:45a9474c-d21f-4d4e-9ff0-49727e06e8d8",
+                        "name": "ZXzXCzx",
+                        "modifiedDate": "2021-06-28T12:36:30.408Z",
+                        "modifiedBy": "LOMT-ADMIN"
+                    },
+                    {
+                        "versionUrn": "urn:pearson:work:71554927-8614-46a1-8235-b988b250eb44",
+                        "name": "ZXzXCzx",
+                        "modifiedDate": "2022-02-15T11:29:25.844Z",
+                        "modifiedBy": "SERV.C4SMWIPNONPROD"
+                    }
+                ],
+            },
+            isLoading:false,
+            currentSingleAssessmentSelected:{},
+            citeErrorFlag:"",
+            assessmenterrFlag:false,
+            sortBy:"modifiedDate"
+    
+     },
+    }
+    
+    let store1 = mockStore(initialState1);
+
+
+    it('checking for rendering table when assessmentType=cite is passed ', () => {
+
+         let props1 = {
+            assessmentType:"cite",
+            citeData: JSON.stringify({assessments:["cite","abc"]}),
+            tdxApiData : JSON.stringify({assessments:["tdx","def"]}),
+            mmiApiData: JSON.stringify({assessments:["mmi","hij"]}),
+        }
+ 
+        const component = mount(<Provider store={store1}>
+                                   <CiteTdxTable {...props1} />
+                                </Provider>)
+        console.log(component.debug())
+        expect(component.find('.assessment-table-class')).toHaveLength(1)
+    })
+
+
+    it('checking for rendering table when assessmentType=tdx is passed ', () => {
+
+        let props2 = {
+            assessmentType:"tdx",
+            citeData: JSON.stringify({assessments:["cite","abc"]}),
+            tdxApiData : JSON.stringify({assessments:["tdx","def"]}),
+            mmiApiData: JSON.stringify({assessments:["mmi","hij"]}),
+        }
+ 
+        const component = mount(<Provider store={store1}>
+                                   <CiteTdxTable {...props2} />
+                                </Provider>)
+        console.log(component.debug())
+        expect(component.find('.assessment-table-class')).toHaveLength(1)
+    })
+
+    it('checking for div with classname no results is rendered', () => {
+
+        
+    let initialState2 = {
+        citeTdxReducer:{ 
+            currentAssessmentSelected:{
+                "versionUrn":"dfer",
+                "name":"mmoi"
+            },
+            citeData: {
+                page: 0,
+                pageSize: 25,
+                numberOfRemainingElements: 22142,
+                assessments: [],
+            },
+            tdxData : {
+                page: 1,
+                pageSize: 20,
+                numberOfRemainingElements: 22142,
+                assessments: [],
+            },
+            mmiData: {
+                page: 2,
+                pageSize: 15,
+                numberOfRemainingElements: 22142,
+                assessments: []},
+            isLoading:false,
+            currentSingleAssessmentSelected:{},
+            citeErrorFlag:"",
+            assessmenterrFlag:false,
+            sortBy:"modifiedDate"
+    
+     },
+    }
+
+    let store2 = mockStore(initialState2);
+        let props2 = {
+            assessmentType:"tdx"
+        }
+ 
+        const component = mount(<Provider store={store2}>
+                                   <CiteTdxTable {...props2} />
+                                </Provider>)
+        console.log(component.debug())
+        expect(component.find('.no-result')).toHaveLength(1)
+    })
+
+
+
 
 });
