2825fdeea97fd8d3a8a8cfe33d85e99b39f964d8
Gupta, Ankit <ankit.gupta1@pearson.com>
2022-02-18 10:37:15 +0000

    update canvas-1.34.17 tag

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { modifyObjKeys } from './js/appUtils'
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.34.16 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.34.17 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
