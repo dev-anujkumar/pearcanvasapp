580896af5997059b832fc4bc4ce1c6316c8fc266
Kumar Anuj <kumar.anuj@magicsw.com>
2022-03-23 18:18:56 +0530

    increased branch coverage

--- __test__/testData/slateAncestorMockData.js
+++ __test__/testData/slateAncestorMockData.js
@@ -183,6 +183,119 @@ export const allSlateData2 = {
     ]
 }
 
+export const allSlateData5 = {
+    "frontmatter": [
+        {
+            "containerUrn": "urn:pearson:manifest:d15504e0-4673-4d01-a55d-ae1b3acc4f0a",
+            "entityUrn": "urn:pearson:entity:59be1726-72f7-41a2-b4cd-c011e02eb852",
+            "title": "",
+            "label": "module",
+            "matterType": "frontmatter",
+            "contents": [
+                {
+                    "containerUrn": "urn:pearson:manifest:212179e4-93ff-4e1a-bc11-da6790ea2abb",
+                    "entityUrn": "urn:pearson:entity:60b05c81-2336-4407-852d-9af8178fddad",
+                    "title": "",
+                    "label": "section"
+                }
+            ]
+        }
+    ],
+    "bodymatter": [
+        {
+            "containerUrn": "urn:pearson:manifest:09630d62-5da1-407f-9d64-7ba6343999f8",
+            "entityUrn": "urn:pearson:entity:f07aed30-c520-430d-bc4c-e02dfb3b2553",
+            "title": "",
+            "label": "volume",
+            "matterType": "bodymatter",
+            "contents": [
+                {
+                    "containerUrn": "urn:pearson:manifest:09630d62-5da1-407f-9d64-7ba6343999f8",
+                    "entityUrn": "urn:pearson:entity:f07aed30-c520-430d-bc4c-e02dfb3b2554",
+                    "title": "",
+                    "label": "chapter",
+                    "matterType": "bodymatter",
+                    "contents": [
+                        {
+                            "containerUrn": "urn:pearson:manifest:e0f34dc8-9de9-4647-b4a0-f481e4e4e603",
+                            "entityUrn": "urn:pearson:entity:2af5690c-c2d9-4a0a-9104-a4f9a262878d",
+                            "title": "",
+                            "label": "container-introduction"
+                        },
+                        {
+                            "containerUrn": "urn:pearson:manifest:d366b187-2137-4f4b-9f6e-95ef26e8e86e",
+                            "entityUrn": "urn:pearson:entity:4807ec41-4820-4e9f-a350-3e18c16179a0",
+                            "title": "",
+                            "label": "assessment-slate"
+                        },
+                        {
+                            "containerUrn": "urn:pearson:manifest:84ebc18e-d28c-49c2-8ab1-2596c3d05fb5",
+                            "entityUrn": "urn:pearson:entity:fcba2d6f-26a1-48c7-89ab-ac6a9ae108d6",
+                            "title": "",
+                            "label": "section"
+                        },
+                        {
+                            "containerUrn": "urn:pearson:manifest:f8149bcb-094f-4c5a-8ad8-86be326fa419",
+                            "entityUrn": "urn:pearson:entity:bbd45705-8f20-4773-9fbc-7a375353f711",
+                            "title": "",
+                            "label": "module",
+                            "contents": [
+                                {
+                                    "containerUrn": "urn:pearson:manifest:4f5e6ba9-542d-4641-8a39-220efdb3aa03",
+                                    "entityUrn": "urn:pearson:entity:ac872d58-1699-4599-b7db-938e161172c7",
+                                    "title": "",
+                                    "label": "section"
+                                }
+                            ]
+                        }
+                    ]
+                },        
+                {
+                    "containerUrn": "urn:pearson:manifest:e0f34dc8-9de9-4647-b4a0-f481e4e4e603",
+                    "entityUrn": "urn:pearson:entity:2af5690c-c2d9-4a0a-9104-a4f9a262878d",
+                    "title": "",
+                    "label": "container-introduction"
+                },
+                {
+                    "containerUrn": "urn:pearson:manifest:d366b187-2137-4f4b-9f6e-95ef26e8e86e",
+                    "entityUrn": "urn:pearson:entity:4807ec41-4820-4e9f-a350-3e18c16179a0",
+                    "title": "",
+                    "label": "assessment-slate"
+                },
+                {
+                    "containerUrn": "urn:pearson:manifest:84ebc18e-d28c-49c2-8ab1-2596c3d05fb5",
+                    "entityUrn": "urn:pearson:entity:fcba2d6f-26a1-48c7-89ab-ac6a9ae108d6",
+                    "title": "",
+                    "label": "section"
+                },
+                {
+                    "containerUrn": "urn:pearson:manifest:f8149bcb-094f-4c5a-8ad8-86be326fa419",
+                    "entityUrn": "urn:pearson:entity:bbd45705-8f20-4773-9fbc-7a375353f711",
+                    "title": "",
+                    "label": "module",
+                    "contents": [
+                        {
+                            "containerUrn": "urn:pearson:manifest:4f5e6ba9-542d-4641-8a39-220efdb3aa03",
+                            "entityUrn": "urn:pearson:entity:ac872d58-1699-4599-b7db-938e161172c7",
+                            "title": "",
+                            "label": "section"
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    "backmatter": [
+        {
+            "containerUrn": "urn:pearson:manifest:4ed94cfc-766d-49c5-870e-074b4074da0f",
+            "entityUrn": "urn:pearson:entity:d0922165-b29b-444f-a318-ad739ec81ee0",
+            "title": "",
+            "label": "section",
+            "matterType": "backmatter"
+        }
+    ]
+}
+
 export const slateList = [
     "urn:pearson:entity:2af5690c-c2d9-4a0a-9104-a4f9a262878d",
     "urn:pearson:entity:fcba2d6f-26a1-48c7-89ab-ac6a9ae108d6",
@@ -373,7 +486,18 @@ export const allSlateData4 = {
                     "entityUrn": "urn:pearson:entity:2af5690c-c2d9-4a0a-9104-a4f9a262878d",
                     "title": "",
                     "label": "part",
-                    "contents":[1,2]
+                    "contents":[{
+                        "containerUrn": "urn:pearson:manifest:63162bff-38c7-4885-b071-fc842f5bdc28",
+                        "entityUrn": "urn:pearson:entity:03f6a4e3-7acc-4b7f-8341-ad2ef9e37ea3",
+                        "title": "",
+                        "label": "container-introduction"
+                    },{
+                        "containerUrn": "urn:pearson:manifest:63162bff-38c7-4885-b071-fc842f5bdc28",
+                        "entityUrn": "urn:pearson:entity:03f6a4e3-7acc-4b7f-8341-ad2ef9e37ea3",
+                        "title": "",
+                        "label": "section"
+                    }
+                ]
                 },
                 {
                     "containerUrn": "urn:pearson:manifest:d366b187-2137-4f4b-9f6e-95ef26e8e86e",
