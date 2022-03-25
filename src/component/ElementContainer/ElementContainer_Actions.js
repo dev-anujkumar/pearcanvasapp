b34bdde6842cc233d2d1c3f73ab05245349add05
Ankur Khandelwal <ankur.khandelwal@pearson.com>
2022-03-25 17:51:42 +0530

    Added test cases for PCAT-11325

--- src/component/ElementContainer/ElementContainer_Actions.js
+++ src/component/ElementContainer/ElementContainer_Actions.js
@@ -1068,7 +1068,6 @@ const getAltTextLongDesc = async (id) => {
             longdescription: properties["cplg:longDescription"]
         }
     } catch(error){
-        console.log(" error: ", error)
         return { 
             altText : "",
             longdescription: ""
@@ -1115,7 +1114,7 @@ export const saveTEMetadata = async (editedImageList) => {
                         'myCloudProxySession': config.myCloudProxySession
                     }
                 });
-    
+    console.log("=====> response: ", response)
                 promiseArray.push(response);
             }
             await Promise.all(promiseArray);
