2e11179f87022d08594514d591853ba03b8938a9
Shruti Verma <shruti.verma@magicsw.com>
2022-06-09 12:46:11 +0530

    test cases

--- __test__/appstore/glossaryFootnoteReducer.test.js
+++ __test__/appstore/glossaryFootnoteReducer.test.js
@@ -1,32 +1,54 @@
 import glossaryFootnoteReducer from '../../src/appstore/glossaryFootnoteReducer';
 
-const initialState = {
-    glossaryFootnoteValue: '',
-    glossaryFootNoteCurrentValue : '',
-    elementIndex :  0
+const INITIAL_STATE = {
+    glossaryFootnoteValue:{"type":"","popUpStatus":false},
+    glossaaryFootnoteValue: '',
+    glossaryFootNoteCurrentValue:'',
+    elementIndex : ''
 };
 
 const glossaryFootnoteData = { 
     glossaaryFootnoteValue: 'testing footnote value',
+    glossaryFootnoteValue: 'testing footnote value',
     glossaryFootNoteCurrentValue : 'checking footnote current value',
     elementIndex :  2 
 }
 
 describe('testing glossary Footnote Reducer cases --', () => {
     it('should return the initial state', () => {
-        expect(glossaryFootnoteReducer(initialState, {
+        expect(glossaryFootnoteReducer(INITIAL_STATE, {
 
-        })).toEqual(initialState);
+        })).toEqual(INITIAL_STATE);
     });
     it('get glossaryFootnoteValue', () => {
         let expectedState = {
-            ...initialState,
+            ...INITIAL_STATE,
             glossaryFootnoteValue: 'testing footnote value',
             glossaryFootNoteCurrentValue : 'checking footnote current value',
             elementIndex :  2 
         }
-        expect(glossaryFootnoteReducer(initialState, {
+        expect(glossaryFootnoteReducer(INITIAL_STATE, {
             type: "OPEN_GLOSSARY_FOOTNOTE",
+            payload: glossaryFootnoteData            
+        })).toEqual(expectedState);
+    })
+    it('get glossaryFootnoteValue', () => {
+        let expectedState = {
+            ...INITIAL_STATE,
+            glossaryFootnoteValue: 'testing footnote value'
+        }
+        expect(glossaryFootnoteReducer(INITIAL_STATE, {
+            type: "UPDATE_NEW_ELEMENT_WORK_ID",
+            payload: glossaryFootnoteData            
+        })).toEqual(expectedState);
+    })
+    it('get glossaryFootnoteValue', () => {
+        let expectedState = {
+            ...INITIAL_STATE,
+            glossaryFootNoteCurrentValue : 'checking footnote current value',
+        }
+        expect(glossaryFootnoteReducer(INITIAL_STATE, {
+            type: "UPDATE_CURRENT_VALUE",
             payload: glossaryFootnoteData            
         })).toEqual(expectedState);
     })
