8bed0855df6494d45ad05aa48441419bb68b316e
Pratik Valvi <pratik.valvi@pearson.com>
2022-06-14 13:01:58 +0530

    PCAT-15382 Few more testcases of searchReducer

--- __test__/appstore/searchReducer.test.js
+++ __test__/appstore/searchReducer.test.js
@@ -1,4 +1,5 @@
-import { searchReducer } from "../../src/appstore/searchReducer";
+import { searchReducer, commentSearchReducer } from "../../src/appstore/searchReducer";
+
 const INITIAL_STATE = {
     searchTerm: '',
     parentId: '',
@@ -23,7 +24,7 @@ const payload1 = {
     scrollTop: 142
 }
 
-const expectedSlate1 = {
+const expectedState1 = {
     searchTerm: "urn:pearson:work:4aebbd03-7626-4658-8f35-e78df2d2bd44",
     parentId: "urn:pearson:work:4aebbd03-7626-4658-8f35-e78df2d2bd44",
     deeplink: false, 
@@ -31,13 +32,40 @@ const expectedSlate1 = {
     scrollTop: 142
 }
 
-describe("Testcases for searchReducer", () => {
-    it("Should return INITIAL_STATE", () => {
-        expect(searchReducer(INITIAL_STATE, {})).toBe(INITIAL_STATE);
-    });
+const payload2 = {
+    type: "SET_COMMENT_SEARCH_URN",
+    payload: "urn:pearson:work:4aebbd03-7626-4658-8f35-e78df2d2bd44",
+    parent: "urn:pearson:work:4aebbd03-7626-4658-8f35-e78df2d2bd44",
+    scroll: true,
+    scrollTop: 142
+}
 
-    it("Test1 - SET_SEARCH_URN", () => {
-        const result = searchReducer(INITIAL_STATE, payload1);
-        expect(result).toEqual(expectedSlate1);
+const expectedState2 = {
+    commentSearchTerm: "urn:pearson:work:4aebbd03-7626-4658-8f35-e78df2d2bd44",
+    parentId: "urn:pearson:work:4aebbd03-7626-4658-8f35-e78df2d2bd44", 
+    scroll: true,
+    scrollTop: 142
+}
+
+describe("Testing searchReducer", () => {
+    describe("Testcases for searchReducer", () => {
+        it("Should return INITIAL_STATE", () => {
+            expect(searchReducer(INITIAL_STATE, {})).toBe(INITIAL_STATE);
+        });
+    
+        it("Test1 - SET_SEARCH_URN", () => {
+            const result = searchReducer(INITIAL_STATE, payload1);
+            expect(result).toEqual(expectedState1);
+        });
+    });
+    describe("Testcases for commentSearchReducer", () => {
+        it("Should return INITIAL_COMMENT_STATE", () => {
+            expect(commentSearchReducer(INITIAL_COMMENT_STATE, {})).toBe(INITIAL_COMMENT_STATE);
+        });
+    
+        it("Test1 - SET_COMMENT_SEARCH_URN", () => {
+            const result = commentSearchReducer(INITIAL_COMMENT_STATE, payload2);
+            expect(result).toEqual(expectedState2);
+        });
     });
 });
