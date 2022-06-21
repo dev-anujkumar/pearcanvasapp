71e8308e3801f403a52705f52a60eea0c68bab98
Jaya Singh <jaya.singh1@pearson.com>
2022-06-21 11:22:28 +0000

    App.jsx edited online with Bitbucket

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { modifyObjKeys } from './js/appUtils'
 import './styles/style.css';
 
 
-console.log("!!!!! ---- canvas-1.38.7 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.38.8 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
