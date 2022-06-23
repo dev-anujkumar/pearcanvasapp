9dc64794810f6c3437069753e7499e81ce4665fa
Ritvik Sethi <ritvik.sethi@pearson.com>
2022-06-23 15:04:15 +0530

    minor changes

--- src/component/HOCs/WrapperChannel/CommunicationChannel.jsx
+++ src/component/HOCs/WrapperChannel/CommunicationChannel.jsx
@@ -148,8 +148,7 @@ function CommunicationChannel(WrappedComponent) {
                     this.props.getProjectDetails()
                     this.props.fetchProjectLFs()
                     this.props.tcmCosConversionSnapshot()       // for creation of pre-snapshots for cos converted projects
-                    this.props.fetchUserLocation() //Do not remove: needed in future
-                    break;
+                    this.props.fetchUserLocation() 
                 case 'permissionsDetails':
                     this.handlePermissioning(message);
                     break;
