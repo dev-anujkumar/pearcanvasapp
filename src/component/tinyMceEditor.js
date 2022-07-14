a95fbe9aa22659eccf82c719a83793533a5d7c92
Kazim Raja <kazim.raja@pearson.com>
2022-07-14 15:19:02 +0530

    PCAT-14882 Text to retain formatting when it's Cut Copy & Pasted

--- src/component/tinyMceEditor.js
+++ src/component/tinyMceEditor.js
@@ -23,7 +23,7 @@ import { getGlossaryFootnoteId } from "../js/glossaryFootnote";
 import { checkforToolbarClick, customEvent, spanHandlers, removeBOM, getWirisAltText, removeImageCache, removeMathmlImageCache } from '../js/utils';
 import { saveGlossaryAndFootnote, setFormattingToolbar } from "./GlossaryFootnotePopup/GlossaryFootnote_Actions";
 import { ShowLoader, LaunchTOCForCrossLinking } from '../constants/IFrameMessageTypes';
-import { sendDataToIframe, hasReviewerRole, removeBlankTags } from '../constants/utility.js';
+import { sendDataToIframe, hasReviewerRole, removeBlankTags, handleTextToRetainFormatting } from '../constants/utility.js';
 import store from '../appstore/store';
 import { MULTIPLE_LINE_POETRY_ERROR_POPUP } from '../constants/Action_Constants';
 import { ERROR_CREATING_GLOSSARY, ERROR_CREATING_ASSETPOPOVER, MANIFEST_LIST, MANIFEST_LIST_ITEM, TEXT, ERROR_DELETING_MANIFEST_LIST_ITEM } from '../component/SlateWrapper/SlateWrapperConstants.js';
@@ -2371,8 +2371,13 @@ export class TinyMceEditor extends Component {
         let testElement = document.createElement('div');
         testElement.innerHTML = args.content;
         if (testElement.innerText.trim().length) {
-            let tempContent = testElement.innerText.replace(/&/g, "&amp;");
-            args.content = tempContent.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+            console.log("test", args.content)
+            if ((this.props?.element?.type === "element-authoredtext") && !this.props?.element?.elementdata?.headers && (this.props?.element?.elementdata?.designtype !== 'handwritingstyle')) {
+                args.content = handleTextToRetainFormatting(args.content, testElement)
+            } else {
+                let tempContent = testElement.innerText.replace(/&/g, "&amp;");
+                args.content = tempContent.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+            }
         } else {
             args.content = tinymce.activeEditor.selection.getContent();
         }
