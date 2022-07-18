4fa0032b3dac4ea8b5d9d63dd57b2607a0544baa
Tushar Shivaji Khandejod <tushar.khandejod@magicsw.com>
2022-07-18 10:43:32 +0530

    Test cases for file Search_Action.js is added

--- __test__/component/Toolbar/Search/Search_Action.test.js
+++ __test__/component/Toolbar/Search/Search_Action.test.js
@@ -37,6 +37,33 @@ config.slateEntityURN = "urn:pearson:entity:d68e34b0-0bd9-4e8b-9935-e9f0ff83d1fb
 config.slateManifestURN = "urn:pearson:manifest:d91706aa-0e9b-4015-aaef-fb3a9cf46ec0"
 config.projectUrn = "urn:pearson:distributable:977c95a8-e16a-413c-bfd0-788fd2a3698d"
 
+const DefaultAssessmentSlateData = {
+    "urn:pearson:manifest:d9023151-3417-4482-8175-fc965466220e": {
+        "contentUrn": "urn:pearson:entity:c8240c45-ba81-4a8a-8f9e-32b68108eb4e",
+        "id": "urn:pearson:manifest:3c780b1f-06ad-4e3d-b226-6775cba97b29",
+        "schema": "http://schemas.pearson.com/wip-authoring/manifest/1",
+        "type": "manifest",
+        "versionUrn": "urn:pearson:manifest:3c780b1f-06ad-4e3d-b226-6775cba97b29",
+        "contents": {
+            "backmatter": [],
+            "frontmatter": [],
+            "schema": "http://schemas.pearson.com/wip-authoring/manifest/1#/definitions/manifest",
+            "bodymatter": [
+                {
+                    "id":"urn:pearson:work:e09f9098-bc7a-410b-9619-c372102cd5b9",
+                    "type":"element-authoredtext",
+                    "contentUrn":"urn:pearson:entity:2e1a0320-b129-4fb9-920c-e8ce5f4bbc3c"
+                },
+                {
+                    "id":"urn:pearson:work:7af88fe2-0f49-4b28-8f2d-87134201fd9b",
+                    "type":"element-authoredtext",
+                    "contentUrn":"urn:pearson:entity:2e1a0320-b129-4fb9-920c-e8ce5f4bbc3c"
+                },
+            ]
+        }
+    }
+}
+
 describe('-----------------Testing Search Actions-----------------', () => {
     const mockdata = {
         "urn:pearson:manifest:c565f350-b712-41ef-823a-a66baffa0b89": {
@@ -136,6 +163,52 @@ describe('-----------------Testing Search Actions-----------------', () => {
             expect(spyFunction).toHaveBeenCalled();
             spyFunction.mockClear();
         });
+        it('if condition check', () => {
+            let scrollArgs = {
+                type : 'SET_SEARCH_URN',
+                scrollTop : 0
+            }
+            let getState = () => {
+                return {
+                    searchReducer: {
+                        slateLevelData: "1234"
+                    }
+                };
+            }
+            let dispatch = (obj) => {
+                expect(obj.type).toBe(SET_SEARCH_URN);
+            }
+            const spyFunction = jest.spyOn(actions, 'setScroll');
+            axios.get = jest.fn(() => Promise.reject({}));
+            actions.setScroll(scrollArgs)(dispatch, getState);
+            expect(spyFunction).toHaveBeenCalled();
+            spyFunction.mockClear();
+        })
+        it('Elseif condition check', () => {
+            let scrollArgs = {
+                type : 'SET_COMMENT_SEARCH_URN',
+                scrollTop : 0
+            }
+            let getState = () => {
+                return {
+                    searchReducer: {
+                        slateLevelData: "1234",
+                        commentSearchTerm: "abc",
+                    },
+                    commentSearchReducer: {
+                        commentSearchTerm: "abc",
+                    }
+                };
+            }
+            let dispatch = (obj) => {
+                expect(obj.type).toBe(SET_COMMENT_SEARCH_URN);
+            }
+            const spyFunction = jest.spyOn(actions, 'setScroll');
+            axios.get = jest.fn(() => Promise.reject({}));
+            actions.setScroll(scrollArgs)(dispatch, getState);
+            expect(spyFunction).toHaveBeenCalled();
+            spyFunction.mockClear();
+        })
     });
 
     describe('Testing------- getContainerData', () => {
@@ -144,6 +217,48 @@ describe('-----------------Testing Search Actions-----------------', () => {
             actions.getContainerData();
             expect(spygetContainerData).toHaveBeenCalled();
         });
+        it('testing 2------- getContainerData ', () => {
+            const spygetContainerData = jest.spyOn(actions, 'getContainerData');
+            let dispatch = (obj) => {
+                expect(obj.type).toBe(SET_SEARCH_URN);
+            }
+            config.slateManifestURN = "urn:pearson:manifest:d9023151-3417-4482-8175-fc965466220e";
+            let getState = () => {
+                return {
+                    appStore: {
+                        pageNumberData: {},
+                        slateLevelData: DefaultAssessmentSlateData,
+                        isLearnosityProjectInfo: [],
+                    },
+                };
+            }
+            actions.getContainerData()(dispatch, getState);
+            expect(spygetContainerData).toHaveBeenCalled();
+        });
+        it('testing 3------- getContainerData ', () => {
+            let dispatch = (obj) => {
+                expect(obj.type).toBe(SET_SEARCH_URN);
+            }
+            config.slateManifestURN = "urn:pearson:manifest:d9023151-3417-4482-8175-fc965466220e";
+            let getState = () => {
+                return {
+                    appStore: {
+                        pageNumberData: {},
+                        slateLevelData: DefaultAssessmentSlateData,
+                        isLearnosityProjectInfo: [],
+                    },
+                };
+            }
+            let searchTerm = "urn:pearson:manifest:d9023151-3417-4482-8175-fc965466220e";
+            let res = {
+                data: DefaultAssessmentSlateData
+            }
+            const spygetContainerData = jest.spyOn(actions, 'getContainerData');
+            axios.get = jest.fn(() => Promise.resolve(res));
+            actions.getContainerData(searchTerm)(dispatch, getState)
+            expect(spygetContainerData).toHaveBeenCalled();
+            spygetContainerData.mockClear();
+        });
     });
     
     describe('Testing------- getCommentElements', () => {
@@ -151,6 +266,36 @@ describe('-----------------Testing Search Actions-----------------', () => {
             const spygetCommentElements = jest.spyOn(actions, 'getCommentElements');
             actions.getCommentElements();
             expect(spygetCommentElements).toHaveBeenCalled();
+        });
+        it('testing 2------- getCommentElements ', () => {
+            let dispatch = (obj) => {
+                expect(obj.type).toBe(SET_COMMENT_SEARCH_URN);
+            }
+            const axiosObject = axios.create({
+                headers: {
+                    'Content-Type': 'application/json',
+                    'myCloudProxySession': config.myCloudProxySession
+                }
+            });
+
+            config.slateManifestURN = "urn:pearson:manifest:d9023151-3417-4482-8175-fc965466220e";
+            let getState = () => {
+                return {
+                    appStore: {
+                        pageNumberData: {},
+                        slateLevelData: DefaultAssessmentSlateData,
+                        isLearnosityProjectInfo: [],
+                    },
+                };
+            }
+            let res = {
+                data: DefaultAssessmentSlateData
+            }
+            const spygetContainerData = jest.spyOn(actions, 'getCommentElements');
+            axiosObject.get = jest.fn(() => Promise.resolve(res));
+            actions.getCommentElements()(dispatch, getState)
+            expect(spygetContainerData).toHaveBeenCalled();
+            spygetContainerData.mockClear();
         });
     });
 });
