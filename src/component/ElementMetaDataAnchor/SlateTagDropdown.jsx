37daa0f4eba237a44d76991e57fb22125f4701a6
Sai Prakash Sasubilli <saiprakash.sasubilli@pearson.com>
2022-07-28 13:51:59 +0530

    PCAT-15160 - code correction

--- src/component/ElementMetaDataAnchor/SlateTagDropdown.jsx
+++ src/component/ElementMetaDataAnchor/SlateTagDropdown.jsx
@@ -102,7 +102,7 @@ class SlateTagDropdown extends React.Component {
             sendDataToIframe({ 'type': OpenLOPopup, 'message': { 'text': ViewLearningObjectiveSlate, 'data': currentSlateLOData, 'chapterContainerUrn': config.parentContainerUrn, 'isLOExist': isLOExist, 'editAction': '' } });
         }
         else if (e.target.innerText == ViewLearningObjectiveSlateDropdown && config.slateType === 'assessment') {
-            sendDataToIframe({ 'type': OpenLOPopup, 'message': { 'text': ViewLearningObjectiveAssessment, 'data': currentSlateLOData, 'chapterContainerUrn': config.parentContainerUrn, 'isLOExist': true, 'editAction': '','apiConstants':apiKeys_LO,'assessmentUrn':config.assessmentId ,'previewData': previewData,'isSubscribed':isSubscribed } });
+            sendDataToIframe({ 'type': OpenLOPopup, 'message': { 'text': ViewLearningObjectiveAssessment, 'data': currentSlateLOData, 'chapterContainerUrn': config.parentContainerUrn, 'isLOExist': true, 'editAction': '','apiConstants':apiKeys_LO,'assessmentUrn': assessmentuRN ?? config.assessmentId ,'previewData': previewData,'isSubscribed':isSubscribed } });
         }
         else if(checkSlateLock(this.props.slateLockInfo)){
             this.props.showSlateLockPopup(true);
