a72b034ac3a3c3e839133f0a2078bfb2ded403b5
Tushar Shivaji Khandejod <tushar.khandejod@magicsw.com>
2022-09-15 12:03:57 +0530

    PCAT-15884 : one unnecessary import is removed

--- __test__/component/ElementInteractive/ElementInteractive.test.js
+++ __test__/component/ElementInteractive/ElementInteractive.test.js
@@ -11,7 +11,6 @@ import { Interactivefpo , InteractiveFlashcards, Interactive3party, Interactivep
     ,Interactivesimulation,Interactivesurvey,Interactivetimeline,Interactivehotspot,Interactiveaccountingtable,
     Interactivefillinblank,Interactivegalleryimage,Interactivegalleryvideo,Interactivevideomcq,Interactivemcq , InteractiveGuidedExample, interactiveElm } from '../../../fixtures/ElementInteractiveTesting.js'
 import thunk from 'redux-thunk';
-import { getCookieByName } from '../../../src/constants/utility';
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 const dummyData = [
