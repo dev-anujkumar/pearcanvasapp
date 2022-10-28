7b24f5e41387f94b805d6bbdccaebb3b7953d64b
Akash <akash.shukla2@pearson.com>
2022-10-28 21:17:20 +0530

    PCAT-17032 - blockquote element handling for incorrect index done

--- src/component/CutCopyDialog/CutCopyDialog.jsx
+++ src/component/CutCopyDialog/CutCopyDialog.jsx
@@ -3,8 +3,18 @@ import store from '../../appstore/store';
 import config from './../../config/config';
 import { popupCutCopyParentData} from '../FigureHeader/AutoNumberActions';
 
-const CutCopyDialog = props => {
+// function to be called on click of refresh option
+const refreshElement = (props) => {
+    let index = null;
+    // handling blockquote index for element refresh 
+    if(props?.element?.elementdata?.type === 'blockquote' && props?.index) {
+        index = props?.index + '-0';
+    }
+    props.handleBlur(true, null, index, null, null, null, 'REFRESH_ELEMENT');
+}
 
+const CutCopyDialog = props => {
+    
     const positionStyle = { left: `${props.copyClickedX}px`, top: `${props.copyClickedY}px` }
     const popupSlateNotAcceptedTypes = ['groupedcontent', 'showhide', 'citations', 'element-citation', 'poetry', 'stanza'];
     const refreshRestrictedElementTypes = ['groupedcontent', 'showhide', 'citations', 'element-aside', 'manifestlist', 'popup', 'discussion', 'poetry', 'element-dialogue'];
@@ -17,7 +27,7 @@ const CutCopyDialog = props => {
                 <div className="copyUrn" onClick={(e) => { copyToClipBoard(e, props) }}>Copy {props.element.id.includes('work') ? 'Work' : 'Manifest'} URN</div>
             </div>
             {
-                showRefreshOption && <div className="copyUrn" onClick={(e) => props.handleBlur(true,null,null,null,null,null,'REFRESH_ELEMENT')}>
+                showRefreshOption && <div className="copyUrn" onClick={() => refreshElement(props)}>
                     Refresh
                 </div>
             }
