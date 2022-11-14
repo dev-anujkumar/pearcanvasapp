7f45e1a065a49ec80a190864ee10b4f70df586e3
Ankit Gupta <ankit.gupta1@pearson.com>
2022-11-14 14:27:35 +0000

    update canvas-1.42.1.01 tag

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import { modifyObjKeys } from './js/appUtils'
 // IMPORT - Assets // 
 import './styles/style.css';
 
-console.log("!!!!! ---- canvas-1.42.1 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.42.1.01 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
