b15d6faefb61c98ff2ce532ec49b7bde9b3e25ac
Anushka Bajpai <anushka.bajpai@pearson.com>
2023-01-17 16:57:33 +0530

    PCAT-18126 | Diabled Cypress alignment button and edit pop up.

--- src/component/ElementMetaDataAnchor/ElementMetaDataAnchor.jsx
+++ src/component/ElementMetaDataAnchor/ElementMetaDataAnchor.jsx
@@ -118,20 +118,10 @@ export class ElementMetaDataAnchor extends Component {
         this.props.showBlocker(true);
         this.launchExternalFrameworkPopup()
       }
-    else{
-        this.props.showBlocker(true);
-        let slateManifestURN= config.tempSlateManifestURN ? config.tempSlateManifestURN : config.slateManifestURN
-        let apiKeys_LO = {
-          'loApiUrl': config.LEARNING_OBJECTIVES_ENDPOINT,
-          'strApiKey': config.STRUCTURE_APIKEY,
-          'mathmlImagePath': config.S3MathImagePath ? config.S3MathImagePath : defaultMathImagePath,
-          'productApiUrl': config.PRODUCTAPI_ENDPOINT,
-          'manifestApiUrl': config.ASSET_POPOVER_ENDPOINT,
-          'assessmentApiUrl': config.ASSESSMENT_ENDPOINT,
-          'myCloudProxySession': config.myCloudProxySession
-        };
-        sendDataToIframe({ 'type': 'getLOEditPopup', 'message': { lodata: loData, projectURN: config.projectUrn, slateURN: slateManifestURN, apiKeys_LO, wrapperURL: config.WRAPPER_URL } }) 
-      }
+      else if ( !loData ) {
+          this.props.showBlocker(true);
+          sendDataToIframe({ 'type': 'getLOEditPopup', 'message': { lodata: loData, projectURN: config.projectUrn, slateURN: '', apiKeys_LO: '', wrapperURL: config.WRAPPER_URL } }) 
+        }
     }
   }
 
