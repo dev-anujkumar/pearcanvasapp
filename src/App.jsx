0823cec6348b655863b35c918f99c54792a205f2
Jaya Singh <jaya.singh1@pearson.com>
2023-02-06 14:56:40 +0000

    tag updated to canvas-1.44.27

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import { modifyObjKeys } from './js/appUtils'
 // IMPORT - Assets // 
 import './styles/style.css';
 
-console.log("!!!!! ---- canvas-1.44.26 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.44.27 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
