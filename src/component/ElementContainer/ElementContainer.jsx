a29b5fc05813fdc65a3868b8544df5a7e15e9494
Nishu Tyagi <nishu.tyagi@magicsw.com>
2023-02-06 20:22:21 +0530

    Fixed PCAT-18455: Approve to WIP warning pop-up is showing for pop-up element

--- src/component/ElementContainer/ElementContainer.jsx
+++ src/component/ElementContainer/ElementContainer.jsx
@@ -1679,7 +1679,7 @@ class ElementContainer extends Component {
         let toolbar = document.querySelector('div#tinymceToolbar .tox-toolbar__primary')
         if (action === "add") {
             toolbar?.classList?.add("disable");
-        } else if (action === "remove" && slateStatus !== "approved" && !popupSlate) {
+        } else if (action === "remove" && (slateStatus !== "approved" && !popupSlate)) {
             toolbar?.classList?.remove("disable");
         }
     }
