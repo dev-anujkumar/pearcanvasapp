3f11f9692b440c8844c76c4b2439d9131fa82811
Shruti Verma <shruti.verma@magicsw.com>
2023-03-03 18:46:02 +0530

    Canvas testcases

--- __test__/component/ElementAsideContainer/SectionSeperator.test.js
+++ __test__/component/ElementAsideContainer/SectionSeperator.test.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import SectionSeperator from '../../../src/component/ElementAsideContainer/SectionSeperator';
-import { spy, stub } from 'sinon';
-import tinymce from 'tinymce/tinymce';
+import { stub } from 'sinon';
 const setActiveElement = new stub();
 
 jest.mock('../../../src/config/config.js', () => ({
-    asideToolbar: ['bold','italic','underline','strikethrough','clearformatting','increaseindent','decreaseindent','footnote','glossary','orderedlist','unorderedlist','mathml','chemml','inlinecode','superscript','subscript','specialcharactor','undo','redo','assetpopover','slatetag'],   
-    toolBarList : ['bold','italic','underline','strikethrough','clearformatting','increaseindent','decreaseindent','footnote','glossary','orderedlist','unorderedlist','mathml','chemml','inlinecode','superscript','subscript','specialcharactor','undo','redo','assetpopover','slatetag']
-    }));
+    asideToolbar: ['bold', 'italic', 'underline', 'strikethrough', 'clearformatting', 'increaseindent', 'decreaseindent', 'footnote', 'glossary', 'orderedlist', 'unorderedlist', 'mathml', 'chemml', 'inlinecode', 'superscript', 'subscript', 'specialcharactor', 'undo', 'redo', 'assetpopover', 'slatetag'],
+    toolBarList: ['bold', 'italic', 'underline', 'strikethrough', 'clearformatting', 'increaseindent', 'decreaseindent', 'footnote', 'glossary', 'orderedlist', 'unorderedlist', 'mathml', 'chemml', 'inlinecode', 'superscript', 'subscript', 'specialcharactor', 'undo', 'redo', 'assetpopover', 'slatetag']
+}));
 
-describe('Testing SectionSeperator component with props', () => {
+describe('Testing SectionSeperator component with borderToggle', () => {
     let props = {
-        elemBorderToggle: true,
         borderToggle: 'active',
-        permissions: []
+        userRole: "admin",
+        permissions: ['elements_add_remove'],
+        element: { type: "element-authoredtext", subtype: "test", id: "work" },
     }
-    let wrapper = mount(< SectionSeperator setActiveElement = {setActiveElement} {...props} />)
 
+    let wrapper = mount(< SectionSeperator setActiveElement={setActiveElement} {...props} />)
     const instance = wrapper.instance();
+
     describe('Testing SectionSeperator component', () => {
         it('should render handleSeperatorBlur function', () => {
             const event = {
                 preventDefault: () => { },
                 stopPropagation: () => { }
             };
-
             instance.handleSeperatorBlur(event)
             const sectionFocus = wrapper.find('SectionSeperator').state().sectionFocus
             expect(sectionFocus).toEqual(false);
@@ -36,10 +36,49 @@ describe('Testing SectionSeperator component with props', () => {
                 preventDefault: () => { },
                 stopPropagation: () => { }
             };
+            instance.setState({
+                showCopyPopup: true,
+            })
             instance.handleSeperator(event)
             const sectionFocus = wrapper.find('SectionSeperator').state().sectionFocus
             expect(sectionFocus).toEqual(true);
         })
     })
 
+})
+
+describe('Testing SectionSeperator component without borderToggle', () => {
+    let props = {
+        userRole: "admin",
+        permissions: ['elements_add_remove'],
+        element: { type: "element-authoredtext", subtype: "test", id: "work" }
+    }
+
+    let wrapper = mount(< SectionSeperator setActiveElement={setActiveElement} {...props} />)
+    const instance = wrapper.instance();
+
+    describe('Testing SectionSeperator component', () => {
+        it('should render handleSeperatorBlur function', () => {
+            const event = {
+                preventDefault: () => { },
+                stopPropagation: () => { }
+            };
+            instance.handleSeperatorBlur(event)
+            const sectionFocus = wrapper.find('SectionSeperator').state().sectionFocus
+            expect(sectionFocus).toEqual(false);
+        })
+
+        it('should render handleSeperator function', () => {
+            const event = {
+                preventDefault: () => { },
+                stopPropagation: () => { }
+            };
+            instance.setState({
+                showCopyPopup: true,
+            })
+            instance.handleSeperator(event)
+            const sectionFocus = wrapper.find('SectionSeperator').state().sectionFocus
+            expect(sectionFocus).toEqual(true);
+        })
+    })
 })
