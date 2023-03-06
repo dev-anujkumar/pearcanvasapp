a8995b0ec18b2c1d1587dc7c9d7861f98c7bb1fc
Anushka Bajpai <anushka.bajpai@pearson.com>
2023-03-06 11:20:09 +0530

    testcase/PCAT-18818 | Increased coverage of FigureImageAsset file.

--- fixtures/FigureImageAssetTestingData.js
+++ fixtures/FigureImageAssetTestingData.js
@@ -25,7 +25,12 @@ export const figureImageAssetWithOutImage = {
             "path": "https://cite-media-stg.pearson.com/legacy_paths/796ae729-d5af-49b5-8c99-437d41cd2ef7/FPO-image.png",
             "height": "422",
             "width": "680",
-            "podwidth": ""
+            "podwidth": "",
+            "tableasHTML": [
+                "No Label",
+                "Table",
+                "Custom"
+            ]
         },
         "html": {
             "title": "<p><br></p>",
@@ -194,4 +199,405 @@ export const figureImageAssetWithOutImage = {
         "processedText": "<span class=\"codeNoHighlightLine\"><br /></span>"
     },
     "imgSrc": null
+}
+
+export const figureImageAssetWithOutImageBranch = {
+    "model": {
+        "id": "urn:pearson:work:7a54be9e-29c0-4e16-a75b-52a8376b436d",
+        "type": "figure",
+        "figuretype": "image",
+        "subtype": "imageTextWidth",
+        "schema": "http://schemas.pearson.com/wip-authoring/figure/1",
+        "titlecontentintitlefield": true,
+        "alignment": "text-width",
+        "title": {
+            "schema": "http://schemas.pearson.com/wip-authoring/authoredtext/1#/definitions/authoredtext",
+            "text": ""
+        },
+        "captions": {
+            "schema": "http://schemas.pearson.com/wip-authoring/authoredtext/1#/definitions/authoredtext",
+            "text": ""
+        },
+        "credits": {
+            "schema": "http://schemas.pearson.com/wip-authoring/authoredtext/1#/definitions/authoredtext",
+            "text": ""
+        },
+        "figuredata": {
+            "schema": "http://schemas.pearson.com/wip-authoring/image/1#/definitions/image",
+            "imageid": "cypress-01",
+            "path": "",
+            "height": "422",
+            "width": "680",
+            "podwidth": "",
+            "tableasHTML": ""
+        },
+        "html": {
+            "title": "<p><br></p>",
+            "captions": "<p><br></p>",
+            "credits": "<p><br></p>",
+            "footnotes": {},
+            "assetsPopover": {},
+            "glossaryentries": {},
+            "indexEntries": {}
+        },
+        "versionUrn": "urn:pearson:work:7a54be9e-29c0-4e16-a75b-52a8376b436d",
+        "contentUrn": "urn:pearson:entity:b936f978-ffdb-4bf8-b632-a71ec9d76100",
+        "indexPos": 0,
+        "slateEntityUrn": "urn:pearson:entity:3c8ab998-f803-46a7-a175-aecc2ff285e2"
+    },
+    "index": 0,
+    "elementId": "urn:pearson:work:7a54be9e-29c0-4e16-a75b-52a8376b436d",
+    "permissions": [
+        "login",
+        "logout",
+        "bookshelf_access",
+        "generate_epub_output",
+        "demand_on_print",
+        "toggle_tcm",
+        "content_preview",
+        "add_instructor_resource_url",
+        "grid_crud_access",
+        "alfresco_crud_access",
+        "set_favorite_project",
+        "sort_projects",
+        "search_projects",
+        "project_edit",
+        "edit_project_title_author",
+        "promote_review",
+        "promote_live",
+        "create_new_version",
+        "project_add_delete_users",
+        "create_custom_user",
+        "toc_add_pages",
+        "toc_delete_entry",
+        "toc_rearrange_entry",
+        "toc_edit_title",
+        "elements_add_remove",
+        "split_slate",
+        "full_project_slate_preview",
+        "access_formatting_bar",
+        "authoring_mathml",
+        "slate_traversal",
+        "trackchanges_edit",
+        "trackchanges_approve_reject",
+        "tcm_feedback",
+        "notes_access_manager",
+        "quad_create_edit_ia",
+        "quad_linking_assessment",
+        "add_multimedia_via_alfresco",
+        "toggle_element_page_no",
+        "toggle_element_borders",
+        "global_search",
+        "global_replace",
+        "edit_print_page_no",
+        "notes_adding",
+        "notes_deleting",
+        "notes_delete_others_comment",
+        "note_viewer",
+        "notes_assigning",
+        "notes_resolving_closing",
+        "notes_relpying",
+        "note_search_comment",
+        "lo_edit_metadata",
+        "preflight_access",
+        "learnosity_access",
+        "access-to-cypress+"
+    ],
+    "slateLockInfo": {
+        "isLocked": false,
+        "userId": ""
+    },
+    "glossaryFootnoteValue": {
+        "popUpStatus": false
+    },
+    "markedIndexValue": {
+        "type": "Markedindex",
+        "popUpStatus": false,
+        "markIndexid": ""
+    },
+    "alfrescoAssetData": {},
+    "alfrescoElementId": "",
+    "alfrescoListOption": {},
+    "launchAlfrescoPopup": false,
+    "isCiteChanged": false,
+    "changedSiteData": {},
+    "figureDropdownData": {
+        "id": "0000",
+        "image": [
+            "No Label",
+            "Figure",
+            "Table",
+            "Equation",
+            "Custom"
+        ],
+        "audio": [
+            "No Label",
+            "Custom"
+        ],
+        "video": [
+            "No Label",
+            "Video",
+            "Custom"
+        ],
+        "smartlinks": [
+            "No Label",
+            "Figure",
+            "Table",
+            "Custom",
+            "Knowdl"
+        ],
+        "mathml": [
+            "No Label",
+            "Equation",
+            "Custom"
+        ],
+        "preformattedtext": [
+            "No Label",
+            "Exhibit",
+            "Custom"
+        ],
+        "tableasmarkup": [
+            "No Label",
+            "Table",
+            "Custom"
+        ]
+    },
+    "slateAncestors": {
+        "containerUrn": "urn:pearson:manifest:7b501972-6881-48d5-9776-48260db07d7b",
+        "entityUrn": "urn:pearson:entity:3c8ab998-f803-46a7-a175-aecc2ff285e2",
+        "title": "",
+        "label": "section",
+        "matterType": "BodyMatter",
+        "ancestor": {
+            "containerUrn": "urn:pearson:manifest:f56fec4b-0718-4f4a-8741-84cfa9d29f08",
+            "entityUrn": "urn:pearson:entity:edf90a5b-a97a-4cfb-bdf7-2f2cbdd44e3a",
+            "title": "",
+            "label": "chapter",
+            "ancestor": {
+                "containerUrn": "urn:pearson:distributable:fc5a0834-c117-4629-95bc-270d413c0ba9",
+                "entityUrn": "urn:pearson:entity:ab412b78-4a52-4a84-bf2c-5ee248e1f7d2",
+                "title": "New Jan Scope2",
+                "label": "project"
+            }
+        }
+    },
+    "isAutoNumberingEnabled": true,
+    "figureTypeData": {
+        "imageClass": "figure-image",
+        "dataType": "image",
+        "imageDimension": "imageTextWidth",
+        "actualSizeClass": "",
+        "imgWidth": "",
+        "imgHeight": "",
+        "figTitleClass": "heading4ImageTextWidthTitle",
+        "figureHtmlData": {
+            "formattedLabel": "<p class=\"paragraphNumeroUno\"><br/></p>",
+            "formattedNumber": "<p class=\"paragraphNumeroUno\"><br/></p>",
+            "formattedTitle": "<p class=\"paragraphNumeroUno\"><br></p>"
+        },
+        "processedText": "<span class=\"codeNoHighlightLine\"><br /></span>"
+    }
+}
+
+export const figureImageAssetWithImage = {
+    "model": {
+        "id": "urn:pearson:work:7a54be9e-29c0-4e16-a75b-52a8376b436d",
+        "type": "figure",
+        "figuretype": "image",
+        "subtype": "imageTextWidth",
+        "schema": "http://schemas.pearson.com/wip-authoring/figure/1",
+        "titlecontentintitlefield": true,
+        "alignment": "text-width",
+        "title": {
+            "schema": "http://schemas.pearson.com/wip-authoring/authoredtext/1#/definitions/authoredtext",
+            "text": ""
+        },
+        "captions": {
+            "schema": "http://schemas.pearson.com/wip-authoring/authoredtext/1#/definitions/authoredtext",
+            "text": ""
+        },
+        "credits": {
+            "schema": "http://schemas.pearson.com/wip-authoring/authoredtext/1#/definitions/authoredtext",
+            "text": ""
+        },
+        "figuredata": {
+            "schema": "http://schemas.pearson.com/wip-authoring/image/1#/definitions/image",
+            "imageid": "urn:pearson:alfresco:743e8f65-118a-4c9b-8dcf-bce493cc230a",
+            "path": "https://cite-media-stg.pearson.com/legacy_paths/796ae729-d5af-49b5-8c99-437d41cd2ef7/FPO-image.png",
+            "height": "422",
+            "width": "680",
+            "podwidth": "",
+            "tableasHTML": [
+                "No Label",
+                "Table",
+                "Custom"
+            ]
+        },
+        "html": {
+            "title": "<p><br></p>",
+            "captions": "<p><br></p>",
+            "credits": "<p><br></p>",
+            "footnotes": {},
+            "assetsPopover": {},
+            "glossaryentries": {},
+            "indexEntries": {}
+        },
+        "versionUrn": "urn:pearson:work:7a54be9e-29c0-4e16-a75b-52a8376b436d",
+        "contentUrn": "urn:pearson:entity:b936f978-ffdb-4bf8-b632-a71ec9d76100",
+        "indexPos": 0,
+        "slateEntityUrn": "urn:pearson:entity:3c8ab998-f803-46a7-a175-aecc2ff285e2"
+    },
+    "index": 0,
+    "elementId": "urn:pearson:work:7a54be9e-29c0-4e16-a75b-52a8376b436d",
+    "permissions": [
+        "login",
+        "logout",
+        "bookshelf_access",
+        "generate_epub_output",
+        "demand_on_print",
+        "toggle_tcm",
+        "content_preview",
+        "add_instructor_resource_url",
+        "grid_crud_access",
+        "alfresco_crud_access",
+        "set_favorite_project",
+        "sort_projects",
+        "search_projects",
+        "project_edit",
+        "edit_project_title_author",
+        "promote_review",
+        "promote_live",
+        "create_new_version",
+        "project_add_delete_users",
+        "create_custom_user",
+        "toc_add_pages",
+        "toc_delete_entry",
+        "toc_rearrange_entry",
+        "toc_edit_title",
+        "elements_add_remove",
+        "split_slate",
+        "full_project_slate_preview",
+        "access_formatting_bar",
+        "authoring_mathml",
+        "slate_traversal",
+        "trackchanges_edit",
+        "trackchanges_approve_reject",
+        "tcm_feedback",
+        "notes_access_manager",
+        "quad_create_edit_ia",
+        "quad_linking_assessment",
+        "add_multimedia_via_alfresco",
+        "toggle_element_page_no",
+        "toggle_element_borders",
+        "global_search",
+        "global_replace",
+        "edit_print_page_no",
+        "notes_adding",
+        "notes_deleting",
+        "notes_delete_others_comment",
+        "note_viewer",
+        "notes_assigning",
+        "notes_resolving_closing",
+        "notes_relpying",
+        "note_search_comment",
+        "lo_edit_metadata",
+        "preflight_access",
+        "learnosity_access",
+        "access-to-cypress+"
+    ],
+    "slateLockInfo": {
+        "isLocked": false,
+        "userId": ""
+    },
+    "glossaryFootnoteValue": {
+        "popUpStatus": false
+    },
+    "markedIndexValue": {
+        "type": "Markedindex",
+        "popUpStatus": false,
+        "markIndexid": ""
+    },
+    "alfrescoAssetData": {},
+    "alfrescoElementId": "",
+    "alfrescoListOption": {},
+    "launchAlfrescoPopup": false,
+    "isCiteChanged": false,
+    "changedSiteData": {},
+    "figureDropdownData": {
+        "id": "0000",
+        "image": [
+            "No Label",
+            "Figure",
+            "Table",
+            "Equation",
+            "Custom"
+        ],
+        "audio": [
+            "No Label",
+            "Custom"
+        ],
+        "video": [
+            "No Label",
+            "Video",
+            "Custom"
+        ],
+        "smartlinks": [
+            "No Label",
+            "Figure",
+            "Table",
+            "Custom",
+            "Knowdl"
+        ],
+        "mathml": [
+            "No Label",
+            "Equation",
+            "Custom"
+        ],
+        "preformattedtext": [
+            "No Label",
+            "Exhibit",
+            "Custom"
+        ],
+        "tableasmarkup": [
+            "No Label",
+            "Table",
+            "Custom"
+        ]
+    },
+    "slateAncestors": {
+        "containerUrn": "urn:pearson:manifest:7b501972-6881-48d5-9776-48260db07d7b",
+        "entityUrn": "urn:pearson:entity:3c8ab998-f803-46a7-a175-aecc2ff285e2",
+        "title": "",
+        "label": "section",
+        "matterType": "BodyMatter",
+        "ancestor": {
+            "containerUrn": "urn:pearson:manifest:f56fec4b-0718-4f4a-8741-84cfa9d29f08",
+            "entityUrn": "urn:pearson:entity:edf90a5b-a97a-4cfb-bdf7-2f2cbdd44e3a",
+            "title": "",
+            "label": "chapter",
+            "ancestor": {
+                "containerUrn": "urn:pearson:distributable:fc5a0834-c117-4629-95bc-270d413c0ba9",
+                "entityUrn": "urn:pearson:entity:ab412b78-4a52-4a84-bf2c-5ee248e1f7d2",
+                "title": "New Jan Scope2",
+                "label": "project"
+            }
+        }
+    },
+    "isAutoNumberingEnabled": true,
+    "figureTypeData": {
+        "imageClass": "figure-image",
+        "dataType": "image",
+        "imageDimension": "imageTextWidth",
+        "actualSizeClass": "",
+        "imgWidth": "",
+        "imgHeight": "",
+        "figTitleClass": "heading4ImageTextWidthTitle",
+        "figureHtmlData": {
+            "formattedLabel": "<p class=\"paragraphNumeroUno\"><br/></p>",
+            "formattedNumber": "<p class=\"paragraphNumeroUno\"><br/></p>",
+            "formattedTitle": "<p class=\"paragraphNumeroUno\"><br></p>"
+        },
+        "processedText": "<span class=\"codeNoHighlightLine\"><br /></span>"
+    },
+    "imgSrc": "urn:pearson:alfresco:743e8f65-118a-4c9b-8dcf-bce493cc230a"
 }
