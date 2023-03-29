b85ba66fc9c855a21dd58069dbaa0ce46d16deff
Rahul Maheshwari <rahul.maheshwari@magicsw.com>
2023-03-29 16:58:09 +0530

    removed unused code

--- src/component/CommonLoader/CommonLoader.jsx
+++ src/component/CommonLoader/CommonLoader.jsx
@@ -4,22 +4,12 @@
 import React from 'react';
 import './../../styles/CommonLoader/CommonLoader.css';
   
-
 const CommonLoader = (props) => {
-
     return (
         <>
         <div id="commonLoader"></div>
         </>
     );
-}
-
-const mapStateToProps = state => ({
-})
-
-const mapActionsToProps = (dispatch) => {
-    return {
-    }
 }
 
 export default CommonLoader;
