cbce5a6397c94a63f21d72110cd37f7ff600ed50
Ankit Gupta <ankit.gupta1@pearson.com>
2023-04-05 13:06:34 +0530

    PCAT:18383:Update Auth and Preview

--- src/js/google_analytics.js
+++ src/js/google_analytics.js
@@ -7,28 +7,22 @@ let isGtmInitialized = false
   * @description initialise GTM in project
   */
 export const initializeGTM = (env) => {
-  //const auth = env.GOOGLE_TAG_MANAGER_AUTH
-  //const preview = env.GOOGLE_TAG_MANAGER_PREVIEW
-  //env.GTM_ID ='GTM-N6W3WRM' // new
-  //env.GTM_ID ='GTM-NFMDD8B' // old
+  const auth = env.GOOGLE_TAG_MANAGER_AUTH || 'uMmKRWMnVp0n5ln33vV-MA'
+  const preview = env.GOOGLE_TAG_MANAGER_PREVIEW || 'env-21'
+  // env.GTM_ID ='GTM-N6W3WRM' // new
+  // env.GTM_ID ='GTM-NFMDD8B' // old
   const GTM_ID = env.GTM_ID || 'GTM-N6W3WRM'
   console.log(GTM_ID, "inside GTM",env)
-  
-//   if (auth && preview) {
-//     const tagManagerArgs = {
-//       gtmId: GTM_ID,
-//       auth,
-//       preview
-//     }
-    if(GTM_ID){
-        const tagManagerArgs = {
-                gtmId: GTM_ID,
-        }
-        TagManager.initialize(tagManagerArgs)
-        isGtmInitialized = true
+  if (auth && preview) {
+    const tagManagerArgs = {
+      gtmId: GTM_ID,
+      auth,
+      preview
     }
-    
-  //}
+
+    TagManager.initialize(tagManagerArgs)
+    isGtmInitialized = true
+  }
 }
 
 
