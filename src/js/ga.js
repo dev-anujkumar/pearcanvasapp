393898ec51b8fd458c6c4c03e084f0dbfdf990d7
Ankit Gupta <ankit.gupta1@pearson.com>
2023-04-13 21:29:31 +0530

    PCAT-18383: Update ga preview

--- src/js/ga.js
+++ src/js/ga.js
@@ -8,8 +8,8 @@ let isGtmInitialized = false
   */
 export const initializeGTM = (env) => {
   const auth = env.GOOGLE_TAG_MANAGER_AUTH || 'uMmKRWMnVp0n5ln33vV-MA'
-  //const preview = env.GOOGLE_TAG_MANAGER_PREVIEW || 'env-21'
-  const preview = ""
+  const preview = env.GOOGLE_TAG_MANAGER_PREVIEW || 'env-21'
+  //const preview = ""
   // env.GTM_ID ='GTM-NFMDD8B' // old
   const GTM_ID = env.GTM_ID || 'GTM-N6W3WRM'
   console.log(GTM_ID, "inside GTM",env)
@@ -31,9 +31,14 @@ export const triggerCustomEventsGTM = (event, data) => {
     console.log(isGtmInitialized,"isGtmInitialized",window.dataLayer)
     if (isGtmInitialized && window && window.dataLayer) {
         console.log(event,"Push data event",data)
-        window.dataLayer.push({
-            event,
-        ...data
-        });
+        let ee= {
+          event: event,
+          slate_data: data
+      }
+      window.dataLayer.push(ee);
+        // window.dataLayer.push({
+        //     event,
+        // ...data
+        // });
     }
 }
