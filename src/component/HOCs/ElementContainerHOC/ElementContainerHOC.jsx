5f8a2302e4ac801cb12477d1bf5ad57898a0aa63
Rahul Maheshwari <rahul.maheshwari@magicsw.com>
2023-04-21 13:45:37 +0530

    Tinymce null issue handeled for Poetry element

--- src/component/HOCs/ElementContainerHOC/ElementContainerHOC.jsx
+++ src/component/HOCs/ElementContainerHOC/ElementContainerHOC.jsx
@@ -39,12 +39,10 @@ const ElementContainerHOC = (WrappedComponent) => {
             let Title = subTitle = element.contents && element.contents['formatted-title']
             let credit = element.contents && element.contents.hasOwnProperty('creditsarray') && element.contents['creditsarray'][0]
             
-let poetryElem = <div className={divClass}>
+            let poetryElem = <div className={divClass}>
                 <div className={divContainerClass}>
                     <figure className={figureClass} >
                         <header>
-                            <TinyMceEditor permissions={this.props.permissions} openGlossaryFootnotePopUp={this.props.openGlossaryFootnotePopUp} element={this.props.model} handleEditorFocus={this.props.handleFocus} handleBlur={this.props.handleBlur} index={`${index}-0`} placeholder="Enter Label..." tagName={'h4'} className={figLabelClass + " figureLabel formatted-text"} model={formattedTitle} slateLockInfo={slateLockInfo} glossaryFootnoteValue={this.props.glossaryFootnoteValue} glossaaryFootnotePopup={this.props.glossaaryFootnotePopup} elementId={this.props.elementId} createPoetryElements={this.props.createPoetryElements} poetryField="formatted-title" 
-                             currentElement={Title} showHideType={this?.props?.showHideType}/>
                             <KeyboardWrapper index={`${this.props.index}-0`} enable>
                                 <TinyMceEditor permissions={this.props.permissions} openGlossaryFootnotePopUp={this.props.openGlossaryFootnotePopUp} element={this.props.model} handleEditorFocus={this.props.handleFocus} handleBlur={this.props.handleBlur} index={`${index}-1`} placeholder="Enter Title..." tagName={'h4'} className={figTitleClass + " figureTitle formatted-text"} model={formattedSubtitle} slateLockInfo={slateLockInfo} glossaryFootnoteValue={this.props.glossaryFootnoteValue} glossaaryFootnotePopup={this.props.glossaaryFootnotePopup} elementId={this.props.elementId} createPoetryElements={this.props.createPoetryElements} poetryField="formatted-subtitle"
                                     currentElement={subTitle} showHideType={this?.props?.showHideType} />
