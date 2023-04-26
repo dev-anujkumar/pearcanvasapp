988db9d687a1d6b3ff5071933f9648749db95fd1
Rahul Maheshwari <rahul.maheshwari@pearson.com>
2023-04-26 08:35:55 +0000

    Tag updated to canvas-1.47.8

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import { modifyObjKeys } from './js/appUtils';
 // IMPORT - Assets // 
 import './styles/style.css';
 
-console.log("!!!!! ---- canvas-1.47.7 ---- !!!!!")
+console.log("!!!!! ---- canvas-1.47.8 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
