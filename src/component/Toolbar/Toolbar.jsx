d6f63e385158b24864e359a4120ca2e6f93f0244
Kumar Anuj <kumar.anuj@magicsw.com>
2023-06-26 13:38:54 +0530

    PCAT-18123: Handled slate label for title,cover,copyright,contents,appendix and lti slates

--- src/component/Toolbar/Toolbar.jsx
+++ src/component/Toolbar/Toolbar.jsx
@@ -18,7 +18,7 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 import { slateVersioning } from '../SlateWrapper/SlateWrapper_Actions';
 import { MOVED_TO_WIP } from '../../constants/Element_Constants';
 import { ShowLoader } from '../../constants/IFrameMessageTypes';
-import { ALLOWED_SLATES_IN_RC, APPROVED_BANNER_MESSAGE1, APPROVED_BANNER_MESSAGE2, EDIT_CONTENT_BTN, SUBSCRIBER_BANNER_MESSAGE } from '../SlateWrapper/SlateWrapperConstants';
+import { ALLOWED_SLATES_IN_RC, APPROVED_BANNER_MESSAGE1, APPROVED_BANNER_MESSAGE2, EDIT_CONTENT_BTN, SLATES_DEFAULT_LABEL, SUBSCRIBER_BANNER_MESSAGE } from '../SlateWrapper/SlateWrapperConstants';
 
 const _Toolbar = props => {
     const { isToolBarBlocked,roleId } = props;
@@ -124,10 +124,11 @@ const _Toolbar = props => {
         if(ALLOWED_SLATES_IN_RC.includes(config.slateType) && !popupSlate) {
             updateRCSlate = true
         }
+        const slateLabel = props.slateTocLabel[config.slateType] ?? SLATES_DEFAULT_LABEL[config.slateType]
         sendDataToIframe({ 'type': ShowLoader, 'message': { status: true } })
         const approveToWipStatus = await props.slateVersioning(updateRCSlate)
         if(approveToWipStatus) {
-        showNotificationOnCanvas(`${props.slateTocLabel[config.slateType]} and its parent items has moved to WIP status`)
+        showNotificationOnCanvas(`${slateLabel} and its parent items has moved to WIP status`)
         changeAudioNarration()
         }
     }
