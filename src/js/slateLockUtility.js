9d95616f12062a95ab4a24d4564e282dc205d1d1
Jaya Singh <jaya.singh@magicsw.com>
2023-09-15 17:29:42 +0530

    added consoles

--- src/js/slateLockUtility.js
+++ src/js/slateLockUtility.js
@@ -5,5 +5,6 @@ import config from "../config/config"
  **/ 
 export const checkSlateLock = (slateLockInfo) => {
     let lockedUserId = slateLockInfo ? slateLockInfo.userId.replace(/.*\(|\)/gi, ''): ""; // Retrieve only PROOT id
+    console.log('CHECKING LOCKED USER ID', lockedUserId, 'CHECKING CONFIG userId VALUE', slateLockInfo.isLocked, 'CONFIG USER ID', config.userId)
     return (slateLockInfo.isLocked && config.userId !== lockedUserId);
 }
