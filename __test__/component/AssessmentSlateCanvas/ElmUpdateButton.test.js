11e711339417291e8e050e709dccec6391b069ba
Nishu Tyagi <nishu.tyagi@pearson.com>
2023-12-21 21:48:35 +0530

    Fixed elmupdatebutton file testcases

--- __test__/component/AssessmentSlateCanvas/ElmUpdateButton.test.js
+++ __test__/component/AssessmentSlateCanvas/ElmUpdateButton.test.js
@@ -20,4 +20,18 @@ describe('Testing ElmUpdateButton component', () => {
         status: true,
         assessmentItem: false
     }
+    it('Test 1- Unapproved Status --else', () => {
+        let props1 = {
+            elmAssessment: {
+                activeWorkUrn: "urn:pearson:work:fa7bcbce-1cc5-467e-be1d-66cc513ec464",
+                assessmentStatus: "wip",
+                latestWorkUrn: "urn:pearson:work:fa7bcbce-1cc5-467e-be1d-66cc513ec565",
+                showUpdateStatus: false
+            },
+            status: true,
+            assessmentItem: false
+        }
+        const component = mount(<ElmUpdateButton {...props1} />)
+        expect(component).toHaveLength(1);
+    })
 });
