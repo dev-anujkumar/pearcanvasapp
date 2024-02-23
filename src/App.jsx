21347516da759c7637b3a2f5ce1113d8e63bd709
Jaya Singh <jaya.singh1@pearson.com>
2024-02-23 08:22:53 +0000

    tag updated to canvas-ui.v1.0.6

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { modifyObjKeys } from './js/appUtils';
 // IMPORT - Assets //
 import './styles/style.css';
 
-console.log("!!!!! ---- canvas-ui.v1.0.5 ---- !!!!!")
+console.log("!!!!! ---- canvas-ui.v1.0.6 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
