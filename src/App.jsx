a34c987ca5f044dc0f9917fa2651fcc0396071b2
Rahul Maheshwari <rahul.maheshwari@pearson.com>
2024-03-04 14:57:31 +0000

    Tag updated to canvas-ui.v1.0.15

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import { modifyObjKeys } from './js/appUtils';
 import './styles/style.css';
 import './styles/bookStyle.css';
 
-console.log("!!!!! ---- canvas-ui.v1.0.14 ---- !!!!!")
+console.log("!!!!! ---- canvas-ui.v1.0.15 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
