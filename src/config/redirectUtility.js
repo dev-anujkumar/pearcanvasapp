191e97eaec7a6e2ce36146efae1d82e25912095f
Kumar Anuj <kumar.anuj@magicsw.com>
2024-03-06 11:14:29 +0530

    PCAT-22230: Updated redirection ul based on environment

--- src/config/redirectUtility.js
+++ src/config/redirectUtility.js
@@ -4,10 +4,10 @@ export const redirect = () => {
     const windowOrigin = window.parent.location.origin
     if (!(windowOrigin.includes('dev') || windowOrigin.includes('staging') ||
         windowOrigin.includes('qa') || windowOrigin.includes('test') ||
-        windowOrigin.includes('perf') || windowOrigin.includes('prf') || windowOrigin.includes('stg'))) {
-        config.IES_REDIRECT_URL = 'https://iam.pearson.com/auth/XUI/?realm=/pearson&authIndexType=service&authIndexValue=Login&goto='
-    } else if (windowOrigin.includes('perf') || windowOrigin.includes('prf')) {
-        config.IES_REDIRECT_URL = 'https://iam-ppe.pearson.com/auth/XUI/?realm=/pearson&authIndexType=service&authIndexValue=Login&goto='
+        windowOrigin.includes('prf') || windowOrigin.includes('stg'))) {
+        config.IES_REDIRECT_URL = 'https://iam.pearson.com/auth/loginRedirect.html?tree=Login&url='
+    } else if (windowOrigin.includes('prf')) {
+        config.IES_REDIRECT_URL = 'https://iam-ppe.pearson.com/auth/loginRedirect.html?tree=Login&url='
     }
     const redirectUrl = config.IES_REDIRECT_URL + encodeURIComponent(currentUrl)
     window.parent.location.replace(redirectUrl)
