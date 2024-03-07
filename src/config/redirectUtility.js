b71b103959bc6c158ff12101cb6bfdf980825577
Kumar Anuj <kumar.anuj@magicsw.com>
2024-03-07 12:56:11 +0530

    PCAT-22230: Removed redirect url from config

--- src/config/redirectUtility.js
+++ src/config/redirectUtility.js
@@ -1,14 +1,15 @@
-import config from "./config"
+
 export const redirect = () => {
     const currentUrl = window.parent.location.href
+    let iesRedirectUrl = "https://iam-stage.pearson.com/auth/loginRedirect.html?tree=Login&url="
     const windowOrigin = window.parent.location.origin
     if (!(windowOrigin.includes('dev') || windowOrigin.includes('staging') ||
         windowOrigin.includes('qa') || windowOrigin.includes('test') ||
         windowOrigin.includes('prf') || windowOrigin.includes('stg'))) {
-        config.IES_REDIRECT_URL = 'https://iam.pearson.com/auth/loginRedirect.html?tree=Login&url='
+        iesRedirectUrl = 'https://iam.pearson.com/auth/loginRedirect.html?tree=Login&url='
     } else if (windowOrigin.includes('prf')) {
-        config.IES_REDIRECT_URL = 'https://iam-ppe.pearson.com/auth/loginRedirect.html?tree=Login&url='
+        iesRedirectUrl = 'https://iam-ppe.pearson.com/auth/loginRedirect.html?tree=Login&url='
     }
-    const redirectUrl = config.IES_REDIRECT_URL + encodeURIComponent(currentUrl)
+    const redirectUrl = iesRedirectUrl + encodeURIComponent(currentUrl)
     window.parent.location.replace(redirectUrl)
 }
