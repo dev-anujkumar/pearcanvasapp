7870554230ec33dea11d574259519f86cd1c4cd9
Shivani Goel <shivani.goel@magicsw.com>
2024-04-16 17:54:39 +0530

    PCAT-22112: Added fix for vulnerability and deprecate package

--- jest.config.js
+++ jest.config.js
@@ -172,6 +172,7 @@ module.exports = {
   //To support media and CSS parsing
   moduleNameMapper: {
     "\\.(css|less)$": "identity-obj-proxy",
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.js"
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.js",
+    "^axios$": "axios/dist/node/axios.cjs"
   }
 };
