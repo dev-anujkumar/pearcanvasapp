81cf3b25b77272c6ed05770bd88b85eddd8fbc06
Akash Shukla <akash.shukla2@pearson.com>
2024-04-23 08:10:38 +0000

    Tag updated - canvas-ui.v1.0.51

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import { modifyObjKeys } from './js/appUtils';
 import './styles/style.css';
 import './styles/bookStyle.css';
 
-console.log("!!!!! ---- canvas-ui.v1.0.50 ---- !!!!!")
+console.log("!!!!! ---- canvas-ui.v1.0.51 ---- !!!!!")
 
 class App extends Component {
     constructor(props) {
